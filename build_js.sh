a3b0599cb8f7f741b039be8b014a1312fd3b9484
Manolis Savva <msavva@fb.com>
2019-05-27 21:28:54 -0700

    add build_js.sh test script

---
+++ build_js.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+git submodule update --init --recursive
+
+DATA_DIR="$(pwd)/data/"
+
+mkdir -p build_js
+cd build_js
+
+cmake ../src \
+    -DBUILD_WEBGL=ON \
+    -DBUILD_GUI_VIEWERS=ON \
+    -DBUILD_PYTHON_BINDINGS=OFF \
+    -DBUILD_DATATOOL=OFF \
+    -DBUILD_PTEX_SUPPORT=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$EMSCRIPTEN" \
+    -DTARGET_GLES2=OFF \
+    -DTARGET_WEBGL=ON \
+    -DCMAKE_TOOLCHAIN_FILE="../src/deps/corrade/toolchains/generic/Emscripten-wasm.cmake" \
+    -DCMAKE_INSTALL_PREFIX="." \
+    -DCMAKE_CXX_FLAGS="-s FORCE_FILESYSTEM=1 -s TOTAL_MEMORY=1677721600 --preload-file $DATA_DIR/scene_datasets/habitat-test-scenes@/" \
+
+cmake --build . -- -j 4
+cmake --build . --target install -- -j 4
