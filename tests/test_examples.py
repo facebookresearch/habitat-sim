8ec6daa63d243916e5bf53c23ebe6adceb0c0b8c
Erik Wijmans <etw@gatech.edu>
2019-05-29 14:57:51 -0700

    Revamp example and add test to example

---
+++ tests/test_examples.py
@@ -0,0 +1,21 @@
+import multiprocessing
+import os.path as osp
+
+import pytest
+
+import examples.new_actions
+
+
+@pytest.mark.skipif(
+    not osp.exists("data/scene_datasets/habitat-test-scenes/van-gogh-room.glb"),
+    reason="New actions script requires the habitat-test-scenes",
+)
+def test_new_actions_example():
+    # This test needs to be done in its own process as there is a potentially for
+    # an OpenGL context clash otherwise
+    mp_ctx = multiprocessing.get_context("spawn")
+    proc = mp_ctx.Process(target=examples.new_actions.main)
+    proc.start()
+    proc.join()
+
+    assert proc.exitcode == 0
