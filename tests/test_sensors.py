c4403bed13c649eb49694bfa1138863aa32a5f2f
Erik Wijmans <etw@gatech.edu>
2019-06-05 17:41:44 -0700

    isort with pytest installed

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -9,13 +9,13 @@ import json
 import os.path as osp
 
 import numpy as np
+import pytest
 import quaternion
 
 import habitat_sim
 import habitat_sim.bindings as hsim
 import habitat_sim.errors
 import habitat_sim.utils
-import pytest
 from examples.settings import make_cfg
 
 _test_scenes = [
