452e12212ec23e99473d5f24ea8a88d391f804df
Erik Wijmans <etw@gatech.edu>
2019-07-17 17:54:25 -0700

    Fix

--- habitat_sim/agent/controls/__init__.py
+++ habitat_sim/agent/controls/__init__.py
@@ -4,9 +4,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .controls import ActuationSpec, ObjectControls, SceneNodeControl, register_move_fn
-from .default_controls import *
-from .pyrobot_noisy_controls import PyRobotNoisyActuationSpec
+from habitat_sim.agent.controls.controls import (
+    ActuationSpec,
+    ObjectControls,
+    SceneNodeControl,
+    register_move_fn,
+)
+from habitat_sim.agent.controls.default_controls import *
+from habitat_sim.agent.controls.pyrobot_noisy_controls import PyRobotNoisyActuationSpec
 
 __all__ = [
     "ActuationSpec",
