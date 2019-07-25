887fc8fb0be5064e4472e94b71fe18595772b44e
erikwijmans <ewijmans2@gmail.com>
2019-07-24 19:52:47 -0700

    Seems to be working!

--- tmp.py
+++ tmp.py
@@ -2,14 +2,15 @@ import timeit
 
 import numpy as np
 
-from habitat_sim.sensors.depth_noise_model import depth_noise_model
+from habitat_sim.sensors.noise_models import make_sensor_noise_model
 
 gt_depth = np.random.uniform(0, 10, size=(256, 256))
+depth_noise_model = make_sensor_noise_model("RedwoodDepthNoiseModel")
 
 print(np.abs(gt_depth - depth_noise_model(gt_depth)).mean())
 
 
 print(
-    100
-    / np.mean(timeit.repeat(lambda: depth_noise_model(gt_depth), repeat=5, number=100))
+    1000
+    / np.min(timeit.repeat(lambda: depth_noise_model(gt_depth), repeat=10, number=1000))
 )
