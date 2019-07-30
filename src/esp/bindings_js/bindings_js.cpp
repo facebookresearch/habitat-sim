65fc18034f7d166e0ea04a821a6e1c0b00fceaf6
Angel Xuan Chang <angelx@fb.com>
2019-07-30 10:37:48 -0700

    Initial javascript bindings.

---
+++ src/esp/bindings_js/bindings_js.cpp
@@ -0,0 +1,202 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <emscripten/bind.h>
+
+namespace em = emscripten;
+
+#include "esp/sim/SimulatorWithAgents.h"
+
+using namespace esp;
+using namespace esp::agent;
+using namespace esp::core;
+using namespace esp::geo;
+using namespace esp::gfx;
+using namespace esp::nav;
+using namespace esp::scene;
+using namespace esp::sensor;
+using namespace esp::sim;
+
+// Consider
+// https://becominghuman.ai/passing-and-returning-webassembly-array-parameters-a0f572c65d97
+em::val Observation_getData(Observation& obs) {
+  auto buffer = obs.buffer;
+  if (buffer != nullptr) {
+    return em::val(em::typed_memory_view(buffer->totalBytes,
+                                         static_cast<uint8_t*>(buffer->data)));
+  } else {
+    return em::val::undefined();
+  }
+}
+
+ObservationSpace Simulator_getAgentObservationSpace(SimulatorWithAgents& sim,
+                                                    int agentId,
+                                                    std::string sensorId) {
+  ObservationSpace space;
+  sim.getAgentObservationSpace(agentId, sensorId, space);
+  return space;
+}
+
+std::map<std::string, ObservationSpace> Simulator_getAgentObservationSpaces(
+    SimulatorWithAgents& sim,
+    int agentId) {
+  std::map<std::string, ObservationSpace> spaces;
+  sim.getAgentObservationSpaces(agentId, spaces);
+  return spaces;
+}
+
+EMSCRIPTEN_BINDINGS(habitat_sim_bindings_js) {
+  em::register_vector<SensorSpec::ptr>("VectorSensorSpec");
+  em::register_vector<size_t>("VectorSizeT");
+  em::register_vector<std::string>("VectorString");
+
+  em::register_map<std::string, float>("MapStringFloat");
+  em::register_map<std::string, std::string>("MapStringString");
+  em::register_map<std::string, Sensor::ptr>("MapStringSensor");
+  em::register_map<std::string, SensorSpec::ptr>("MapStringSensorSpec");
+  em::register_map<std::string, Observation>("MapStringObservation");
+  em::register_map<std::string, ActionSpec::ptr>("ActionSpace");
+
+  em::value_array<vec2f>("vec2f")
+      .element(em::index<0>())
+      .element(em::index<1>());
+
+  em::value_array<vec3f>("vec3f")
+      .element(em::index<0>())
+      .element(em::index<1>())
+      .element(em::index<2>());
+
+  em::value_array<vec4f>("vec4f")
+      .element(em::index<0>())
+      .element(em::index<1>())
+      .element(em::index<2>())
+      .element(em::index<3>());
+
+  em::value_array<vec2i>("vec2i")
+      .element(em::index<0>())
+      .element(em::index<1>());
+
+  em::value_array<vec3i>("vec3i")
+      .element(em::index<0>())
+      .element(em::index<1>())
+      .element(em::index<2>());
+
+  em::value_array<vec4i>("vec4i")
+      .element(em::index<0>())
+      .element(em::index<1>())
+      .element(em::index<2>())
+      .element(em::index<3>());
+
+  em::class_<AgentConfiguration>("AgentConfiguration")
+      .smart_ptr_constructor("AgentConfiguration",
+                             &AgentConfiguration::create<>)
+      .property("height", &AgentConfiguration::height)
+      .property("radius", &AgentConfiguration::radius)
+      .property("mass", &AgentConfiguration::mass)
+      .property("linearAcceleration", &AgentConfiguration::linearAcceleration)
+      .property("angularAcceleration", &AgentConfiguration::angularAcceleration)
+      .property("linearFriction", &AgentConfiguration::linearFriction)
+      .property("angularFriction", &AgentConfiguration::angularFriction)
+      .property("coefficientOfRestitution",
+                &AgentConfiguration::coefficientOfRestitution)
+      .property("sensorSpecifications",
+                &AgentConfiguration::sensorSpecifications);
+
+  em::class_<ActionSpec>("ActionSpec")
+      .smart_ptr_constructor(
+          "ActionSpec",
+          &ActionSpec::create<const std::string&, const ActuationMap&>)
+      .property("name", &ActionSpec::name)
+      .property("actuation", &ActionSpec::actuation);
+
+  em::class_<SensorSuite>("SensorSuite")
+      .smart_ptr_constructor("SensorSuite", &SensorSuite::create<>)
+      .function("get", &SensorSuite::get);
+
+  em::class_<SensorSpec>("SensorSpec")
+      .smart_ptr_constructor("SensorSpec", &SensorSpec::create<>)
+      .property("uuid", &SensorSpec::uuid)
+      .property("sensorType", &SensorSpec::sensorType)
+      .property("sensorSubtype", &SensorSpec::sensorSubtype)
+      .property("position", &SensorSpec::position)
+      .property("orientation", &SensorSpec::orientation)
+      .property("resolution", &SensorSpec::resolution)
+      .property("parameters", &SensorSpec::parameters);
+
+  em::class_<Sensor>("Sensor").function("specification",
+                                        &Sensor::specification);
+
+  em::class_<SceneConfiguration>("SceneConfiguration")
+      .smart_ptr_constructor("SceneConfiguration",
+                             &SceneConfiguration::create<>)
+      .property("dataset", &SceneConfiguration::dataset)
+      .property("id", &SceneConfiguration::id)
+      .property("filepaths", &SceneConfiguration::filepaths)
+      .property("sceneUpDir", &SceneConfiguration::sceneUpDir)
+      .property("sceneFrontDir", &SceneConfiguration::sceneFrontDir)
+      .property("sceneScaleUnit", &SceneConfiguration::sceneScaleUnit);
+
+  em::class_<SimulatorConfiguration>("SimulatorConfiguration")
+      .smart_ptr_constructor("SimulatorConfiguration",
+                             &SimulatorConfiguration::create<>)
+      .property("scene", &SimulatorConfiguration::scene)
+      .property("defaultAgentId", &SimulatorConfiguration::defaultAgentId)
+      .property("defaultCameraUuid", &SimulatorConfiguration::defaultCameraUuid)
+      .property("gpuDeviceId", &SimulatorConfiguration::gpuDeviceId)
+      .property("width", &SimulatorConfiguration::width)
+      .property("height", &SimulatorConfiguration::height)
+      .property("compressTextures", &SimulatorConfiguration::compressTextures);
+
+  em::class_<AgentState>("AgentState")
+      .smart_ptr_constructor("AgentState", &AgentState::create<>)
+      .property("position", &AgentState::position)
+      .property("rotation", &AgentState::rotation)
+      .property("velocity", &AgentState::velocity)
+      .property("angularVelocity", &AgentState::angularVelocity)
+      .property("force", &AgentState::force)
+      .property("torque", &AgentState::torque);
+
+  em::class_<Agent>("Agent")
+      .smart_ptr<Agent::ptr>("Agent::ptr")
+      .property("config",
+                em::select_overload<const AgentConfiguration&() const>(
+                    &Agent::getConfig))
+      .property("sensorSuite", em::select_overload<const SensorSuite&() const>(
+                                   &Agent::getSensorSuite))
+      .function("getState", &Agent::getState)
+      .function("hasAction", &Agent::hasAction)
+      .function("act", &Agent::act);
+
+  em::class_<Observation>("Observation")
+      .smart_ptr_constructor("Observation", &Observation::create<>)
+      .function("getData", &Observation_getData);
+
+  em::class_<ObservationSpace>("ObservationSpace")
+      .smart_ptr_constructor("ObservationSpace", &ObservationSpace::create<>)
+      .property("dataType", &ObservationSpace::dataType)
+      .property("shape", &ObservationSpace::shape);
+
+  em::class_<SimulatorWithAgents>("Simulator")
+      .smart_ptr_constructor(
+          "Simulator",
+          &SimulatorWithAgents::create<const SimulatorConfiguration&>)
+      .function("seed", &SimulatorWithAgents::seed)
+      .function("reconfigure", &SimulatorWithAgents::reconfigure)
+      .function("reset", &SimulatorWithAgents::reset)
+      .function("getAgentObservations",
+                &SimulatorWithAgents::getAgentObservations)
+      .function("getAgentObservation",
+                &SimulatorWithAgents::getAgentObservation)
+      .function("getAgentObservationSpaces",
+                &Simulator_getAgentObservationSpaces)
+      .function("getAgentObservationSpace", &Simulator_getAgentObservationSpace)
+      .function("getAgent", &SimulatorWithAgents::getAgent)
+      .function("addAgent",
+                em::select_overload<Agent::ptr(const AgentConfiguration&)>(
+                    &SimulatorWithAgents::addAgent))
+      .function("addAgentToNode",
+                em::select_overload<Agent::ptr(const AgentConfiguration&,
+                                               scene::SceneNode&)>(
+                    &SimulatorWithAgents::addAgent));
+}
