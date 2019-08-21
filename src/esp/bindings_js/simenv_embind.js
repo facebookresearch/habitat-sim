bec7cdebbe4541ff4548f8ec4cd10a121655a556
Mandeep Singh Baines <msb@fb.com>
2019-08-21 15:38:56 -0700

    WebGL: initial documentation of js classes

--- src/esp/bindings_js/simenv_embind.js
+++ src/esp/bindings_js/simenv_embind.js
@@ -1,10 +1,22 @@
+/**
+ * SimEnv class
+ */
 class SimEnv {
-  // Uses embind simulator to act
+  // PUBLIC methods.
+
+  /**
+   * Create a simulator.
+   * @param {Object} config - simulator config
+   * @param {number} agentId - default agent id
+   */
   constructor(config, agentId) {
     this.sim = new Module.Simulator(config);;
     this.defaultAgentId = agentId;
   }
 
+  /**
+   * Resets the simulation.
+   */
   reset() {
     this.sim.reset();
     const agent = this.sim.getAgent(this.defaultAgentId);
@@ -13,10 +25,48 @@ class SimEnv {
     }
   }
 
+  /**
+   * Take one step in the simulation.
+   * @param {string} action - action to take
+   */
   step(action) {
     const agent = this.sim.getAgent(this.defaultAgentId);
     agent.act(action);
   }
+
+  /**
+   * Add an agent to the simulation.
+   * @param {Object} config - agent config
+   * @param {Object} state - initial state of agent
+   */
+  addAgent(config, state) {
+    if (state) {
+      this.initialAgentState = this.createAgentState(state);
+    }
+    return this.sim.addAgent(this.createAgentConfig(config));
+  }
+
+  /**
+   * Get the observation space for a given sensorId.
+   * @param {number} sensorId - id of sensor
+   * @returns {ObservationSpace} observation space of sensor
+   */
+  getObservationSpace(sensorId) {
+    return this.sim.getAgentObservationSpace(this.defaultAgentId, sensorId);
+  }
+
+  /**
+   * Get an observation from the given sensorId.
+   * @param {number} sensorId - id of sensor
+   * @returns {Observation} observation from sensor
+   */
+  getObservation(sensorId, buffer) {
+    const obs = new Module.Observation();
+    this.sim.getAgentObservation(0, sensorId, obs);
+    return obs;
+  }
+
+  // PRIVATE methods.
 
   createSensorSpec(config) {
     const converted = new Module.SensorSpec();
@@ -51,22 +101,4 @@ class SimEnv {
     }
     return converted;
   }
-
-  addAgent(config, state) {
-    if (state) {
-      this.initialAgentState = this.createAgentState(state);
-    }
-    return this.sim.addAgent(this.createAgentConfig(config));
-  }
-
-  getObservationSpace(sensorId) {
-    return this.sim.getAgentObservationSpace(this.defaultAgentId, sensorId);
-  }
-
-  getObservation(sensorId, buffer) {
-    const obs = new Module.Observation();
-    this.sim.getAgentObservation(0, sensorId, obs);
-    return obs;
-  }
-
 }
