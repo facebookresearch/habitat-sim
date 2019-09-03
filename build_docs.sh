36419ca5bde2d8a370b01294a02bebd1736a06e8
Vladimír Vondruš <mosra@centrum.cz>
2019-09-04 00:57:05 +0200

    Initial setup for C++ docs.

--- build_docs.sh
+++ build_docs.sh
@@ -3,4 +3,7 @@
 # Propagate failures properly
 set -e
 
+# Build C++ docs first so the Python docs can make use of the tag file
+./docs/m.css/documentation/doxygen.py docs/Doxyfile-mcss
+
 ./docs/m.css/documentation/python.py docs/conf.py
