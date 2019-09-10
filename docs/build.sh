0f852e40993f3ef46659b72de534027e3ab6db50
Vladimír Vondruš <mosra@centrum.cz>
2019-09-10 22:54:49 +0200

    Move the build_docs.sh helper into the docs directory.

--- docs/build.sh
+++ docs/build.sh
@@ -4,6 +4,6 @@
 set -e
 
 # Build C++ docs first so the Python docs can make use of the tag file
-./docs/m.css/documentation/doxygen.py docs/Doxyfile-mcss
+./m.css/documentation/doxygen.py docs/Doxyfile-mcss
 
-./docs/m.css/documentation/python.py docs/conf.py
+./m.css/documentation/python.py docs/conf.py
