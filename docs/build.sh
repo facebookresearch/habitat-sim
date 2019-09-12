f8228d0f4093485e9bc42b9d0651e990511884b9
Vladimír Vondruš <mosra@centrum.cz>
2019-09-12 23:10:26 +0200

    docs: make it possible to supply external m.css.

--- docs/build.sh
+++ docs/build.sh
@@ -3,7 +3,20 @@
 # Propagate failures properly
 set -e
 
-# Build C++ docs first so the Python docs can make use of the tag file
-./m.css/documentation/doxygen.py docs/Doxyfile-mcss
+if [[ $# -eq 1 ]]; then
+  export mcss_path=$1
+elif [[ $# -ne 0 ]]; then
+  echo "usage: ./build.sh [path-to-m.css]"
+  exit 1
+else
+  if [ ! -d m.css ]; then
+    echo "m.css submodule not found, please run git submodule update --init or specify the path to it"
+    exit 1
+  fi
+  mcss_path=./m.css
+fi
 
-./m.css/documentation/python.py docs/conf.py
+# Build C++ docs first so the Python docs can make use of the tag file
+$mcss_path/documentation/doxygen.py Doxyfile-mcss
+
+$mcss_path/documentation/python.py conf.py
