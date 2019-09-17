433511396606328134e5297b080862fa5f809362
Vladimír Vondruš <mosra@centrum.cz>
2019-09-17 20:15:34 +0200

    Docs: new light theme and various other updates (#209)
    * docs: dang, I forgot to clean this up.
    
    * docs: relative paths actually *do* work.
    
    Not sure why I thought they aren't.
    
    * Cross-link to the quaternion package as well.
    
    For some reason I grabbed a totally broken inventory file at first. This
    one is less broken but the names get inspected weird so it needs to be
    fixed up after.
    
    * Move the build_docs.sh helper into the docs directory.
    
    * Updated m.css submodule.
    
    * docs: capitalize project name.
    
    * docs: move the generated tagfile outside of the doxygen output dir.
    
    Somehow it doesn't generated when it's there. Or something.
    
    * docs: make it possible to supply external m.css.
    
    * docs: switch to a custom light theme.
    
    The resulting CSS is generated by the build.sh script and added to
    .gitignore -- changes should be made to theme.css / pygments-pastie.css
    instead.
    
    * docs: link to tutorials from the main page.
    
    Similarly to how API does that.
    
    * docs: update magnum bindings doc inventory.
    
    The URL scheme got stabilized there, so all links are different now.
    
    * docs: less confusing names in the navbar.
    
    * docs: make main project URL just a /.
    
    So it's "relocatable", i.e. when running the whole site on localhost,
    the navbar links goes to the main site instead of out into the wild.

---
+++ docs/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Propagate failures properly
+set -e
+
+if [[ $# -eq 1 ]]; then
+  export mcss_path=$1
+elif [[ $# -ne 0 ]]; then
+  echo "usage: ./build.sh [path-to-m.css]"
+  exit 1
+else
+  if [ ! -d m.css ]; then
+    echo "m.css submodule not found, please run git submodule update --init or specify the path to it"
+    exit 1
+  fi
+  mcss_path=./m.css
+fi
+
+# Regenerate the compiled CSS file
+$mcss_path/css/postprocess.py \
+  theme.css \
+  $mcss_path/css/m-grid.css \
+  $mcss_path/css/m-components.css \
+  $mcss_path/css/m-layout.css \
+  pygments-pastie.css \
+  $mcss_path/css/pygments-console.css \
+  $mcss_path/css/m-documentation.css \
+  -o theme.compiled.css
+
+# Build C++ docs first so the Python docs can make use of the tag file
+$mcss_path/documentation/doxygen.py Doxyfile-mcss
+
+$mcss_path/documentation/python.py conf.py
