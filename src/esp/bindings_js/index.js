196716cd47cea65b4e47717d1edffda2995919d2
Amanpreet Singh <asg@fb.com>
2019-09-19 23:34:06 -0700

    Initial WebVR prototype support
    - Add VRDemo class which will initialize demo for WebVR
    - Add FPS for VRDemo
    - Call VRDemo when vrEnabled window variable is set
    - Create a separate HTML file for WebVR and build it also in webpack

--- src/esp/bindings_js/index.js
+++ src/esp/bindings_js/index.js
@@ -1,12 +1,22 @@
 /*global Module */
 
 import WebDemo from "./modules/web_demo";
+import VRDemo from "./modules/vr_demo";
 import "./bindings.css";
 
 Module["onRuntimeInitialized"] = function() {
   console.log("hsim_bindings initialized");
 
+  let demo;
+  if (window.vrEnabled) {
+    if (navigator && navigator.getVRDisplays) {
+      console.log("Web VR is supported");
+      demo = new VRDemo();
+    }
+  }
 
-  const demo = new WebDemo();
+  if (!demo) {
+    demo = new WebDemo();
+  }
   demo.display();
 };
