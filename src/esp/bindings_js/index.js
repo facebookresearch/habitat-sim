a56cbbb6bb02c4986544447fa1754b3593dbe406
Mandeep Singh Baines <msb@fb.com>
2019-09-23 14:34:11 -0700

    [esp/bindings_js] load scene files bases on URL parameters
    Instead of using a large .data file, we preload the scene files
    into the virtual filesystem based on URL scene parameter. This is
    a workaround until Corrade PR#39 is ready.
    
    This should speed up startup since we now only load the files we
    need.

--- src/esp/bindings_js/index.js
+++ src/esp/bindings_js/index.js
@@ -1,12 +1,25 @@
-/*global Module */
+/* global FS, Module */
 
 import WebDemo from "./modules/web_demo";
 import VRDemo from "./modules/vr_demo";
 import "./bindings.css";
 
-Module["onRuntimeInitialized"] = function() {
-  console.log("hsim_bindings initialized");
+Module.preRun.push(() => {
+  let scene = "skokloster-castle.glb";
+  for (let arg of window.location.search.substr(1).split("&")) {
+    let [key, value] = arg.split("=");
+    if (key === "scene" && value) {
+      scene = value;
+    }
+  }
+  FS.createPreloadedFile("/", scene, scene, true, false);
+  Module.scene = scene;
+  const navmesh = scene.substr(0, scene.lastIndexOf(".")) + ".navmesh";
+  FS.createPreloadedFile("/", navmesh, navmesh, true, false);
+});
 
+Module.onRuntimeInitialized = () => {
+  console.log("hsim_bindings initialized");
   let demo;
   if (window.vrEnabled) {
     if (navigator && navigator.getVRDisplays) {
