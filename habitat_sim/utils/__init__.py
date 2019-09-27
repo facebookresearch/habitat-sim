3b228159fdf26d642d86d4247156f43b558235f5
Vladimír Vondruš <mosra@centrum.cz>
2019-09-27 20:27:30 +0200

    docs: due to the new __all__, utils.common got lost from the docs again.

--- habitat_sim/utils/__init__.py
+++ habitat_sim/utils/__init__.py
@@ -4,9 +4,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Added for backward compatibility with Habitat-API
+# quat_from_angle_axis and quat_rotate_vector imports
+# added for backward compatibility with Habitat-API
 # TODO @maksymets: remove after habitat-api/examples/new_actions.py will be
 # fixed
+
+
+from habitat_sim.utils import common
 from habitat_sim.utils.common import quat_from_angle_axis, quat_rotate_vector
 
-__all__ = ["quat_from_angle_axis", "quat_rotate_vector"]
+__all__ = ["quat_from_angle_axis", "quat_rotate_vector", "common"]
