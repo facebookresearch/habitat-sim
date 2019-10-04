197d59e11ffae17dde58b2c682e084b935547275
Erik Wijmans <etw@gatech.edu>
2019-10-03 21:00:00 -0400

    Add test

---
+++ tests/test_snap_point.py
@@ -0,0 +1,32 @@
+import math
+
+import hypothesis
+import hypothesis.strategies as st
+import numpy as np
+import pytest
+
+import habitat_sim
+
+
+@pytest.fixture(scope="function")
+def test_data():
+    pf = habitat_sim.PathFinder()
+    pf.load_nav_mesh(
+        "data/scene_datasets/habitat-test-scenes/skokloster-castle.navmesh"
+    )
+
+    return pf, pf.get_random_navigable_point()
+
+
+@hypothesis.given(nx=st.floats(-20, 20), ny=st.floats(-5, 5), nz=st.floats(-20, 20))
+@hypothesis.settings(max_examples=int(1e3))
+def test_snap_point(nx, ny, nz, test_data):
+    pf, start_pt = test_data
+
+    n = np.array([nx, ny, nz])
+    pt = start_pt + n
+
+    proj_pt = pf.snap_point(pt)
+    hypothesis.assume(not math.isnan(proj_pt[0]))
+
+    assert pf.is_navigable(proj_pt), "{} -> {} not navigable!".format(pt, proj_pt)
