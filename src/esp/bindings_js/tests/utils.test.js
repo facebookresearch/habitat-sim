bcfe17ed264bc922206970bcbbb240d5382c3cc1
Amanpreet Singh <asg@fb.com>
2019-10-07 13:05:04 -0700

    [tests/js] Add testing support for JS through Jest
    - Running `npm test` will run JS tests using Jest
    - Tests live inside tests folder in bindings_js and are suffixed with
    .test.js extension

---
+++ src/esp/bindings_js/tests/utils.test.js
@@ -0,0 +1,23 @@
+import { throttle } from "../modules/utils";
+
+test("throttle should work properly", () => {
+  let count = 0;
+  let interval;
+  const startTime = Date.now();
+  function incrementCounter(resolve) {
+    if (count === 5) {
+      resolve(count);
+    } else {
+      count += 1;
+    }
+  }
+
+  return new Promise((resolve) => {
+    interval = window.setInterval(throttle(() => incrementCounter(resolve), 500), 50);
+  }).then((count) => {
+    window.clearInterval(interval);
+    expect(count).toEqual(5);
+    expect(Date.now() - startTime).toBeGreaterThan(2500);
+    return count;
+  })
+});
