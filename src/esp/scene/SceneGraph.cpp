4422753aa6e1426f74c5dfb829b21e40ae09830f
Yili Zhao <yili@fb.com>
2019-10-08 17:10:56 -0700

    format

--- src/esp/scene/SceneGraph.cpp
+++ src/esp/scene/SceneGraph.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
-
-#include <Magnum/Math/Algorithms/GramSchmidt.h>
 #include "SceneGraph.h"
 
 namespace esp {
