e4afe2c9377f49882dee50185d4437d0477b0554
Erik Wijmans <erik.wijmans@oculus.com>
2019-11-07 13:04:41 -0500

    Redwood noisy depth (#189)
    Add depth noise model!

--- src/esp/bindings/bindings.cpp
+++ src/esp/bindings/bindings.cpp
@@ -23,6 +23,10 @@ using namespace py::literals;
 #include "esp/sensor/PinholeCamera.h"
 #include "esp/sensor/Sensor.h"
 
+#ifdef ESP_BUILD_WITH_CUDA
+#include "esp/sensor/RedwoodNoiseModel.h"
+#endif
+
 #include <Magnum/ImageView.h>
 #include <Magnum/Python.h>
 #include <Magnum/SceneGraph/Python.h>
@@ -354,8 +358,7 @@ PYBIND11_MODULE(habitat_sim_bindings, m) {
       .value("DEPTH", SensorType::DEPTH)
       .value("SEMANTIC", SensorType::SEMANTIC);
 
-  // ==== SensorSpec ====
-  py::class_<SensorSpec, SensorSpec::ptr>(m, "SensorSpec")
+  py::class_<SensorSpec, SensorSpec::ptr>(m, "SensorSpec", py::dynamic_attr())
       .def(py::init(&SensorSpec::create<>))
       .def_readwrite("uuid", &SensorSpec::uuid)
       .def_readwrite("sensor_type", &SensorSpec::sensorType)
@@ -368,6 +371,19 @@ PYBIND11_MODULE(habitat_sim_bindings, m) {
       .def_readwrite("encoding", &SensorSpec::encoding)
       .def_readwrite("gpu2gpu_transfer", &SensorSpec::gpu2gpuTransfer)
       .def_readwrite("observation_space", &SensorSpec::observationSpace)
+      .def_readwrite("noise_model", &SensorSpec::noiseModel)
+      .def_property(
+          "noise_model_kwargs",
+          [](SensorSpec& self) -> py::dict {
+            py::handle handle = py::cast(self);
+            if (!py::hasattr(handle, "__noise_model_kwargs")) {
+              py::setattr(handle, "__noise_model_kwargs", py::dict());
+            }
+            return py::getattr(handle, "__noise_model_kwargs");
+          },
+          [](SensorSpec& self, py::dict v) {
+            py::setattr(py::cast(self), "__noise_model_kwargs", v);
+          })
       .def("__eq__",
            [](const SensorSpec& self, const SensorSpec& other) -> bool {
              return self == other;
@@ -442,6 +458,20 @@ PYBIND11_MODULE(habitat_sim_bindings, m) {
       .def_property_readonly("object", nodeGetter<Sensor>, "Alias to node")
       .def_property_readonly("framebuffer_size", &Sensor::framebufferSize)
       .def_property_readonly("render_target", &Sensor::renderTarget);
+
+#ifdef ESP_BUILD_WITH_CUDA
+  py::class_<RedwoodNoiseModelGPUImpl, RedwoodNoiseModelGPUImpl::uptr>(
+      m, "RedwoodNoiseModelGPUImpl")
+      .def(py::init(&RedwoodNoiseModelGPUImpl::create_unique<
+                    const Eigen::Ref<const Eigen::RowMatrixXf>&, int, float>))
+      .def("simulate_from_cpu", &RedwoodNoiseModelGPUImpl::simulateFromCPU)
+      .def("simulate_from_gpu", [](RedwoodNoiseModelGPUImpl& self,
+                                   std::size_t devDepth, const int rows,
+                                   const int cols, std::size_t devNoisyDepth) {
+        self.simulateFromGPU(reinterpret_cast<const float*>(devDepth), rows,
+                             cols, reinterpret_cast<float*>(devNoisyDepth));
+      });
+#endif
 
   // ==== PinholeCamera (subclass of Sensor) ====
   py::class_<sensor::PinholeCamera,
