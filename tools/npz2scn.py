c522a1cf7b03ad36a68cc5a8c4945c14270b2c85
Mandeep Singh Baines <msb@fb.com>
2019-12-18 14:05:29 -0800

    [esp/scene] initial support for Gibson semantic scene
    This change include a Gibson semantic scene generator and parser.

---
+++ tools/npz2scn.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# npz2scn - tool for extracting semantic scene information from 3dscenegraph
+#           dataset (https://3dscenegraph.stanford.edu/)
+
+import argparse
+import json
+import sys
+
+import numpy as np
+
+
+# Convert ndarrays to python lists so that we can serialize.
+# Tranform coordinates by rotating Y-axis to Z-axis
+def fixCoords(entry):
+    size = entry["size"].tolist()
+    size[1], size[2] = size[2], size[1]
+    entry["size"] = size
+    loc = entry["location"].tolist()
+    loc[1], loc[2] = -loc[2], loc[1]
+    entry["location"] = loc
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract object IDs from npz.")
+    parser.add_argument("npz_path", metavar="foo.npz", help="path to .npz file to load")
+    parser.add_argument(
+        "scn_path", metavar="foo.scn", help="path to output file to write"
+    )
+    args = parser.parse_args()
+    data = np.load(args.npz_path, allow_pickle=True)["output"].item()
+    objects = data["object"]
+    for object in objects.values():
+        fixCoords(object)
+    rooms = data["room"]
+    for room in rooms.values():
+        fixCoords(room)
+    output = dict()
+    output["objects"] = list(objects.values())
+    output["rooms"] = list(rooms.values())
+    with open(args.scn_path, "w") as f:
+        x = f.write(json.dumps(output, indent=2))
+        print("wrote %d bytes" % x)
+
+
+if __name__ == "__main__":
+    main()
