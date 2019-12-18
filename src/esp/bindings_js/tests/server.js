01e68af5edd96930e3f8c82e9c4032d85840b1a0
Amanpreet Singh <asg@fb.com>
2019-12-17 16:03:06 -0800

    [tests] Setup JS visual regression testing
    - Use jest-image-snapshot to generate and compare snapshots
    - Use puppeteer to launch chromium browser in headless mode
    - Use finalhandler and static-serve to finalize requests and serve
    static content easily
    - Update package.json with required packages

---
+++ src/esp/bindings_js/tests/server.js
@@ -0,0 +1,62 @@
+import http from "http";
+import finalhandler from "finalhandler";
+import serveStatic from "serve-static";
+import puppeteer from "puppeteer";
+
+export async function createServer() {
+  const serve = serveStatic("./");
+  const server = http.createServer(function(req, res) {
+    var done = finalhandler(req, res);
+    serve(req, res, done);
+  });
+
+  await new Promise((resolve, reject) => {
+    const startServer = () => {
+      server.once("error", e => {
+        if (e.code === "EADDRINUSE") {
+          server.close(startServer);
+        }
+      });
+      server.listen(4004, err => (err ? reject(err) : resolve()));
+    };
+    startServer();
+  });
+  return server;
+}
+
+export async function getBrowserAndPage(url) {
+  const browser = await puppeteer.launch({
+    args: ["--disable-lcd-text"],
+    defaultViewport: { width: 1920, height: 1080 }
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: "load" });
+
+  return { browser, page };
+}
+
+export async function getServerAndURL(path) {
+  const server = await createServer();
+  const address = server.address();
+  const port = address.port;
+  const url = `http://localhost:${port}/${path}`;
+  return {
+    server,
+    url
+  };
+}
+
+export async function closeBrowserAndServer(browser, server) {
+  browser.close();
+  await new Promise(resolve => server.close(resolve));
+}
+
+export async function generateImage(path) {
+  const { server, url } = await getServerAndURL(path);
+  const { browser, page } = await getBrowserAndPage(url);
+  const screenshot = await page.screenshot();
+  closeBrowserAndServer(browser, server);
+  return screenshot;
+}
---
+++ src/esp/bindings_js/tests/viewer_demo.visual.test.js
@@ -0,0 +1,15 @@
+import { getServerAndURL, getBrowserAndPage, closeBrowserAndServer } from "./server.js";
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+
+expect.extend({ toMatchImageSnapshot });
+
+test("viewer rendering should match the snapshot", async () => {
+  jest.setTimeout(10000);
+  const { server, url } = await getServerAndURL("build_js/esp/bindings_js/viewer.html?useDefaultEpisode=true");
+  const { browser, page } = await getBrowserAndPage(url);
+  await page.waitForFunction('document.querySelector("#status").style.color === "white"');
+  const screenshot = await page.screenshot();
+
+  closeBrowserAndServer(browser, server);
+  expect(screenshot).toMatchImageSnapshot();
+});
