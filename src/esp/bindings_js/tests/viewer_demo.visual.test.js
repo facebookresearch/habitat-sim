28b42a963ccb610c765b6cdd6da53da657fc044b
Amanpreet Singh <asg@fb.com>
2019-12-20 12:06:50 -0800

    Add headers

--- src/esp/bindings_js/tests/viewer_demo.visual.test.js
+++ src/esp/bindings_js/tests/viewer_demo.visual.test.js
@@ -1,3 +1,7 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import {
   getServerAndURL,
   getBrowserAndPage,
@@ -8,13 +12,13 @@ import { toMatchImageSnapshot } from "jest-image-snapshot";
 expect.extend({ toMatchImageSnapshot });
 
 test("viewer rendering should match the snapshot", async () => {
-  jest.setTimeout(120000);
+  jest.setTimeout(12000);
   const { server, url } = await getServerAndURL(
     "build_js/esp/bindings_js/viewer.html?scene=skokloster-castle.glb&useDefaultEpisode=true"
   );
   const { browser, page } = await getBrowserAndPage(url);
 
-  page.setDefaultTimeout(120000);
+  page.setDefaultTimeout(12000);
 
   await page.waitForFunction(
     'document.querySelector("#status").style.color === "white"'
