a621f6a2ac918483d60c91bf53df994272964ef3
Amanpreet Singh <asg@fb.com>
2019-12-28 01:44:37 -0800

    [redesign] Initial class design for sensor api

---
+++ src/esp/sensor/Base.h
@@ -0,0 +1,29 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#pragma once
+
+#include "esp/scene/SceneNode.h"
+#include "esp/io/json.h"
+
+#include <string>
+
+namespace esp {
+namespace sensor {
+class BaseSensor : public Magnum::SceneGraph::AbstractFeature3D {
+  public:
+    explicit BaseSensor(scene::SceneNode& node, const esp::io::JsonDocument spec);
+    std::string getType();
+    std::string getUUID();
+    std::vector<char> getObservation();
+    std::vector<int> getObservationShape();
+    std::string getObservationDataType();
+};
+
+class SensorFactory {
+  public:
+    static BaseSensor create(scene::SceneNode& node, const esp::io::JsonDocument spec);
+}
+} // namespace sensor
+} // namespace esp
