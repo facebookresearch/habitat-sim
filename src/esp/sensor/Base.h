9210636bf495e365c72a3ee7f32146e330b11e02
Amanpreet Singh <asg@fb.com>
2019-12-28 10:38:01 -0800

    Fix lint

--- src/esp/sensor/Base.h
+++ src/esp/sensor/Base.h
@@ -4,26 +4,27 @@
 
 #pragma once
 
-#include "esp/scene/SceneNode.h"
 #include "esp/io/json.h"
+#include "esp/scene/SceneNode.h"
 
 #include <string>
 
 namespace esp {
 namespace sensor {
 class BaseSensor : public Magnum::SceneGraph::AbstractFeature3D {
-  public:
-    explicit BaseSensor(scene::SceneNode& node, const esp::io::JsonDocument spec);
-    std::string getType();
-    std::string getUUID();
-    std::vector<char> getObservation();
-    std::vector<int> getObservationShape();
-    std::string getObservationDataType();
+ public:
+  explicit BaseSensor(scene::SceneNode& node, const esp::io::JsonDocument spec);
+  std::string getType();
+  std::string getUUID();
+  std::vector<char> getObservation();
+  std::vector<int> getObservationShape();
+  std::string getObservationDataType();
 };
 
 class SensorFactory {
-  public:
-    static BaseSensor create(scene::SceneNode& node, const esp::io::JsonDocument spec);
-}
-} // namespace sensor
-} // namespace esp
+ public:
+  static BaseSensor create(scene::SceneNode& node,
+                           const esp::io::JsonDocument spec);
+};
+}  // namespace sensor
+}  // namespace esp
