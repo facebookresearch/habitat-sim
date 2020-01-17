151143df42e33f766cba5cf5ba5c178500bf4e9f
Michael Piseno <mpiseno@fb.com>
2020-01-17 13:22:58 -0800

    modified test to take sim as an argument

--- tests/test_random_seed.py
+++ tests/test_random_seed.py
@@ -1,25 +1,8 @@
-import os
-import sys
-
 import examples.settings
 import habitat_sim
 
-cwd = os.getcwd()
-if cwd not in sys.path:
-    sys.path.append(cwd)
 
-
-def test_random_seed():
-    # put the path to your test scene here. You MUST define a scene instead of
-    # just the default otherwise this will segfault
-    test_scene = "./data/scene_datasets/habitat-test-scenes/van-gogh-room.glb"
-    cfg_settings = examples.settings.default_sim_settings.copy()
-
-    # keyword "NONE" initializes a scene with no scene mesh
-    cfg_settings["scene"] = test_scene
-    hab_cfg = examples.settings.make_cfg(cfg_settings)
-    sim = habitat_sim.Simulator(hab_cfg)
-
+def test_random_seed(sim):
     # Test that the same seed gives the same point
     sim.seed(1)
     point1 = sim.pathfinder.get_random_navigable_point()
