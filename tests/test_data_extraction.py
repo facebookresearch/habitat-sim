8f6b5b58a8fde287ac391a8a7c024c95037243a2
Michael Piseno <mpiseno@fb.com>
2020-01-31 17:09:20 -0800

    formatting and added pytorch as dependency

--- tests/test_data_extraction.py
+++ tests/test_data_extraction.py
@@ -1,7 +1,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
