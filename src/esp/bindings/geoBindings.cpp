599bebb47371e58a1ac74ad6e43f8a272ce4888b
Aaron Gokaslan <agokaslan@fb.com>
2020-02-20 14:18:51 -0800

    Extend bindings for OBB and misc fixes. (#493)
    * OBB fixes and extends. Also added error val to sample navigable point
    * Make getRandomNavigablePoint failure value constant.
    
    Co-Authored-By: Erik Wijmans <etw@gatech.edu>
    
    Co-authored-by: Erik Wijmans <ewijmans2@gmail.com>

--- src/esp/bindings/geoBindings.cpp
+++ src/esp/bindings/geoBindings.cpp
@@ -24,11 +24,24 @@ void initGeoBindings(py::module& m) {
 
   // ==== OBB ====
   py::class_<OBB>(m, "OBB")
+      .def(py::init<box3f&>())
+      .def("contains", &OBB::contains)
+      .def("closest_point", &OBB::closestPoint)
+      .def("distance", &OBB::distance)
+      .def("to_aabb", &OBB::toAABB)
       .def_property_readonly("center", &OBB::center)
       .def_property_readonly("sizes", &OBB::sizes)
       .def_property_readonly("half_extents", &OBB::halfExtents)
       .def_property_readonly(
-          "rotation", [](const OBB& self) { return self.rotation().coeffs(); });
+          "rotation", [](const OBB& self) { return self.rotation().coeffs(); })
+      .def_property_readonly(
+          "local_to_world",
+          [](const OBB& self) { return self.localToWorld().matrix(); })
+      .def_property_readonly("world_to_local", [](const OBB& self) {
+        return self.worldToLocal().matrix();
+      });
+
+  geo.def("compute_gravity_aligned_MOBB", &geo::computeGravityAlignedMOBB);
 }
 
 }  // namespace geo
