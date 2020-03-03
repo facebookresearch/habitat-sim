5304310ebbd6d0cf2036b48e364e16c9a3f817fb
Matthew May <mmay51598@gmail.com>
2020-03-03 11:29:03 -0800

    Fix most compiler warnings (#512)
    * fix-compiler-warnings
    
    * remove unneeded imports
    
    * use CORRADE_UNUSED

--- src/esp/physics/bullet/BulletPhysicsManager.h
+++ src/esp/physics/bullet/BulletPhysicsManager.h
@@ -62,9 +62,9 @@ class BulletPhysicsManager : public PhysicsManager {
    * @param physicsManagerAttributes A structure containing values for physical
    * parameters necessary to initialize the physical scene and simulator.
    */
-  bool initPhysics(
-      scene::SceneNode* node,
-      const assets::PhysicsManagerAttributes& physicsManagerAttributes);
+  bool initPhysics(scene::SceneNode* node,
+                   const assets::PhysicsManagerAttributes&
+                       physicsManagerAttributes) override;
 
   //============ Object/Scene Instantiation =============
 
@@ -80,8 +80,9 @@ class BulletPhysicsManager : public PhysicsManager {
    * @param meshGroup collision meshs for the scene.
    * @return true if successful and false otherwise
    */
-  bool addScene(const assets::PhysicsSceneAttributes& physicsSceneAttributes,
-                const std::vector<assets::CollisionMeshData>& meshGroup);
+  bool addScene(
+      const assets::PhysicsSceneAttributes& physicsSceneAttributes,
+      const std::vector<assets::CollisionMeshData>& meshGroup) override;
 
   virtual int addObject(
       const int objectLibIndex,
@@ -96,17 +97,17 @@ class BulletPhysicsManager : public PhysicsManager {
    * btDiscreteDynamicsWorld::stepSimulation.
    * @param dt The desired amount of time to advance the physical world.
    */
-  void stepPhysics(double dt);
+  void stepPhysics(double dt) override;
 
   /** @brief Set the gravity of the physical world.
    * @param gravity The desired gravity force of the physical world.
    */
-  void setGravity(const Magnum::Vector3& gravity);
+  void setGravity(const Magnum::Vector3& gravity) override;
 
   /** @brief Get the current gravity in the physical world.
    * @return The current gravity vector in the physical world.
    */
-  Magnum::Vector3 getGravity() const;
+  Magnum::Vector3 getGravity() const override;
 
   //============ Interacting with objects =============
   // NOTE: engine specifics for interaction are handled by the objects
@@ -120,7 +121,7 @@ class BulletPhysicsManager : public PhysicsManager {
    * PhysicsManager::existingObjects_.
    * @param  margin The desired collision margin for the object.
    */
-  void setMargin(const int physObjectID, const double margin);
+  void setMargin(const int physObjectID, const double margin) override;
 
   /** @brief Set the friction coefficient of the scene collision geometry. See
    * @ref staticSceneObject_. See @ref
@@ -128,14 +129,15 @@ class BulletPhysicsManager : public PhysicsManager {
    * @param frictionCoefficient The scalar friction coefficient of the scene
    * geometry.
    */
-  void setSceneFrictionCoefficient(const double frictionCoefficient);
+  void setSceneFrictionCoefficient(const double frictionCoefficient) override;
 
   /** @brief Set the coefficient of restitution for the scene collision
    * geometry. See @ref staticSceneObject_. See @ref
    * BulletRigidObject::setRestitutionCoefficient.
    * @param restitutionCoefficient The scalar coefficient of restitution to set.
    */
-  void setSceneRestitutionCoefficient(const double restitutionCoefficient);
+  void setSceneRestitutionCoefficient(
+      const double restitutionCoefficient) override;
 
   //============ Bullet-specific Object Getter functions =============
 
@@ -146,14 +148,14 @@ class BulletPhysicsManager : public PhysicsManager {
    * @return The scalar collision margin of the object or @ref
    * esp::PHYSICS_ATTR_UNDEFINED if failed..
    */
-  double getMargin(const int physObjectID) const;
+  double getMargin(const int physObjectID) const override;
 
   /** @brief Get the current friction coefficient of the scene collision
    * geometry. See @ref staticSceneObject_ and @ref
    * BulletRigidObject::getFrictionCoefficient.
    * @return The scalar friction coefficient of the scene geometry.
    */
-  double getSceneFrictionCoefficient() const;
+  double getSceneFrictionCoefficient() const override;
 
   /** @brief Get the current coefficient of restitution for the scene collision
    * geometry. This determines the ratio of initial to final relative velocity
@@ -161,7 +163,7 @@ class BulletPhysicsManager : public PhysicsManager {
    * BulletRigidObject::getRestitutionCoefficient.
    * @return The scalar coefficient of restitution for the scene geometry.
    */
-  double getSceneRestitutionCoefficient() const;
+  double getSceneRestitutionCoefficient() const override;
 
   /**
    * @brief Query the Aabb from bullet physics for the root compound shape of a
@@ -196,7 +198,7 @@ class BulletPhysicsManager : public PhysicsManager {
    * @return Whether or not the object is in contact with any other collision
    * enabled objects.
    */
-  bool contactTest(const int physObjectID);
+  bool contactTest(const int physObjectID) override;
 
  protected:
   btDbvtBroadphase bBroadphase_;
@@ -218,7 +220,7 @@ class BulletPhysicsManager : public PhysicsManager {
    * Magnum::MeshPrimitive::Triangles.
    * @return true if valid, false otherwise.
    */
-  bool isMeshPrimitiveValid(const assets::CollisionMeshData& meshData);
+  bool isMeshPrimitiveValid(const assets::CollisionMeshData& meshData) override;
 
   /** @brief Create and initialize an @ref RigidObject and assign it an ID. See
    * @ref allocateObjectID and @ref BulletRigidObject::initializeObject.
