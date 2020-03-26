db017bf478b2750aa91224e7337a7e1be13f8bdc
Alexander Clegg <alexanderwclegg@gmail.com>
2020-03-26 14:18:10 -0700

    Update src/esp/assets/ResourceManager.cpp
    Co-Authored-By: Vladimír Vondruš <mosra@centrum.cz>

--- src/esp/assets/ResourceManager.cpp
+++ src/esp/assets/ResourceManager.cpp
@@ -788,7 +788,7 @@ int ResourceManager::getObjectID(const std::string& configFile) {
 }
 
 std::string ResourceManager::getObjectConfig(const int objectID) {
-  if (physicsObjectConfigList_.size() <= objectID) {
+  if (physicsObjectConfigList_.size() <= std::size_t(objectID)) {
     Corrade::Utility::Debug() << "ResourceManager::getObjectConfig - Aborting. "
                                  "No template with index "
                               << objectID;
