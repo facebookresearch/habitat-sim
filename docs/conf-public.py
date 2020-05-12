742f5f945554790acb5e7f1004253ea2148f6c6c
Vladimír Vondruš <mosra@centrum.cz>
2020-05-12 07:56:15 +0200

    doc: don't alias public and local tagfile location.
    Yet another case of Doxygen unable to create a directory, however this
    time it fails silently. Sigh.
    
    This fails when generating public docs and the build/docs/ directory
    doesn't exist. The m.css scripts creates build/docs-public/ in that
    case, so tell Doxygen to put everything there, including the tagfile.

--- docs/conf-public.py
+++ docs/conf-public.py
@@ -23,3 +23,6 @@ HTML_HEADER = """<!-- Global site tag (gtag.js) - Google Analytics -->
 SEARCH_DOWNLOAD_BINARY = "searchdata-v1.bin"
 SEARCH_BASE_URL = "https://aihabitat.org/docs/habitat-sim/"
 SEARCH_EXTERNAL_URL = "https://google.com/search?q=site:aihabitat.org+{query}"
+
+assert M_DOX_TAGFILES[2][0] == "../build/docs/habitat-cpp.tag"
+M_DOX_TAGFILES[2] = ("../build/docs-public/habitat-cpp.tag", "../habitat-cpp/", [], ["m-doc-external"])
