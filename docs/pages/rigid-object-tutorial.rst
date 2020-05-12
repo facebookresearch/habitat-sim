cdf0d61fea450917e3125e52e3d0bf71c3536c4c
Alexander William Clegg <alexclegg@fb.com>
2020-05-11 17:15:31 -0700

    More tutorial changes

--- docs/pages/rigid-object-tutorial.rst
+++ docs/pages/rigid-object-tutorial.rst
@@ -3,6 +3,7 @@
     habitat_sim.sim
     habitat_sim.agent
     habitat_sim.attributes
+    habitat_sim.scene
 
 Interactive Rigid Objects
 #########################
@@ -126,15 +127,42 @@ Velocities can also be specified in the local space of the object to easily appl
 `Embodied Agents`_
 ==================
 
-For this tutorial section, you will need to download the `merged locobot asset`_ and unzip into path/to/habitat-sim/data/objects/
+For this tutorial section, you will need to download the `merged locobot asset`_ and extract it into path/to/habitat-sim/data/objects/
 
 .. _merged locobot asset: http://dl.fbaipublicfiles.com/habitat/locobot_merged.zip
 
-This tutorial has covered adding objects to the world and manipulating them by setting positions, velocity, forces, and torques.
+Previous stages of this tutorial have covered adding objects to the world and manipulating them by setting positions, velocity, forces, and torques.
+In all of these examples, the agent has been a passive onlooker observing the scene.
+However, the agent can also be attached to a simulated object for embodiement and control.
+This can be done by passing the :ref:`Agent`'s scene node to the :ref:`Simulator.add_object` function.
 
+In this example, the agent is embodied by a rigid robot asset and the :ref:`habitat_sim.physics.VelocityControl` structure is used to control the robot's actions.
+
+.. include:: ../../examples/tutorials/rigid_object_tutorial.py
+    :code: py
+    :start-after: # [embodied_agent]
+    :end-before: # [/embodied_agent]
+
+.. image:: images/rigid-object-tutorial-images/robot_control.gif
+    :width: 20em
 
 `Feature Detail Review`_
 ========================
+
+Adding/Removing Objects
+***********************
+
+Objects can be instanced from templates (i.e. :ref:`PhysicsObjectAttributes`) into the scene by template id with :ref:`Simulator.add_object` or by template string key with :ref:`Simulator.add_object_by_handle`.
+These functions return a unique id which can be used to refer to the object instance. In the case of errors in construction, -1 is returned.
+
+By default, a new :ref:`SceneNode` will be created when an object is instanced. However, the object can be attached to an existing :ref:`SceneNode` (e.g. that of the :ref:`Agent`) if provided. This is demonstrated in `Embodied Agents`_.
+
+Object instances can be removed by id with :ref:`Simulator.remove_object`. Optionally, the object's :ref:`SceneNode` can be left behind in the :ref:`SceneGraph` when it is removed (e.g. to prevent deletion of an embodied :ref:`Agent`'s :ref:`SceneNode`).
+
+:ref:`Simulator.get_existing_object_ids` will return a list of unique object ids for all objects instanced in the scene.
+
+MotionType
+**********
 
 Objects can be configured to fill different roles in a simulated scene by assigning a :ref:`habitat_sim.physics.MotionType`:
 
@@ -155,6 +183,9 @@ Objects can be configured to fill different roles in a simulated scene by assign
 
   Static object states are not expected to change and cannot be affected by scene dynamics or programmatic state setters.
 
+VelocityControl
+***************
+
 Each object's :ref:`habitat_sim.physics.VelocityControl` structure provides a simple interface for setting up continuous velocity control of the object in either the global or local coordinate frame.
 This can be queried from the simulator with :ref:`Simulator.get_object_velocity_control`.
 
@@ -162,3 +193,4 @@ For :ref:`habitat_sim.physics.MotionType.KINEMATIC` objects, velocity control wi
 
 For :ref:`habitat_sim.physics.MotionType.DYNAMIC` object, velocity control will set the initial velocity of the object before simualting.
 In this case, velocity will be more accurate with smaller timestep requests to :ref:`Simulator.step_physics`.
+Note that dynamics such as forces, collisions, and gravity will affect these objects, but expect extreme damping as velocities are being manually set before each timestep when controlled.
--- examples/tutorials/rigid_object_tutorial.py
+++ examples/tutorials/rigid_object_tutorial.py
@@ -72,7 +72,7 @@ def make_configuration():
 
     # sensor configurations
     # Note: all sensors must have the same resolution
-    # setup rgb and depth sensors with slight offset
+    # setup 2 rgb sensors for 1st and 3rd person views
     camera_resolution = [540, 720]
     sensors = {
         "rgba_camera_1stperson": {
@@ -107,7 +107,7 @@ def make_configuration():
 
 
 def simulate(sim, dt=1.0, get_frames=True):
-    # simulate dt seconds to the nearest fixed timestep
+    # simulate dt seconds at 60Hz to the nearest fixed timestep
     print("Simulating " + str(dt) + " world seconds.")
     observations = []
     start_time = sim.get_world_time()
@@ -137,184 +137,178 @@ def main(make_video=True, show_video=True):
 
     # [basics]
 
-    if False:
-        # load some object templates from configuration files
-        sphere_template_id = sim.load_object_configs(
-            str(os.path.join(data_path, "test_assets/objects/sphere"))
-        )[0]
+    # load some object templates from configuration files
+    sphere_template_id = sim.load_object_configs(
+        str(os.path.join(data_path, "test_assets/objects/sphere"))
+    )[0]
 
-        # add an object to the scene
-        id_1 = sim.add_object(sphere_template_id)
-        sim.set_translation(np.array([2.50, 0, 0.2]), id_1)
+    # add an object to the scene
+    id_1 = sim.add_object(sphere_template_id)
+    sim.set_translation(np.array([2.50, 0, 0.2]), id_1)
 
-        # simulate
-        observations = simulate(sim, dt=1.5, get_frames=make_video)
+    # simulate
+    observations = simulate(sim, dt=1.5, get_frames=make_video)
 
-        if make_video:
-            make_video_cv2(observations, prefix="sim_basics", open_vid=show_video)
+    if make_video:
+        make_video_cv2(observations, prefix="sim_basics", open_vid=show_video)
 
-        # [/basics]
+    # [/basics]
 
-        remove_all_objects(sim)
+    remove_all_objects(sim)
 
-        # [dynamic_control]
+    # [dynamic_control]
 
-        observations = []
-        # search for an object template by key sub-string
-        cheezit_template_handle = sim.get_template_handles("data/objects/cheezit")[0]
-        box_positions = [
-            np.array([2.39, -0.37, 0]),
-            np.array([2.39, -0.64, 0]),
-            np.array([2.39, -0.91, 0]),
-            np.array([2.39, -0.64, -0.22]),
-            np.array([2.39, -0.64, 0.22]),
-        ]
-        box_orientation = mn.Quaternion.rotation(
-            mn.Rad(math.pi / 2.0), np.array([-1.0, 0, 0])
+    observations = []
+    # search for an object template by key sub-string
+    cheezit_template_handle = sim.get_template_handles("data/objects/cheezit")[0]
+    box_positions = [
+        np.array([2.39, -0.37, 0]),
+        np.array([2.39, -0.64, 0]),
+        np.array([2.39, -0.91, 0]),
+        np.array([2.39, -0.64, -0.22]),
+        np.array([2.39, -0.64, 0.22]),
+    ]
+    box_orientation = mn.Quaternion.rotation(
+        mn.Rad(math.pi / 2.0), np.array([-1.0, 0, 0])
+    )
+    # instance and place the boxes
+    box_ids = []
+    for b in range(5):
+        box_ids.append(sim.add_object_by_handle(cheezit_template_handle))
+        sim.set_translation(box_positions[b], box_ids[b])
+        sim.set_rotation(box_orientation, box_ids[b])
+
+    # get the object's initialization attributes (all boxes initialized with same mass)
+    object_init_template = sim.get_object_initialization_template(box_ids[0])
+    # anti-gravity force f=m(-g)
+    anti_grav_force = -1.0 * sim.get_gravity() * object_init_template.get_mass()
+
+    # throw a sphere at the boxes from the agent position
+    sphere_template = sim.get_object_template(sphere_template_id)
+    sphere_template.set_scale(np.array([0.5, 0.5, 0.5]))
+    sphere_id = sim.add_object(sphere_template_id)
+    sim.set_translation(
+        sim.agents[0].get_state().position + np.array([0, 1.0, 0]), sphere_id
+    )
+    # get the vector from the sphere to a box
+    target_direction = sim.get_translation(box_ids[0]) - sim.get_translation(sphere_id)
+    # apply an initial velocity for one step
+    sim.set_linear_velocity(target_direction * 5, sphere_id)
+    sim.set_angular_velocity(np.array([0, -1.0, 0]), sphere_id)
+
+    start_time = sim.get_world_time()
+    dt = 3.0
+    while sim.get_world_time() < start_time + dt:
+        # set forces/torques before stepping the world
+        for box_id in box_ids:
+            sim.apply_force(anti_grav_force, np.array([0, 0.0, 0]), box_id)
+            sim.apply_torque(np.array([0, 0.01, 0]), box_id)
+        sim.step_physics(1.0 / 60.0)
+        observations.append(sim.get_sensor_observations())
+
+    if make_video:
+        make_video_cv2(observations, prefix="dynamic_control", open_vid=show_video)
+
+    # [/dynamic_control]
+
+    remove_all_objects(sim)
+
+    # [kinematic_interactions]
+
+    chefcan_template_handle = sim.get_template_handles("data/objects/chefcan")[0]
+    id_1 = sim.add_object_by_handle(chefcan_template_handle)
+    sim.set_translation(np.array([2.4, -0.64, 0]), id_1)
+    # set one object to kinematic
+    sim.set_object_motion_type(habitat_sim.physics.MotionType.KINEMATIC, id_1)
+
+    # drop some dynamic objects
+    id_2 = sim.add_object_by_handle(chefcan_template_handle)
+    sim.set_translation(np.array([2.4, -0.64, 0.28]), id_2)
+    id_3 = sim.add_object_by_handle(chefcan_template_handle)
+    sim.set_translation(np.array([2.4, -0.64, -0.28]), id_3)
+    id_4 = sim.add_object_by_handle(chefcan_template_handle)
+    sim.set_translation(np.array([2.4, -0.3, 0]), id_4)
+
+    # simulate
+    observations = simulate(sim, dt=1.5, get_frames=True)
+
+    if make_video:
+        make_video_cv2(
+            observations, prefix="kinematic_interactions", open_vid=show_video
         )
-        # instance and place the boxes
-        box_ids = []
-        for b in range(5):
-            box_ids.append(sim.add_object_by_handle(cheezit_template_handle))
-            sim.set_translation(box_positions[b], box_ids[b])
-            sim.set_rotation(box_orientation, box_ids[b])
 
-        # get the object's initialization attributes (all boxes initialized with same mass)
-        object_init_template = sim.get_object_initialization_template(box_ids[0])
-        # anti-gravity force f=m(-g)
-        anti_grav_force = -1.0 * sim.get_gravity() * object_init_template.get_mass()
+    # [/kinematic_interactions]
 
-        # throw a sphere at the boxes from the agent position
-        sphere_template = sim.get_object_template(sphere_template_id)
-        sphere_template.set_scale(np.array([0.5, 0.5, 0.5]))
-        sphere_id = sim.add_object(sphere_template_id)
-        sim.set_translation(
-            sim.agents[0].get_state().position + np.array([0, 1.0, 0]), sphere_id
+    remove_all_objects(sim)
+
+    # [kinematic_update]
+    observations = []
+
+    clamp_template_handle = sim.get_template_handles("data/objects/largeclamp")[0]
+    id_1 = sim.add_object_by_handle(clamp_template_handle)
+    sim.set_object_motion_type(habitat_sim.physics.MotionType.KINEMATIC, id_1)
+    sim.set_translation(np.array([0.8, 0, 0.5]), id_1)
+
+    start_time = sim.get_world_time()
+    dt = 1.0
+    while sim.get_world_time() < start_time + dt:
+        # manually control the object's kinematic state
+        sim.set_translation(sim.get_translation(id_1) + np.array([0, 0, 0.01]), id_1)
+        sim.set_rotation(
+            mn.Quaternion.rotation(mn.Rad(0.05), np.array([-1.0, 0, 0]))
+            * sim.get_rotation(id_1),
+            id_1,
         )
-        # get the vector from the sphere to a box
-        target_direction = sim.get_translation(box_ids[1]) - sim.get_translation(
-            sphere_id
-        )
-        # apply an initial velocity for one step
-        sim.set_linear_velocity(target_direction * 5, sphere_id)
-        sim.set_angular_velocity(np.array([0, -1.0, 0]), sphere_id)
+        sim.step_physics(1.0 / 60.0)
+        observations.append(sim.get_sensor_observations())
 
-        start_time = sim.get_world_time()
-        dt = 3.0
-        while sim.get_world_time() < start_time + dt:
-            # set forces/torques before stepping the world
-            for box_id in box_ids:
-                sim.apply_force(anti_grav_force, np.array([0, 0.0, 0]), box_id)
-                sim.apply_torque(np.array([0, 0.01, 0]), box_id)
-            sim.step_physics(1.0 / 60.0)
-            observations.append(sim.get_sensor_observations())
+    if make_video:
+        make_video_cv2(observations, prefix="kinematic_update", open_vid=show_video)
 
-        if make_video:
-            make_video_cv2(observations, prefix="dynamic_control", open_vid=show_video)
+    # [/kinematic_update]
 
-        # [/dynamic_control]
+    # [velocity_control]
 
-        remove_all_objects(sim)
+    # get object VelocityControl structure and setup control
+    vel_control = sim.get_object_velocity_control(id_1)
+    vel_control.linear_velocity = np.array([0, 0, -1.0])
+    vel_control.angular_velocity = np.array([4.0, 0, 0])
+    vel_control.controlling_lin_vel = True
+    vel_control.controlling_ang_vel = True
 
-        # [kinematic_interactions]
+    observations = simulate(sim, dt=1.0, get_frames=True)
 
-        chefcan_template_handle = sim.get_template_handles("data/objects/chefcan")[0]
-        id_1 = sim.add_object_by_handle(chefcan_template_handle)
-        sim.set_translation(np.array([2.4, -0.64, 0]), id_1)
-        # set one object to kinematic
-        sim.set_object_motion_type(habitat_sim.physics.MotionType.KINEMATIC, id_1)
+    # reverse linear direction
+    vel_control.linear_velocity = np.array([0, 0, 1.0])
 
-        # drop some dynamic objects
-        id_2 = sim.add_object_by_handle(chefcan_template_handle)
-        sim.set_translation(np.array([2.4, -0.64, 0.28]), id_2)
-        id_3 = sim.add_object_by_handle(chefcan_template_handle)
-        sim.set_translation(np.array([2.4, -0.64, -0.28]), id_3)
-        id_4 = sim.add_object_by_handle(chefcan_template_handle)
-        sim.set_translation(np.array([2.4, -0.3, 0]), id_4)
+    observations += simulate(sim, dt=1.0, get_frames=True)
 
-        # simulate
-        observations = simulate(sim, dt=1.5, get_frames=True)
+    make_video_cv2(observations, prefix="velocity_control", open_vid=True)
 
-        if make_video:
-            make_video_cv2(
-                observations, prefix="kinematic_interactions", open_vid=show_video
-            )
+    # [/velocity_control]
 
-        # [/kinematic_interactions]
+    # [local_velocity_control]
 
-        remove_all_objects(sim)
+    vel_control.linear_velocity = np.array([0, 0, 2.3])
+    vel_control.angular_velocity = np.array([-4.3, 0.0, 0])
+    vel_control.lin_vel_is_local = True
+    vel_control.ang_vel_is_local = True
 
-        # [kinematic_update]
-        observations = []
+    observations = simulate(sim, dt=1.5, get_frames=True)
 
-        clamp_template_handle = sim.get_template_handles("data/objects/largeclamp")[0]
-        id_1 = sim.add_object_by_handle(clamp_template_handle)
-        sim.set_object_motion_type(habitat_sim.physics.MotionType.KINEMATIC, id_1)
-        sim.set_translation(np.array([0.8, 0, 0.5]), id_1)
+    # video rendering
+    make_video_cv2(observations, prefix="local_velocity_control", open_vid=True)
 
-        start_time = sim.get_world_time()
-        dt = 1.0
-        while sim.get_world_time() < start_time + dt:
-            # manually control the object's kinematic state
-            sim.set_translation(
-                sim.get_translation(id_1) + np.array([0, 0, 0.01]), id_1
-            )
-            sim.set_rotation(
-                mn.Quaternion.rotation(mn.Rad(0.05), np.array([-1.0, 0, 0]))
-                * sim.get_rotation(id_1),
-                id_1,
-            )
-            sim.step_physics(1.0 / 60.0)
-            observations.append(sim.get_sensor_observations())
-
-        if make_video:
-            make_video_cv2(observations, prefix="kinematic_update", open_vid=show_video)
-
-        # [/kinematic_update]
-
-        # [velocity_control]
-
-        # get object VelocityControl structure and setup control
-        vel_control = sim.get_object_velocity_control(id_1)
-        vel_control.linear_velocity = np.array([0, 0, -1.0])
-        vel_control.angular_velocity = np.array([4.0, 0, 0])
-        vel_control.controlling_lin_vel = True
-        vel_control.controlling_ang_vel = True
-
-        observations = simulate(sim, dt=1.0, get_frames=True)
-
-        # reverse linear direction
-        vel_control.linear_velocity = np.array([0, 0, 1.0])
-
-        observations += simulate(sim, dt=1.0, get_frames=True)
-
-        make_video_cv2(observations, prefix="velocity_control", open_vid=True)
-
-        # [/velocity_control]
-
-        # [local_velocity_control]
-
-        vel_control.linear_velocity = np.array([0, 0, 2.3])
-        vel_control.angular_velocity = np.array([-4.3, 0.0, 0])
-        vel_control.lin_vel_is_local = True
-        vel_control.ang_vel_is_local = True
-
-        observations = simulate(sim, dt=1.5, get_frames=True)
-
-        # video rendering
-        make_video_cv2(observations, prefix="local_velocity_control", open_vid=True)
-
-        # [/local_velocity_control]
-
-    # test locobot dynamic velocity control
+    # [/local_velocity_control]
 
     # [embodied_agent]
 
+    # load the lobot_merged asset
     locobot_template_id = sim.load_object_configs(
         str(os.path.join(data_path, "objects/locobot_merged"))
     )[0]
 
-    # add an object to the scene with the agent/camera SceneNode attached
+    # add robot object to the scene with the agent/camera SceneNode attached
     id_1 = sim.add_object(locobot_template_id, sim.agents[0].scene_node)
     sim.set_translation(np.array([1.75, -1.02, 0.4]), id_1)
 
@@ -322,7 +316,7 @@ def main(make_video=True, show_video=True):
     vel_control.linear_velocity = np.array([0, 0, -1.0])
     vel_control.angular_velocity = np.array([0.0, 2.0, 0])
 
-    # simulate
+    # simulate robot dropping into place
     observations = simulate(sim, dt=1.5, get_frames=make_video)
 
     vel_control.controlling_lin_vel = True
@@ -330,34 +324,34 @@ def main(make_video=True, show_video=True):
     vel_control.lin_vel_is_local = True
     vel_control.ang_vel_is_local = True
 
-    # simulate
+    # simulate forward and turn
     observations += simulate(sim, dt=1.0, get_frames=make_video)
 
     vel_control.controlling_lin_vel = False
     vel_control.angular_velocity = np.array([0.0, 1.0, 0])
 
-    # simulate
+    # simulate turn only
     observations += simulate(sim, dt=1.5, get_frames=make_video)
 
     vel_control.angular_velocity = np.array([0.0, 0.0, 0])
     vel_control.controlling_lin_vel = True
     vel_control.controlling_ang_vel = True
 
-    # simulate
+    # simulate forward only with damped angular velocity (reset angular velocity to 0 after each step)
     observations += simulate(sim, dt=1.0, get_frames=make_video)
 
     vel_control.angular_velocity = np.array([0.0, -1.25, 0])
 
-    # simulate
+    # simulate forward and turn
     observations += simulate(sim, dt=2.0, get_frames=make_video)
 
     vel_control.controlling_ang_vel = False
     vel_control.controlling_lin_vel = False
 
-    # simulate
+    # simulate settling
     observations += simulate(sim, dt=3.0, get_frames=make_video)
 
-    # video rendering
+    # video rendering with embedded 1st person view
     make_video_cv2(observations, prefix="robot_control", open_vid=True, multi_obs=True)
 
     # [/embodied_agent]
