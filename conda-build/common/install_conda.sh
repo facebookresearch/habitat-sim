9eabc8a518ef5eaa6273a43b405c88988e38a81d
Oleksandr <maksymets.o@gmail.com>
2020-07-14 16:19:30 -0700

    Move Conda Nightly builds to Docker to support Colab (#671)
    #663 uses Ubuntu 16.04 as base OS to build conda builds. That limits conda build compatibility compare to using Centos7 as base OS.
    
    Added install Docker step to Nightly builds job to support Colab
    Added "mesa-libGL-devel" install to conda Docker to improver build with Display
    Moved build conda env creation to docker build phase

--- conda-build/common/install_conda.sh
+++ conda-build/common/install_conda.sh
@@ -8,5 +8,5 @@ chmod +x  Miniconda2-latest-Linux-x86_64.sh
 ./Miniconda2-latest-Linux-x86_64.sh -b -p /opt/conda
 rm Miniconda2-latest-Linux-x86_64.sh
 export PATH=/opt/conda/bin:$PATH
-conda install -y anaconda-client git ninja conda-build=3.18.9 # last version that works with our setup
+conda install -y anaconda-client git gitpython ninja conda-build=3.18.9 # last version that works with our setup
 conda remove -y --force patchelf
