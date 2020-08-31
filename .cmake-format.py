bf7c3ff2d5d1a9c8b03a344b8a4dad91e2596944
Aaron Gokaslan <agokaslan@fb.com>
2020-08-31 08:04:31 -0700

    Clean up .pre-commit and cmake-format

--- .cmake-format.py
+++ .cmake-format.py
@@ -3,6 +3,5 @@ with section("format"):
     dangle_parens = True  # Dangle Parens to make diffs cleaner
     max_pargs_hwrap = 3  # More aggressive wrapping than defaults
 
-
 with section("markup"):
     enable_markup = False  # Don't format comments
