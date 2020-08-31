e1d4391ed44167c89d02720c328de996c93af094
Aaron Gokaslan <agokaslan@fb.com>
2020-08-31 10:39:17 -0700

    reduce min indent to two

--- .cmake-format.py
+++ .cmake-format.py
@@ -2,6 +2,7 @@
 with section("format"):
     line_width = 88  # Give a little more length
     max_subgroups_hwrap = 3  # So set commands dont get wrapped
+    min_prefix_chars = 2
     dangle_parens = True  # Dangle Parens to make diffs cleaner
     always_wrap = ["add_library", "target_link_libraries"]  # Always wrap these ones
     autosort = True  # Try to autosort lists
