de3423b39b9494941c06a6daf5b98d86a4960609
Aaron Gokaslan <agokaslan@fb.com>
2020-08-31 07:10:27 -0700

    Adjust cmake-format settings

--- .cmake-format.py
+++ .cmake-format.py
@@ -1,6 +1,11 @@
 # flake8: noqa
 with section("format"):
     tab_size = 4
-    max_pargs_hwrap = 3
+    # min_prefix is affected by tab size
+    min_prefix_chars = tab_size * 4
+    dangle_parens = True  # Dangle Parens to make diffs cleaner
+    max_pargs_hwrap = 3  # More aggressive wrapping than defaults
+
+
 with section("markup"):
     enable_markup = False  # Don't format comments
