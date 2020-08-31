ca75cf28990ad3128d56fc27371affb1bd7f5ec9
Aaron Gokaslan <agokaslan@fb.com>
2020-08-31 10:35:41 -0700

    always wrap add_library too

--- .cmake-format.py
+++ .cmake-format.py
@@ -3,7 +3,7 @@ with section("format"):
     line_width = 88  # Give a little more length
     max_subgroups_hwrap = 3  # So set commands dont get wrapped
     dangle_parens = True  # Dangle Parens to make diffs cleaner
-    always_wrap = ["target_link_libraries"]  # Always wrap these ones
+    always_wrap = ["add_library", "target_link_libraries"]  # Always wrap these ones
     autosort = True  # Try to autosort lists
 with section("markup"):
     enable_markup = False  # Don't format comments
