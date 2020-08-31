9c309ea69a39a4e54a6972bc22c7852d714b2e00
Aaron Gokaslan <agokaslan@fb.com>
2020-08-31 09:34:10 -0700

    enable autosort cmake-format feature

--- .cmake-format.py
+++ .cmake-format.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 with section("format"):
-    dangle_parens = True  # Dangle Parens to make diffs cleaner
     max_pargs_hwrap = 3  # More aggressive wrapping than defaults
-
+    dangle_parens = True  # Dangle Parens to make diffs cleaner
+    autosort = True  # Try to autosort lists
 with section("markup"):
     enable_markup = False  # Don't format comments
