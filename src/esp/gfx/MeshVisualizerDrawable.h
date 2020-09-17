e07d8b642f243931f167eb6fd3a7255f3e7e8cfb
John Turner <jturner65@users.noreply.github.com>
2020-09-17 16:04:34 -0400

    Update src/esp/gfx/MeshVisualizerDrawable.h
    Co-authored-by: Alexander Clegg <alexanderwclegg@gmail.com>

--- src/esp/gfx/MeshVisualizerDrawable.h
+++ src/esp/gfx/MeshVisualizerDrawable.h
@@ -2,8 +2,8 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-#ifndef ESP_GFX_MESHVISUALIZATIONDRAWABLE_H_
-#define ESP_GFX_MESHVISUALIZATIONDRAWABLE_H_
+#ifndef ESP_GFX_MESHVISUALIZERDRAWABLE_H_
+#define ESP_GFX_MESHVISUALIZERDRAWABLE_H_
 
 #include <Magnum/Shaders/MeshVisualizer.h>
 #include "Drawable.h"
