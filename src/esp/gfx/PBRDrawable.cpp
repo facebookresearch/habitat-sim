936eff5d405c7ee5e5c926ffa8c5d0d400139c96
bigbike <yili@fb.com>
2020-09-23 20:54:21 -0700

    add PBRDrawable

---
+++ src/esp/gfx/PBRDrawable.cpp
@@ -0,0 +1,66 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "PTexMeshDrawable.h"
+
+#include "esp/assets/PTexMeshData.h"
+#include "esp/gfx/PTexMeshShader.h"
+
+namespace esp {
+namespace gfx {
+
+// static constexpr arrays require redundant definitions until C++17
+constexpr char PTexMeshDrawable::SHADER_KEY[];
+
+PTexMeshDrawable::PTexMeshDrawable(scene::SceneNode& node,
+                                   assets::PTexMeshData& ptexMeshData,
+                                   int submeshID,
+                                   ShaderManager& shaderManager,
+                                   DrawableGroup* group /* = nullptr */)
+    : Drawable{node, ptexMeshData.getRenderingBuffer(submeshID)->mesh, group},
+      atlasTexture_(ptexMeshData.getRenderingBuffer(submeshID)->atlasTexture),
+#ifndef CORRADE_TARGET_APPLE
+      adjFacesBufferTexture_(
+          ptexMeshData.getRenderingBuffer(submeshID)->adjFacesBufferTexture),
+#endif
+      tileSize_(ptexMeshData.tileSize()),
+      exposure_(ptexMeshData.exposure()),
+      gamma_(ptexMeshData.gamma()),
+      saturation_(ptexMeshData.saturation()),
+      visualizerTriangleMesh_(
+          ptexMeshData.getRenderingBuffer(submeshID)->triangleMesh) {
+  auto shaderResource =
+      shaderManager.get<Magnum::GL::AbstractShaderProgram, PTexMeshShader>(
+          SHADER_KEY);
+
+  if (!shaderResource) {
+    shaderManager.set<Magnum::GL::AbstractShaderProgram>(shaderResource.key(),
+                                                         new PTexMeshShader{});
+  }
+  shader_ = &(*shaderResource);
+}
+
+void PTexMeshDrawable::draw(const Magnum::Matrix4& transformationMatrix,
+                            Magnum::SceneGraph::Camera3D& camera) {
+  (*shader_)
+      .setExposure(exposure_)
+      .setGamma(gamma_)
+      .setSaturation(saturation_)
+      .setAtlasTextureSize(atlasTexture_, tileSize_)
+      .bindAtlasTexture(atlasTexture_)
+      // e.g., semantic mesh has its own per vertex annotation, which has been
+      // uploaded to GPU so simply pass 0 to the uniform "objectId" in the
+      // fragment shader
+      .setObjectId(static_cast<RenderCamera&>(camera).useDrawableIds()
+                       ? drawableId_
+                       : node_.getSemanticId())
+#ifndef CORRADE_TARGET_APPLE
+      .bindAdjFacesBufferTexture(adjFacesBufferTexture_)
+#endif
+      .setMVPMatrix(camera.projectionMatrix() * transformationMatrix)
+      .draw(mesh_);
+}
+
+}  // namespace gfx
+}  // namespace esp
---
+++ src/esp/gfx/PBRDrawable.h
@@ -0,0 +1,55 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef ESP_GFX_GENERICDRAWABLE_H_
+#define ESP_GFX_GENERICDRAWABLE_H_
+
+#include <Magnum/Shaders/Phong.h>
+
+#include "esp/gfx/Drawable.h"
+#include "esp/gfx/ShaderManager.h"
+
+namespace esp {
+namespace gfx {
+
+class PBRDrawable : public Drawable {
+ public:
+  //! Create a PBRDrawable for the given object using shader and mesh.
+  //! Adds drawable to given group and uses provided texture, and
+  //! color for textured buffer and color shader output respectively
+  explicit PBRDrawable(scene::SceneNode& node,
+                       Magnum::GL::Mesh& mesh,
+                       ShaderManager& shaderManager,
+                       const Magnum::ResourceKey& lightSetup,
+                       const Magnum::ResourceKey& materialData,
+                       DrawableGroup* group = nullptr);
+
+  void setLightSetup(const Magnum::ResourceKey& lightSetup) override;
+  static constexpr const char* SHADER_KEY_TEMPLATE = "PBR-lights={}-flags={}";
+
+ protected:
+  virtual void draw(const Magnum::Matrix4& transformationMatrix,
+                    Magnum::SceneGraph::Camera3D& camera) override;
+
+  void updateShader();
+  void updateShaderLightingParameters(
+      const Magnum::Matrix4& transformationMatrix,
+      Magnum::SceneGraph::Camera3D& camera);
+
+  Magnum::ResourceKey getShaderKey(Magnum::UnsignedInt lightCount,
+                                   Magnum::Shaders::Phong::Flags flags) const;
+
+  // shader parameters
+  ShaderManager& shaderManager_;
+  // XXX PBR shader class
+  Magnum::Resource<Magnum::GL::AbstractShaderProgram, Magnum::Shaders::Phong>
+      shader_;
+  Magnum::Resource<MaterialData, PBRMaterialData> materialData_;
+  Magnum::Resource<LightSetup> lightSetup_;
+};
+
+}  // namespace gfx
+}  // namespace esp
+
+#endif  // ESP_GFX_GENERICDRAWABLE_H_
