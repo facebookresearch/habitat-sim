dfb2d40747aa7412aa17e1d15a518f7cefbc03ce
Eric Undersander <eundersander@gmail.com>
2020-09-29 10:38:36 -0700

    fix for Bullet static objects/stage

--- src/esp/physics/bullet/BulletRigidObject.cpp
+++ src/esp/physics/bullet/BulletRigidObject.cpp
@@ -46,7 +46,7 @@ BulletRigidObject::~BulletRigidObject() {
   } else if (objectMotionType_ == MotionType::STATIC) {
     // remove collision objects from the world
     for (auto& co : bStaticCollisionObjects_) {
-      bWorld_->removeCollisionObject(co.get());
+      bWorld_->removeRigidBody(co.get());
       collisionObjToObjIds_->erase(co.get());
     }
   }
@@ -255,7 +255,7 @@ bool BulletRigidObject::setMotionType(MotionType mt) {
 
   // remove the existing object from the world to change its type
   if (objectMotionType_ == MotionType::STATIC) {
-    bWorld_->removeCollisionObject(bStaticCollisionObjects_.back().get());
+    bWorld_->removeRigidBody(bStaticCollisionObjects_.back().get());
     collisionObjToObjIds_->erase(bStaticCollisionObjects_.back().get());
     bStaticCollisionObjects_.clear();
   } else if (objectMotionType_ != MotionType::RENDER_ONLY) {
@@ -281,13 +281,16 @@ bool BulletRigidObject::setMotionType(MotionType mt) {
         ~btCollisionObject::CF_KINEMATIC_OBJECT);
     objectMotionType_ = MotionType::STATIC;
 
-    // create a static collision object at the current transform
-    std::unique_ptr<btCollisionObject> staticCollisionObject =
-        std::make_unique<btCollisionObject>();
-    staticCollisionObject->setCollisionShape(bObjectShape_.get());
-    staticCollisionObject->setWorldTransform(
-        bObjectRigidBody_->getWorldTransform());
-    bWorld_->addCollisionObject(
+    // mass == 0 to indicate static. See isStaticObject assert below. See also
+    // examples/MultiThreadedDemo/CommonRigidBodyMTBase.h
+    btVector3 localInertia(0, 0, 0);
+    btRigidBody::btRigidBodyConstructionInfo cInfo(
+        /*mass*/ 0.0, nullptr, bObjectShape_.get(), localInertia);
+    cInfo.m_startWorldTransform = bObjectRigidBody_->getWorldTransform();
+    std::unique_ptr<btRigidBody> staticCollisionObject =
+        std::make_unique<btRigidBody>(cInfo);
+    ASSERT(staticCollisionObject->isStaticObject());
+    bWorld_->addRigidBody(
         staticCollisionObject.get(),
         2,       // collisionFilterGroup (2 == StaticFilter)
         1 + 2);  // collisionFilterMask (1 == DefaultFilter, 2==StaticFilter)
