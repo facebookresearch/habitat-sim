af502e8d704fe4d17ef6e3046f79620b6df16e45
bigbike <yili@fb.com>
2020-10-04 15:24:44 -0700

    minor

--- src/esp/sim/Simulator.cpp
+++ src/esp/sim/Simulator.cpp
@@ -210,7 +210,7 @@ void Simulator::reconfigure(const SimulatorConfiguration& cfg) {
 
     const Magnum::Range3D& sceneBB = rootNode.computeCumulativeBB();
     resourceManager_->setLightSetup(
-        gfx::getLightsAtBoxCorners(sceneBB, Magnum::Color3{300.0f}));
+        gfx::getLightsAtBoxCorners(sceneBB, Magnum::Color3{180.0f}));
 
     // set activeSemanticSceneID_ values and push onto sceneID vector if
     // appropriate - tempIDs[1] will either be old activeSemanticSceneID_ (if
@@ -282,7 +282,7 @@ void Simulator::reset() {
   const Magnum::Range3D& sceneBB =
       getActiveSceneGraph().getRootNode().computeCumulativeBB();
   resourceManager_->setLightSetup(
-      gfx::getLightsAtBoxCorners(sceneBB, Magnum::Color3{300.0f}));
+      gfx::getLightsAtBoxCorners(sceneBB, Magnum::Color3{180.0f}));
 }  // Simulator::reset()
 
 void Simulator::seed(uint32_t newSeed) {
