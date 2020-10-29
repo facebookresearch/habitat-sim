ed1254727a87d34fde21ebcd79f0a13d0798b98d
Alexander Clegg <alexanderwclegg@gmail.com>
2020-10-29 10:34:23 -0700

    Add Advanced Features tutorial code to test_examples and fix minor refactor bug. (#907)

--- examples/tutorials/nb_python/ECCV_2020_Advanced_Features.py
+++ examples/tutorials/nb_python/ECCV_2020_Advanced_Features.py
@@ -1005,7 +1005,7 @@ print(object_orientation)
 
 # add a box with default semanticId configured in the template
 # Note: each face of this box asset is a separate component
-box_template = habitat_sim.attributes.PhysicsObjectAttributes()
+box_template = habitat_sim.attributes.ObjectAttributes()
 box_template.render_asset_handle = str(
     os.path.join(data_path, "test_assets/objects/transform_box.glb")
 )
