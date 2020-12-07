48dadb8b298227572ff2c361349d78e445cd178b
Aaron Gokaslan <agokaslan@fb.com>
2020-12-07 13:36:23 -0500

    Refactor Multi-Agent Sensors API and allow dynamic adding of Sensors (#946)
    * Add refactor Agent API to allow a sensor to be added without reconfigure
    
    * Modify agent config as well
    
    * clean up overwrite sensor_spec
    
    * Make add_sensor private
    
    * Try to revamp multi-agent API
    
    * Fix typing
    
    * Remove unnecessary boolean
    
    * Commit missing test changes
    
    * Fix more typing issues
    
    * Simplify step
    
    * Fix bugs
    
    * Optimize loop and tighten up typing
    
    * Ensure observation order is sorted
    
    * Add multiagent tests
    
    * Add compatibility shim
    
    * Make last_state multiagent compatible
    
    * Refactor multi-agent API to OrderedDict
    
    * Remove assert
    
    * Attempt to fix habitat-lab error
    
    * Add another backwards compat shim
    
    * Fix test for multi-sensor
    
    * Tighten up typing
    
    * Add tests and edge case handling for add_Sensor API
    
    * Fully fix add_sensor edge cases
    
    * Properly configure sim to use _default_agent_id
    
    * Overload reset and make LastState a dict
    
    * Fix sim bug

--- habitat_sim/sensors/sensor_suite.py
+++ habitat_sim/sensors/sensor_suite.py
@@ -1,7 +1,9 @@
+from typing import Dict
+
 from habitat_sim import bindings as hsim
 
 
-class SensorSuite(dict):
+class SensorSuite(Dict[str, hsim.Sensor]):
     r"""Holds all the agents sensors. Simply a dictionary with an extra method
     to lookup the name of a sensor as the key
     """
