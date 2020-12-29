5a0d67e84005523c619fce58b1af90dd6ce21644
John Turner <7strbass@gmail.com>
2020-12-29 14:37:25 -0500

    --update tests to use config-based MM constructor.

--- src/tests/GfxReplayTest.cpp
+++ src/tests/GfxReplayTest.cpp
@@ -35,8 +35,9 @@ TEST(GfxReplayTest, recorder) {
 
   std::shared_ptr<esp::gfx::Renderer> renderer_ = esp::gfx::Renderer::create();
 
+  auto cfg = esp::sim::SimulatorConfiguration{};
+  auto MM = MetadataMediator::create(cfg);
   // must declare these in this order due to avoid deallocation errors
-  auto MM = MetadataMediator::create();
   ResourceManager resourceManager(MM);
   SceneManager sceneManager_;
   std::string boxFile =
@@ -108,8 +109,9 @@ TEST(GfxReplayTest, player) {
 
   std::shared_ptr<esp::gfx::Renderer> renderer_ = esp::gfx::Renderer::create();
 
+  auto cfg = esp::sim::SimulatorConfiguration{};
+  auto MM = MetadataMediator::create(cfg);
   // must declare these in this order due to avoid deallocation errors
-  auto MM = MetadataMediator::create();
   ResourceManager resourceManager(MM);
   SceneManager sceneManager_;
   std::string boxFile =
