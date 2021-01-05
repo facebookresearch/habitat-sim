30617ec8ab535ceae4c68ff79dfc7599071b7a26
erikwijmans <ewijmans2@gmail.com>
2021-01-05 15:35:45 -0500

    Some module changes

--- build.sh
+++ build.sh
@@ -32,7 +32,6 @@ if [[ $my_hostname =~ "fair" ]]; then
   module purge
   module load cuda/10.0
   module load cudnn/v7.4-cuda.10.0
-  module load gcc/7.1.0
   module load cmake/3.15.3/gcc.7.3.0
 fi
 
