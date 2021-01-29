856d4b08c1a2632626bf0d205bf46471a99502b7
Alexander Clegg <alexanderwclegg@gmail.com>
2021-01-29 15:09:17 -0800

    Bump version to 0.1.7 (#1063)
    Update version to 0.1.7

--- habitat_sim/__init__.py
+++ habitat_sim/__init__.py
@@ -6,7 +6,7 @@
 
 import builtins
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 if not getattr(builtins, "__HSIM_SETUP__", False):
     # TODO: kept only for compatibiliy with existing code. Please gradually remove
