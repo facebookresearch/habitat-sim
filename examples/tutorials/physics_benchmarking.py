b306c12a34354b2197ecadc0b6eb76efd9466d73
ScottCSteinhauser <scsteinhauser@gmail.com>
2021-01-29 15:35:23 -0600

    Added command line arguments for running specific tests, outputting test results to a log file, and changed some variable names for more uniformity.

--- examples/tutorials/physics_benchmarking.py
+++ examples/tutorials/physics_benchmarking.py
@@ -114,19 +114,19 @@ def box_drop_test(
         # build box
         cube_handle = obj_templates_mgr.get_template_handles("cube")[0]
 
-        boxParts = []
+        box_parts = []
         boxIDs = []
         for _i in range(5):
-            boxParts += [obj_templates_mgr.get_template_by_handle(cube_handle)]
-        boxParts[0].scale = np.array([1.0, 0.1, 1.0]) * box_size
-        boxParts[1].scale = np.array([1.0, 0.6, 0.1]) * box_size
-        boxParts[2].scale = np.array([1.0, 0.6, 0.1]) * box_size
-        boxParts[3].scale = np.array([0.1, 0.6, 1.0]) * box_size
-        boxParts[4].scale = np.array([0.1, 0.6, 1.0]) * box_size
+            box_parts += [obj_templates_mgr.get_template_by_handle(cube_handle)]
+        box_parts[0].scale = np.array([1.0, 0.1, 1.0]) * box_size
+        box_parts[1].scale = np.array([1.0, 0.6, 0.1]) * box_size
+        box_parts[2].scale = np.array([1.0, 0.6, 0.1]) * box_size
+        box_parts[3].scale = np.array([0.1, 0.6, 1.0]) * box_size
+        box_parts[4].scale = np.array([0.1, 0.6, 1.0]) * box_size
 
         for i in range(5):
             part_name = "box_part_" + str(i)
-            obj_templates_mgr.register_template(boxParts[i], part_name)
+            obj_templates_mgr.register_template(box_parts[i], part_name)
             boxIDs += [sim.add_object_by_handle(part_name)]
             sim.set_object_motion_type(
                 habitat_sim.physics.MotionType.KINEMATIC, boxIDs[i]
@@ -190,107 +190,123 @@ def box_drop_test(
 
 
 benchmarks = {
-    # "box_drop_test_1": lambda: box_drop_test(
-    #    ["test_assets/objects/sphere"], 25, 2, 1.8, 10
-    # ),
-    "box_drop_test_2": lambda: box_drop_test(
-        ["test_assets/objects/sphere", "test_assets/objects/chair"], 50, 2, 1.8, 10
-    ),
-    # "box_drop_test_3": lambda: box_drop_test(
-    #    [
-    #        "test_assets/objects/sphere",
-    #        "test_assets/objects/chair",
-    #        "test_assets/objects/donut",
-    #        "test_assets/objects/nested_box",
-    #    ],
-    #    200,
-    #    2,
-    #    1.8,
-    #    10,
-    # ),
+    "box_drop_test_1": {
+        "description": "Drop 25 spheres into a box.",
+        "test": lambda: box_drop_test(["test_assets/objects/sphere"], 25, 2, 1.8, 10),
+    },
+    "box_drop_test_2": {
+        "description": "Drop 25 spheres and 25 chairs into a box.",
+        "test": lambda: box_drop_test(
+            ["test_assets/objects/sphere", "test_assets/objects/chair"], 50, 2, 1.8, 10
+        ),
+    },
+    "box_drop_test_3": {
+        "description": "Drop 50 spheres, 5 chairs, 50 donuts, and 50 boxes into a box.",
+        "test": lambda: box_drop_test(
+            [
+                "test_assets/objects/sphere",
+                "test_assets/objects/chair",
+                "test_assets/objects/donut",
+                "test_assets/objects/nested_box",
+            ],
+            200,
+            2,
+            1.8,
+            10,
+        ),
+    },
 }  # specify parameters for each scenario
 
-test_sets = {
+# Define a grouping of tests you want to run
+benchmark_sets = {
     "box_drop_tests": ["box_drop_test_1", "box_drop_test_2", "box_drop_test_3"]
 }
 
 
-def createFrameLinePlot(testId, y_axis, title, metric):
+def create_frame_line_plot(test_id, y_axis, title):
     plt.plot(y_axis)
-    plt.title(title + " (%s)" % testId)
+    plt.title(title + " (%s)" % test_id)
     plt.xlabel("Frame #")
-    plt.ylabel(metric)
-    filename = testId + "_%s_graph" % metric + ".png"
+    plt.ylabel(title)
+    filename = test_id + "_%s_graph" % title + ".png"
     filename = filename.replace(" ", "_")
     plt.savefig(output_path + filename)
+    plt.cla()
 
 
-def runTest(testId):
-    print("----- Running %s -----" % testId)
+def run_test(test_id):
+    print("----- Running %s -----" % test_id)
     start_time = time.time()
-    data = benchmarks[testId]()
+    data = benchmarks[test_id]["test"]()
     physics_step_times = data["physics_step_times"]
     graphics_render_times = data["graphics_render_times"]
     collisions = data["collisions"]
     observations = data["observations"]
     end_time = time.time()
 
-    print(" ========================= Performance ======================== ")
-    print("| Number of frames: " + str(len(physics_step_times)))
-    print(
-        "| Average time per physics step: "
+    test_log = ""
+    test_log += "Test: %s completed" % test_id + "\n"
+    test_log += "\t Description: " + benchmarks[test_id]["description"] + "\n"
+    test_log += (
+        "\t ========================= Performance ======================== " + "\n"
+    )
+    test_log += "\t| Number of frames: " + str(len(physics_step_times)) + "\n"
+    test_log += (
+        "\t| Average time per physics step: "
         + str(sum(physics_step_times) / len(physics_step_times))
+        + "\n"
     )
-    print(
-        "| Average time per frame render: "
+    test_log += (
+        "\t| Average time per frame render: "
         + str(sum(graphics_render_times) / len(graphics_render_times))
+        + "\n"
     )
-    print("| Maximum Collisions: " + str(max(collisions)))
-    print("| Total time for test: " + str(end_time - start_time))
-    print("|----- END of %s -----" % testId)
-    print(" ============================================================== ")
+    test_log += "\t| Maximum Collisions: " + str(max(collisions)) + "\n"
+    test_log += "\t| Total time for test: " + str(end_time - start_time) + "\n"
+    test_log += "\t|----- END of %s -----" % test_id + "\n"
+    test_log += (
+        "\t ============================================================== " + "\n"
+    )
+    test_log += "\n"
 
+    print(test_log)
     if make_video:
         vut.make_video(
             observations,
             "rgba_camera_1stperson",
             "color",
-            output_path + testId,
+            output_path + test_id,
             open_vid=show_video,
         )
 
-    if graph_step_times:
-        createFrameLinePlot(
-            testId,
-            physics_step_times,
-            "Time to simulate a frame (1/60 s)",
-            "Simulation Time (s)",
-        )
-    if graph_render_times:
-        createFrameLinePlot(
-            testId,
-            graphics_render_times,
-            "Time to render a frame (1/60 s)",
-            "Render Time (s)",
-        )
-    if graph_collisions:
-        createFrameLinePlot(
-            testId,
-            collisions,
-            "Active Collisions per Frame frame (1/60 s)",
-            "Active Collisions",
-        )
+    for graph_name in graphs_to_produce:
+        if graph_name in data:
+            create_frame_line_plot(test_id, data[graph_name], graph_name)
 
-    return end_time - start_time
-
-
-# Define Main driver function
+    return test_log
 
 
 def main():
-    # runs all unit tests
-    for testId in benchmarks:
-        runTest(testId)
+    log_contents = ""
+    if len(tests_to_run) == 0:
+        # runs all benchmarks when none are specified
+        for test_id in benchmarks:
+            log_contents += run_test(test_id)
+    else:
+        # only runs the benchmark/benchmark_sets specified in commandline arguments
+        for test in tests_to_run:
+            if test in benchmark_sets:
+                for sub_test in benchmark_sets[test]:
+                    log_contents += run_test(sub_test)
+            elif test in benchmarks:
+                log_contents += run_test(test)
+            else:
+                print(test, "done not exist.")
+                log_contents += test + " does not exist." + "\n" + "\n"
+
+    if save_log:
+        with open(output_path + "benchmark_log_file.txt", "w") as log:
+            log.write(log_contents)
 
 
 if __name__ == "__main__":
@@ -299,25 +315,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--no-show-video", dest="show_video", action="store_false")
     parser.add_argument("--no-make-video", dest="make_video", action="store_false")
-    parser.add_argument(
-        "--graph-step-times", dest="graph_step_times", action="store_true"
-    )
-    parser.add_argument(
-        "--graph-render-times", dest="graph_render_times", action="store_true"
-    )
-    parser.add_argument(
-        "--graph-collisions", dest="graph_collisions", action="store_true"
-    )
-    # input: --graph collisions render-times step-times
-    # ^ also do for test suites
-    parser.set_defaults(show_video=True, make_video=True)
+    parser.add_argument("--save-to-log", dest="save_log", action="store_true")
+    parser.add_argument("--graph", default=[], nargs="*", type=str, metavar="N")
+    parser.add_argument("--run", default=[], nargs="*", type=str, metavar="N")
+
+    parser.set_defaults(show_video=True, make_video=True, save_log=False)
     args, _ = parser.parse_known_args()
+
     show_video = args.show_video
     make_video = args.make_video
-    graph_step_times = args.graph_step_times
-    graph_render_times = args.graph_render_times
-    graph_collisions = args.graph_collisions
-    print(graph_step_times, graph_render_times, graph_collisions)
+    save_log = args.save_log
+    graphs_to_produce = args.graph
+    tests_to_run = args.run
 
     if make_video and not os.path.exists(output_path):
         os.mkdir(output_path)
