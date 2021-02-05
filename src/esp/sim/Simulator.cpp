08d5c8b7d10b2450aa0815c9875a7f16c3458a53
Vivian Auduong <auduong@fb.com>
2021-02-05 13:09:54 -0600

    Fix

--- src/esp/sim/Simulator.cpp
+++ src/esp/sim/Simulator.cpp
@@ -837,7 +837,7 @@ agent::Agent::ptr Simulator::addAgent(
   esp::sensor::SensorSuite agentSensors =
       esp::sensor::SensorFactory::createSensors(
           agentNode, agentConfig.sensorSpecifications);
-  ag->setSensorSuite(static_cast<esp::sensor::SensorSuite&&>(agentSensors));
+  ag->setSensorSuite(std::move(agentSensors));
   sensorSuite_.merge(ag->getSensorSuite());
   LOG(INFO) << sensorSuite_.getSensors().size();
 
