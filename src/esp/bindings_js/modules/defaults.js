0bb1452df69a2c8d0e64e470c6cac2bfe9507baf
Eric Undersander <eundersander@gmail.com>
2021-03-22 19:53:08 -0600

    remove reference to deprecated amazon bucket

--- src/esp/bindings_js/modules/defaults.js
+++ src/esp/bindings_js/modules/defaults.js
@@ -29,10 +29,7 @@ export const defaultEpisode = {
 
 export const defaultResolution = { height: 480, width: 640 };
 
-export const defaultScene =
-  window.location.href.indexOf("localhost") === -1
-    ? "https://habitat-resources.s3.amazonaws.com/data/scene_datasets/habitat-test-scenes/skokloster-castle.glb"
-    : "skokloster-castle.glb";
+export const defaultScene = "skokloster-castle.glb";
 
 export const infoSemanticFileName = "info_semantic.json";
 
