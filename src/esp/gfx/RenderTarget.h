7236de0277bf5d672896b4f071bd8b9d6df9d2dc
Alexander William Clegg <alexclegg@fb.com>
2021-03-25 14:24:58 -0700

    doc update

--- src/esp/gfx/RenderTarget.h
+++ src/esp/gfx/RenderTarget.h
@@ -88,7 +88,8 @@ class RenderTarget {
 
   /**
    * @brief Called before any draw calls that target this RenderTarget
-   * Clears the framebuffer (optionally to a provided color) and binds it.
+   * Clears the framebuffer to color specified by the CameraSensorSpec and binds
+   * it.
    */
   void renderEnter(sensor::CameraSensor& cameraSensor);
 
