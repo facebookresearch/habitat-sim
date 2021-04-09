c178dc846ca822b70247d158da1fb7f13eeb7f69
ScottCSteinhauser <scsteinhauser@gmail.com>
2021-04-09 10:16:21 -0500

    Edited and generalized the generateDistanceGradientField to generateScalarGradientField

--- src/esp/geo/VoxelUtils.h
+++ src/esp/geo/VoxelUtils.h
@@ -47,14 +47,65 @@ void generateEuclideanDistanceSDF(
 
 /**
  * @brief Generates a Vector3 field where each vector of a cell is the
- * gradient of how the SDF changes.
+ * gradient of how the scalar field changes.
  * @param voxelWrapper The voxelization for the gradient field.
- * @param gridName The name underwhich to register the newly created distance
- * gradient field.
+ * @param scalarGridName The name of the scalar field for which the gradient
+ * will be generated.
+ * @param gradientGridName The name underwhich to register the newly created
+ * scalar gradient field.
  */
-void generateDistanceGradientField(
+void generateScalarGradientField(
     std::shared_ptr<esp::geo::VoxelWrapper>& voxelWrapper,
-    const std::string& gridName = "DistanceGradientField");
+    const std::string& scalarGridName,
+    const std::string& gradientGridName);
+
+/**
+ * @brief Generates a Vector3 field where each vector of a cell is the
+ * gradient of how the scalar field changes.
+ * @param voxelWrapper The voxelization for the gradient field.
+ * @param scalarGridName The name of the scalar field for which the gradient
+ * will be generated.
+ * @param gradientGridName The name underwhich to register the newly created
+ * scalar gradient field.
+ */
+template <typename T>
+void generateScalarGradientField(
+    std::shared_ptr<esp::geo::VoxelWrapper>& voxelWrapper,
+    const std::string& scalarGridName,
+    const std::string& gradientGridName) {
+  auto v_grid = voxelWrapper->getVoxelGrid();
+  auto m_voxelGridDimensions = v_grid->getVoxelGridDimensions();
+
+  // generate the ESDF if not already created
+  assert(v_grid->gridExists(scalarGridName));
+
+  v_grid->addGrid<Mn::Vector3>(gradientGridName);
+  auto gradientGrid = v_grid->getGrid<Mn::Vector3>(gradientGridName);
+  auto scalarGrid = v_grid->getGrid<T>(scalarGridName);
+  std::vector<Mn::Vector3i> neighbors{
+      Mn::Vector3i(1, 0, 0),  Mn::Vector3i(-1, 0, 0), Mn::Vector3i(0, 1, 0),
+      Mn::Vector3i(0, -1, 0), Mn::Vector3i(0, 1, 0),  Mn::Vector3i(0, -1, 0)};
+  for (int i = 0; i < m_voxelGridDimensions[0]; i++) {
+    for (int j = 0; j < m_voxelGridDimensions[1]; j++) {
+      for (int k = 0; k < m_voxelGridDimensions[2]; k++) {
+        Mn::Vector3i index = Mn::Vector3i(i, j, k);
+        Mn::Vector3 result(0, 0, 0);
+        int validVectors = 0;
+        for (auto neighbor : neighbors) {
+          if (v_grid->isValidIndex(neighbor + index)) {
+            float diff =
+                scalarGrid[i + neighbor[0]][j + neighbor[1]][k + neighbor[2]] -
+                scalarGrid[i][j][k];
+            result += Mn::Vector3(neighbor) * diff;
+            validVectors++;
+          }
+        }
+
+        gradientGrid[i][j][k] = result / validVectors;
+      }
+    }
+  }
+}
 
 /**
  * @brief Returns a vector of all filled/true voxels.
