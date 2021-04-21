554612526a7e81b60526ef0e7c03c1384002b225
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-04-21 13:01:30 -0400

    Pre-commit

--- src/esp/bindings_js/bindings_js.cpp
+++ src/esp/bindings_js/bindings_js.cpp
@@ -319,12 +319,13 @@ EMSCRIPTEN_BINDINGS(habitat_sim_bindings_js) {
   em::class_<CubeMapSensorBase, em::base<VisualSensor>>("CubeMapSensorBase")
       .smart_ptr<CubeMapSensorBase::ptr>("CubeMapSensorBase::ptr");
 
-  em::class_<EquirectangularSensor, em::base<CubeMapSensorBase>>("EquirectangularSensor")
+  em::class_<EquirectangularSensor, em::base<CubeMapSensorBase>>(
+      "EquirectangularSensor")
       .smart_ptr<EquirectangularSensor::ptr>("EquirectangularSensor::ptr");
 
   em::class_<FisheyeSensor, em::base<CubeMapSensorBase>>("FisheyeSensor")
       .smart_ptr<FisheyeSensor::ptr>("FisheyeSensor::ptr");
-  
+
   em::class_<CameraSensor, em::base<VisualSensor>>("CameraSensor")
       .smart_ptr<CameraSensor::ptr>("CameraSensor::ptr");
 
