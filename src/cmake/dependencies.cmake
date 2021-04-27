ce04835cd603b12b015d21eddd41acfbe6b0f579
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-04-27 16:12:44 -0400

    Apply suggestions

--- src/cmake/dependencies.cmake
+++ src/cmake/dependencies.cmake
@@ -102,10 +102,13 @@ endif()
 set(RECASTNAVIGATION_DEMO OFF CACHE BOOL "RECASTNAVIGATION_DEMO" FORCE)
 set(RECASTNAVIGATION_TESTS OFF CACHE BOOL "RECASTNAVIGATION_TESTS" FORCE)
 set(RECASTNAVIGATION_EXAMPLES OFF CACHE BOOL "RECASTNAVIGATION_EXAMPLES" FORCE)
-set(BUILD_SHARED_LIBS OFF CACHE BOOL "BUILD_SHARED_LIBS" FORCE)
+# Temp BUILD_SHARED_LIBS override for Recast, has to be reset back after to avoid affecting Bullet (which needs shared) and potentially other submodules
+set(_PREV_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+set(BUILD_SHARED_LIBS OFF)
 include(GNUInstallDirs)
 add_subdirectory("${DEPS_DIR}/recastnavigation/Recast")
 add_subdirectory("${DEPS_DIR}/recastnavigation/Detour")
+set(BUILD_SHARED_LIBS ${_PREV_BUILD_SHARED_LIBS})
 # Needed so that Detour doesn't hide the implementation of the method on dtQueryFilter
 target_compile_definitions(Detour PUBLIC DT_VIRTUAL_QUERYFILTER)
 
