127f88d070655a754bcf3a215d08aa3383b49d15
Aaron Gokaslan <aaronGokaslan@gmail.com>
2021-05-13 16:10:07 -0400

    [CircleCI] Optimize CI Tests (6X speed up on test suite) (#1238)
    * Try using auto pytest cores
    
    * Report test lengths
    
    * Try removing typeguard
    
    * Fix circle cache keys
    
    * Optimize CI. Update pre-commit
    
    * Remove unnecssary pre-commit arg
    
    * Fix habitat-lab flake
    
    * Avoid Torch race condition
    
    * Whoops

--- examples/tutorials/nb_python/ECCV_2020_Interactivity.py
+++ examples/tutorials/nb_python/ECCV_2020_Interactivity.py
@@ -13,7 +13,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.1
+#       jupytext_version: 1.11.2
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
