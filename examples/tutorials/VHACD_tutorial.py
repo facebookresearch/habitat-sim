a6ff11b28f85c52e8cfe24a09264c0d99b5e673b
Alexander Clegg <alexanderwclegg@gmail.com>
2021-06-04 13:31:23 -0700

    [bugfix] update VHACD tutorial script (#1294)
    * update VHACD tutuorial script with recent API changes
    
    * added the tutorial to test_examples

--- examples/tutorials/VHACD_tutorial.py
+++ examples/tutorials/VHACD_tutorial.py
@@ -25,7 +25,7 @@ def make_configuration():
 
     # sensor configurations
     sensor_specs = []
-    sensor_spec = habitat_sim.SensorSpec()
+    sensor_spec = habitat_sim.CameraSensorSpec()
     sensor_spec.uuid = "rgba_camera_1stperson"
     sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
     sensor_spec.resolution = [544, 720]
@@ -99,7 +99,7 @@ def runVHACDSimulation(obj_path):
         obj_id_1 = obj_templates_mgr.load_configs(
             str(os.path.join(data_path, obj_path))
         )[0]
-        obj_template = obj_templates_mgr.get_template_by_ID(obj_id_1)
+        obj_template = obj_templates_mgr.get_template_by_id(obj_id_1)
         obj_handle = obj_template.render_asset_handle
         obj_templates_mgr.register_template(obj_template, force_registration=True)
         obj_ids += [obj_id_1]
@@ -120,7 +120,7 @@ def runVHACDSimulation(obj_path):
         new_obj_template_1 = obj_templates_mgr.get_template_by_handle(new_handle_1)
 
         obj_templates_mgr.register_template(new_obj_template_1, force_registration=True)
-        obj_ids += [new_obj_template_1.ID]
+        obj_ids += [new_obj_template_1.template_id]
 
         # Medium resolution
         params = habitat_sim.VHACDParameters()
@@ -134,7 +134,7 @@ def runVHACDSimulation(obj_path):
         new_obj_template_2 = obj_templates_mgr.get_template_by_handle(new_handle_2)
 
         obj_templates_mgr.register_template(new_obj_template_2, force_registration=True)
-        obj_ids += [new_obj_template_2.ID]
+        obj_ids += [new_obj_template_2.template_id]
 
         # Low resolution
         params = habitat_sim.VHACDParameters()
@@ -148,7 +148,7 @@ def runVHACDSimulation(obj_path):
         new_obj_template_3 = obj_templates_mgr.get_template_by_handle(new_handle_3)
 
         obj_templates_mgr.register_template(new_obj_template_3, force_registration=True)
-        obj_ids += [new_obj_template_3.ID]
+        obj_ids += [new_obj_template_3.template_id]
 
         # now display objects
         cur_objs = []
@@ -193,6 +193,18 @@ def runVHACDSimulation(obj_path):
 
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--no-show-video", dest="show_video", action="store_false")
+    parser.add_argument("--no-make-video", dest="make_video", action="store_false")
+    parser.set_defaults(show_video=True, make_video=True)
+    args, _ = parser.parse_known_args()
+    show_video = args.show_video
+    make_video = args.make_video
+    if make_video and not os.path.exists(output_path):
+        os.mkdir(output_path)
+
     # List of objects you want to execute the VHACD tests on.
     obj_paths = [
         "test_assets/objects/chair.glb",
@@ -202,10 +214,11 @@ if __name__ == "__main__":
     observations = []
     for obj_path in obj_paths:
         observations += runVHACDSimulation(obj_path)["observations"]
-    vut.make_video(
-        observations,
-        "rgba_camera_1stperson",
-        "color",
-        output_path + "VHACD_vid_1",
-        open_vid=True,
-    )
+    if make_video:
+        vut.make_video(
+            observations,
+            "rgba_camera_1stperson",
+            "color",
+            output_path + "VHACD_vid_1",
+            open_vid=show_video,
+        )
