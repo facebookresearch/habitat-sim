761868d8db3c567ebc9e67a5e50308c7d6ab580d
Alexander William Clegg <alexclegg@fb.com>
2021-06-28 13:31:57 -0700

    update sim version to 0.2.0

--- habitat_sim/__init__.py
+++ habitat_sim/__init__.py
@@ -6,7 +6,7 @@
 
 import builtins
 
-__version__ = "0.1.7"
+__version__ = "0.2.0"
 
 if not getattr(builtins, "__HSIM_SETUP__", False):
     # TODO: kept only for compatibiliy with existing code. Please gradually remove
