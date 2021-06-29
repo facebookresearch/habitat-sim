30b0b01e01cf2d0df5f8bb2a78ca38226520cba0
Alexander Clegg <alexanderwclegg@gmail.com>
2021-06-29 15:06:19 -0700

    ReplicaCAD datasources (#1344)
    * Add ReplicaCAD to data downloader
    
    * add ReplicaCAD quickstart Colab/python script

--- habitat_sim/utils/datasets_download.py
+++ habitat_sim/utils/datasets_download.py
@@ -69,6 +69,18 @@ def initialize_test_data_sources(data_path):
             "link": data_path + "scene_datasets/coda",
             "version": "1.0",
         },
+        "replica_cad_dataset": {
+            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_dataset_v1.0.zip",
+            "package_name": "ReplicaCAD_dataset_v1.0.zip",
+            "link": data_path + "replica_cad",
+            "version": "1.0",
+        },
+        "replica_cad_baked_lighting": {
+            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_baked_lighting_v1.0.zip",
+            "package_name": "ReplicaCAD_baked_lighting_v1.0.zip",
+            "link": data_path + "replica_cad_baked_lighting",
+            "version": "1.0",
+        },
         "ycb": {
             "source": "https://dl.fbaipublicfiles.com/habitat/ycb/hab_ycb_v1.0.zip",
             "package_name": "hab_ycb_v1.0.zip",
@@ -92,6 +104,7 @@ def initialize_test_data_sources(data_path):
             "locobot_merged",
             "mp3d_example_scene",
             "coda_scene",
+            "replica_cad_dataset",
             "hab_fetch",
         ]
     }
