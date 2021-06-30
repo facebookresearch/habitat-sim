567e5f237657dcac2de201d4c69bff1c20531659
Alexander Clegg <alexanderwclegg@gmail.com>
2021-06-30 12:00:00 -0700

    Add --no-replace to datasets_download.py  (#1362)
    * add --no-replace option to skip existing data automatically
    
    Co-authored-by: Aaron Gokaslan <aaronGokaslan@gmail.com>

--- habitat_sim/utils/datasets_download.py
+++ habitat_sim/utils/datasets_download.py
@@ -306,7 +306,7 @@ def download_and_place(
     data_path,
     username: Optional[str] = None,
     password: Optional[str] = None,
-    replace=False,
+    replace: Optional[bool] = None,
 ):
     r"""Data-source download function. Validates uid, handles existing data version, downloads data, unpacks, writes version, cleans up."""
     if not data_sources.get(uid):
@@ -327,7 +327,7 @@ def download_and_place(
             f"Existing data source ({uid}) version ({version_tag}) is current. Data located: '{version_dir}'. Symblink: '{link_path}'."
         )
         replace_existing = (
-            replace if replace else prompt_yes_no("Replace versioned data?")
+            replace if replace is not None else prompt_yes_no("Replace versioned data?")
         )
 
         if replace_existing:
@@ -477,8 +477,16 @@ def main(args):
     )
     arg_group2.add_argument(
         "--replace",
+        dest="replace",
+        default=None,
         action="store_true",
         help="If set, existing equivalent versions of any dataset found during download will be deleted automatically. Otherwise user will be prompted before overriding existing data.",
+    )
+    arg_group2.add_argument(
+        "--no-replace",
+        dest="replace",
+        action="store_false",
+        help="If set, existing equivalent versions of any dataset found during download will be skipped automatically. Otherwise user will be prompted before overriding existing data.",
     )
 
     parser.add_argument(
