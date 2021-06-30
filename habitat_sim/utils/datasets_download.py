c06133e70428878547aa2079b1b4cde46fdc33ba
Alexander William Clegg <alexclegg@fb.com>
2021-06-30 11:01:41 -0700

    refactor cleanup

--- habitat_sim/utils/datasets_download.py
+++ habitat_sim/utils/datasets_download.py
@@ -306,8 +306,7 @@ def download_and_place(
     data_path,
     username: Optional[str] = None,
     password: Optional[str] = None,
-    replace=False,
-    prompt_user_replace=True,
+    replace=None,
 ):
     r"""Data-source download function. Validates uid, handles existing data version, downloads data, unpacks, writes version, cleans up."""
     if not data_sources.get(uid):
@@ -328,11 +327,7 @@ def download_and_place(
             f"Existing data source ({uid}) version ({version_tag}) is current. Data located: '{version_dir}'. Symblink: '{link_path}'."
         )
         replace_existing = (
-            replace
-            if replace
-            else prompt_yes_no("Replace versioned data?")
-            if prompt_user_replace
-            else False
+            replace if replace != None else prompt_yes_no("Replace versioned data?")
         )
 
         if replace_existing:
@@ -482,12 +477,15 @@ def main(args):
     )
     arg_group2.add_argument(
         "--replace",
+        dest="replace",
+        default=None,
         action="store_true",
         help="If set, existing equivalent versions of any dataset found during download will be deleted automatically. Otherwise user will be prompted before overriding existing data.",
     )
     arg_group2.add_argument(
         "--no-replace",
-        action="store_true",
+        dest="replace",
+        action="store_false",
         help="If set, existing equivalent versions of any dataset found during download will be skipped automatically. Otherwise user will be prompted before overriding existing data.",
     )
 
@@ -506,7 +504,6 @@ def main(args):
 
     args = parser.parse_args(args)
     replace = args.replace
-    prompt_user_replace = not args.no_replace and not replace
 
     # get a default data_path from git
     data_path = args.data_path
@@ -568,12 +565,7 @@ def main(args):
                 clean_data(uid, data_path)
             else:
                 download_and_place(
-                    uid,
-                    data_path,
-                    args.username,
-                    args.password,
-                    replace,
-                    prompt_user_replace,
+                    uid, data_path, args.username, args.password, replace
                 )
 
 
