15016d51e73d96249566aa542128f8d828fa5f9f
ASzot <me@andrewszot.com>
2021-07-10 18:29:29 -0400

    Removed assertion, must wait until after hablab is updated

--- habitat_sim/robots/mobile_manipulator.py
+++ habitat_sim/robots/mobile_manipulator.py
@@ -364,8 +364,9 @@ class MobileManipulator(RobotInterface):
     @arm_joint_pos.setter
     def arm_joint_pos(self, ctrl: List[float]):
         """Kinematically sets the arm joints and sets the motors to target."""
-        if len(ctrl) != len(self.params.arm_joints):
-            raise ValueError("Control dimension does not match joint dimension")
+        # Has to be added back in after the Habitat Lab commit goes through.
+        # if len(ctrl) != len(self.params.arm_joints):
+        #    raise ValueError("Control dimension does not match joint dimension")
 
         joint_positions = self.sim_obj.joint_positions
         for i, jidx in enumerate(self.params.arm_joints):
