06a3b063b0df845923080b2064870508fdb47fee
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-07-15 13:59:41 -0400

    Fix duplicate version appending

--- conda-build/macos_matrix_builder.py
+++ conda-build/macos_matrix_builder.py
@@ -65,7 +65,7 @@ def main():
         env = os.environ.copy()
 
         # including a timestamp in anticipation of nightly builds
-        env["VERSION"] = habitat_sim.__version__ + time.strftime(".%Y.%m.%d")
+        env["VERSION"] = habitat_sim.__version__
         if args.nightly:
             env["VERSION"] = env["VERSION"] + time.strftime(".%Y.%m.%d")
         env["WITH_BULLET"] = "0"
