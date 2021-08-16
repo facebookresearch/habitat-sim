43fe816713d4e1cf4a914dc8a7961333fc2c17cf
Eric Undersander <eundersander@gmail.com>
2021-08-16 14:13:13 -0700

    add definition for LoggingContext::DEFAULT_LEVEL  (#1435)
    * also definition for LOGGING_ENV_VAR_NAME

--- src/esp/core/logging.cpp
+++ src/esp/core/logging.cpp
@@ -21,6 +21,9 @@ using Cr::Containers::Literals::operator""_s;
 namespace esp {
 namespace logging {
 
+constexpr const char* LoggingContext::LOGGING_ENV_VAR_NAME;
+constexpr LoggingLevel LoggingContext::DEFAULT_LEVEL;
+
 static_assert(uint8_t(Subsystem::NumSubsystems) ==
                   (sizeof(subsystemNames) / sizeof(subsystemNames[0])),
               "Missing a subsystem name.");
