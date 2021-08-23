22b6680a1dd1b1d75f9902283c0c8feb67ebb353
Eric Undersander <eundersander@gmail.com>
2021-08-22 22:24:11 -0700

    add batched_sim logging subsystem

--- src/esp/core/logging.h
+++ src/esp/core/logging.h
@@ -42,6 +42,7 @@ enum class Subsystem : uint8_t {
   assets,
   sensor,
   agent,
+  batched_sim,
 
   /**
    * @brief Not a subsystem.  Simply tracks how many subsystems there are.
@@ -52,9 +53,9 @@ enum class Subsystem : uint8_t {
 };
 
 constexpr const char* subsystemNames[] = {
-    "Default", "Gfx", "Scene", "Sim",  "Physics", "Nav",    "Metadata",
-    "Geo",     "IO",  "URDF",  "Core", "Assets",  "Sensor", "Agent",
-};
+    "Default", "Gfx",      "Scene",  "Sim",   "Physics",
+    "Nav",     "Metadata", "Geo",    "IO",    "URDF",
+    "Core",    "Assets",   "Sensor", "Agent", "Batched_Sim"};
 
 Subsystem subsystemFromName(Corrade::Containers::StringView name);
 
@@ -102,6 +103,7 @@ ESP_ADD_SUBSYSTEM_FN(core)
 ESP_ADD_SUBSYSTEM_FN(assets)
 ESP_ADD_SUBSYSTEM_FN(sensor)
 ESP_ADD_SUBSYSTEM_FN(agent)
+ESP_ADD_SUBSYSTEM_FN(batched_sim)
 
 namespace esp {
 namespace io {
