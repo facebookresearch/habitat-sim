50be538b6b3fb6fda9247fa08fe6a42f631792a9
John Turner <7strbass@gmail.com>
2021-09-13 18:09:53 -0400

    --convert SceneGraphTest to use Corrade test suite

--- src/tests/SceneGraphTest.cpp
+++ src/tests/SceneGraphTest.cpp
@@ -2,40 +2,45 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-#include <gtest/gtest.h>
+#include <Corrade/TestSuite/Tester.h>
 
 #include "esp/scene/SceneGraph.h"
 
 using esp::gfx::DrawableGroup;
 using esp::scene::SceneGraph;
 
-class SceneGraphTest : public ::testing::Test {
- protected:
-  void SetUp() override { numInitialGroups = g.getDrawableGroups().size(); }
-
+namespace Test {
+struct SceneGraphTest : Cr::TestSuite::Tester {
+  explicit SceneGraphTest();
+  void testGetDrawableGroup();
+  void testDeleteDrawableGroup();
   esp::logging::LoggingContext loggingContext_;
   size_t numInitialGroups;
   SceneGraph g;
   const std::string groupName = "testGroup";
 };
-
-TEST_F(SceneGraphTest, GetDrawableGroup) {
-  EXPECT_EQ(g.getDrawableGroup("not created"), nullptr);
-
-  DrawableGroup* group = g.createDrawableGroup(groupName);
-  ASSERT_NE(group, nullptr);
-  EXPECT_EQ(g.getDrawableGroups().size(), numInitialGroups + 1);
-  ASSERT_EQ(g.getDrawableGroup(groupName), group);
+SceneGraphTest::SceneGraphTest() {
+  numInitialGroups = g.getDrawableGroups().size();
+  addTests({&SceneGraphTest::testGetDrawableGroup,
+            &SceneGraphTest::testDeleteDrawableGroup});
 }
 
-TEST_F(SceneGraphTest, DeleteDrawableGroup) {
-  EXPECT_FALSE(g.deleteDrawableGroup("not created"));
+void SceneGraphTest::testGetDrawableGroup() {
+  CORRADE_VERIFY(g.getDrawableGroup("not created") == nullptr);
 
   DrawableGroup* group = g.createDrawableGroup(groupName);
-  ASSERT_NE(group, nullptr);
-  ASSERT_EQ(g.getDrawableGroup(groupName), group);
-
-  ASSERT_TRUE(g.deleteDrawableGroup(groupName));
-  EXPECT_EQ(g.getDrawableGroups().size(), numInitialGroups);
-  ASSERT_EQ(g.getDrawableGroup(groupName), nullptr);
+  CORRADE_VERIFY(group != nullptr);
+  CORRADE_COMPARE(g.getDrawableGroups().size(), numInitialGroups + 1);
+  CORRADE_COMPARE(g.getDrawableGroup(groupName), group);
 }
+
+void SceneGraphTest::testDeleteDrawableGroup() {
+  CORRADE_VERIFY(!g.deleteDrawableGroup("not created"));
+
+  CORRADE_VERIFY(g.deleteDrawableGroup(groupName));
+  CORRADE_COMPARE(g.getDrawableGroups().size(), numInitialGroups);
+  CORRADE_VERIFY(g.getDrawableGroup(groupName) == nullptr);
+}
+}  // namespace Test
+
+CORRADE_TEST_MAIN(Test::SceneGraphTest)
