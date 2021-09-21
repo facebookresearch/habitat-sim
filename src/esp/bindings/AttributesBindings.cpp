d57bffaf082a8ee46714081dc1c3aa9c44b60c5d
John Turner <7strbass@gmail.com>
2021-09-21 09:58:44 -0400

    --use ESP_CHECK for python error instead of abort.

--- src/esp/bindings/AttributesBindings.cpp
+++ src/esp/bindings/AttributesBindings.cpp
@@ -88,10 +88,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const std::string& val) {
-            CORRADE_ASSERT(false,
-                           "Attributes should only use named properties or "
-                           "subconfigurations to set string values, to "
-                           "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set string values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
       .def(
@@ -99,10 +99,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const char* val) {
-            CORRADE_ASSERT(false,
-                           "Attributes should only use named properties or "
-                           "subconfigurations to set string values, to "
-                           "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set string values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
       .def(
@@ -110,10 +110,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const int val) {
-            CORRADE_ASSERT(false,
-                           "Attributes should only use named properties or "
-                           "subconfigurations to set integer values, to "
-                           "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set integer values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
       .def(
@@ -121,10 +121,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const double val) {
-            CORRADE_ASSERT(false,
-                           "Attributes should only use named properties or "
-                           "subconfigurations to set floating-point values, to "
-                           "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set floating-point values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
       .def(
@@ -132,10 +132,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const bool val) {
-            CORRADE_ASSERT(false,
-                           "Attributes should only use named properties or "
-                           "subconfigurations to set boolean values, to "
-                           "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set boolean values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
       .def(
@@ -143,11 +143,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const Magnum::Quaternion& val) {
-            CORRADE_ASSERT(
-                false,
-                "Attributes should only use named properties or "
-                "subconfigurations to set Nagnum::Quaternion values, to "
-                "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set Nagnum::Quaternion values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
       .def(
@@ -155,11 +154,10 @@ void initAttributesBindings(py::module& m) {
           [](CORRADE_UNUSED AbstractAttributes& self,
              CORRADE_UNUSED const std::string& key,
              CORRADE_UNUSED const Magnum::Vector3& val) {
-            CORRADE_ASSERT(
-                false,
-                "Attributes should only use named properties or "
-                "subconfigurations to set Magnum::Vector3 values, to "
-                "guarantee essential value type integrity.", );
+            ESP_CHECK(false,
+                      "Attributes should only use named properties or "
+                      "subconfigurations to set Magnum::Vector3 values, to "
+                      "guarantee essential value type integrity.");
           },
           "key"_a, "value"_a)
 
