f448c07806c085a29d798b907a6bbfbd28793b21
Juan Jesus Rodriguez <juanjrodriguez@fb.com>
2021-09-27 17:04:05 -0700

    added blit_rgba_to_default()

--- examples/viewer.py
+++ examples/viewer.py
@@ -16,8 +16,12 @@ class SkeletonPythonViewer(Application):
         configuration = self.Configuration()
         configuration.title = "Skeleton Viewer Application"
         Application.__init__(self, configuration)
-        self.viewport_size = gl.default_framebuffer.viewport.size()
+        self.viewport_size = gl.default_framebuffer.viewport.size() #viewport size
 
+        # attempt to set proper width and height
+        sim_settings["width"] = self.viewport_size[1]
+        sim_settings["height"] = self.viewport_size[0]
+        
         self.cfg = make_cfg(sim_settings)
         self.sim = habitat_sim.Simulator(self.cfg)
         self.agent_id = sim_settings["default_agent"]
@@ -27,7 +31,13 @@ class SkeletonPythonViewer(Application):
         gl.default_framebuffer.clear(
             gl.FramebufferClear.COLOR | gl.FramebufferClear.DEPTH
         )
-        obs = self.sim.get_sensor_observations()
+
+        self.sim._sensors["color_sensor"].draw_observation() # attempt to draw_observation
+
+        # added to use blit_rgba_to_default()
+        render_target = self.sim.agents[0]._sensors["color_sensor"].render_target
+        render_target.blit_rgba_to_default()
+
         self.swap_buffers()
 
 
