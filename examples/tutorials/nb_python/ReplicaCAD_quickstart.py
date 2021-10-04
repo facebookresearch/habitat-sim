40cf8055a4f1533900e22d7d02538d8e10895ec7
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-10-04 12:19:16 -0400

    Autoupdate hooks and freeze clang-format

--- examples/tutorials/nb_python/ReplicaCAD_quickstart.py
+++ examples/tutorials/nb_python/ReplicaCAD_quickstart.py
@@ -14,7 +14,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.4
+#       jupytext_version: 1.13.0
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
