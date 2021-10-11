a9b8650e60d1e53337d078ec560626b37c43aa51
Aaron Gokaslan <aaronGokaslan@gmail.com>
2021-10-11 11:16:22 -0400

    (bugfix): Fix conda and docs nightly jobs (#1531)
    * Fix conda and doc builds
    
    * Unpin conda
    
    * Re-use docker for test
    
    * Simplify
    
    * Try to update to miniconda3
    
    * Unpin conda-build
    
    * Remove unnecessary source
    
    * Revert Docker change
    
    * Try to fix
    
    * Try to fix again
    
    * Try to be more aggressive
    
    * Fallback
    
    * Try to fix install
    
    * Reload bashrc
    
    * Put raw eval in
    
    * Examine what broke
    
    * Fix circleci
    
    * load bashrc explicitly
    
    * Remove debugging print
    
    * Revert errant change
    
    * Update .circleci/config.yml
    
    Co-authored-by: Erik Wijmans <etw@gatech.edu>
    
    Co-authored-by: Erik Wijmans <etw@gatech.edu>

--- conda-build/common/install_conda.sh
+++ conda-build/common/install_conda.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # Anaconda
-wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-chmod +x  Miniconda2-latest-Linux-x86_64.sh
-./Miniconda2-latest-Linux-x86_64.sh -b -p /opt/conda
-rm Miniconda2-latest-Linux-x86_64.sh
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x  Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda
+rm Miniconda3-latest-Linux-x86_64.sh
 export PATH=/opt/conda/bin:$PATH
-conda install -y anaconda-client git gitpython ninja conda-build=3.18.9 # last version that works with our setup
+conda install -y anaconda-client git gitpython ninja conda-build # conda-build=3.18.9 # last version that works with our setup
 conda remove -y --force patchelf
