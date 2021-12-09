08ca2831a07e56734f2138520141bbe2d6fbf299
Simar Kareer <skareer@outlook.com>
2021-12-09 16:11:47 -0500

    Update src/esp/physics/bullet/BulletArticulatedObject.cpp
    Co-authored-by: Alexander Clegg <alexanderwclegg@gmail.com>

--- src/esp/physics/bullet/BulletArticulatedObject.cpp
+++ src/esp/physics/bullet/BulletArticulatedObject.cpp
@@ -442,7 +442,7 @@ std::vector<float> BulletArticulatedObject::getJointPositions() {
 
 std::vector<float> BulletArticulatedObject::getJointMotorTorques(
     double fixedTimeStep) {
-  std::vector<float> torques(btMultiBody_->getNumLinks());
+  std::vector<float> torques(btMultiBody_->getNumDofs());
   for (auto& motor : jointMotors_) {
     btMultibodyLink& btLink = btMultiBody_->getLink(motor.second->index);
     // int startIndex = velocities ? btLink.m_dofOffset : btLink.m_cfgOffset;
