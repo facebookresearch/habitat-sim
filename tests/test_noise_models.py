b14c0e91ade5833b7c10a6bf6ca7c66aa0f5db76
Erik Wijmans <ewijmans2@gmail.com>
2022-03-14 11:12:00 -0400

    Fix noise model (#1684)
    * Fix redwood depth noise model
    
    * Fix redwood depth noise model
    
    * Set warp size too
    
    * Slight change

--- tests/test_noise_models.py
+++ tests/test_noise_models.py
@@ -19,7 +19,7 @@ from habitat_sim.sensors.noise_models.redwood_depth_noise_model import (
 
 @pytest.mark.gfxtest
 @pytest.mark.skipif(not habitat_sim.cuda_enabled, reason="Test requires cuda")
-@pytest.mark.parametrize("noise_multiplier,tolerance", [(0.0, 1e-5), (1.0, 5e-2)])
+@pytest.mark.parametrize("noise_multiplier,tolerance", [(0.0, 1e-5), (1.0, 1e-2)])
 def test_compare_gpu_cpu_redwood_depth(noise_multiplier: float, tolerance: float):
     depth = np.linspace(0, 20, num=(256 * 256), dtype=np.float32).reshape(256, 256)
 
@@ -37,7 +37,7 @@ def test_compare_gpu_cpu_redwood_depth(noise_multiplier: float, tolerance: float
         noise_multiplier=noise_multiplier,
     )
 
-    NUM_SIMS = 20
+    NUM_SIMS = 100
     cuda_depths = [cuda_impl(depth) for _ in range(NUM_SIMS)]
     cpu_depths = [cpu_impl.simulate(depth) for _ in range(NUM_SIMS)]
 
