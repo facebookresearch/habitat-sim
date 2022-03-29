29943eb79527d4c7dbc28106615eba3ca231cb34
Alexander William Clegg <alexclegg@fb.com>
2022-03-29 12:40:59 -0700

    add docstring and sensor uuid passthrough

--- src_python/habitat_sim/simulator.py
+++ src_python/habitat_sim/simulator.py
@@ -425,7 +425,7 @@ class Simulator(SimulatorBackend):
             agent_sensorsuite = self.__sensors[agent_id]
             for _sensor_uuid, sensor in agent_sensorsuite.items():
                 sensor.draw_observation()
-                self.debug_draw()
+                self.debug_draw(_sensor_uuid)
 
         # As backport. All Dicts are ordered in Python >= 3.7
         observations: Dict[int, ObservationDict] = OrderedDict()
@@ -438,8 +438,12 @@ class Simulator(SimulatorBackend):
             return next(iter(observations.values()))
         return observations
 
-    def debug_draw(self):
-        pass
+    def debug_draw(self, sensor_uuid: Optional[str] = None) -> None:
+        r"""Override this method in derived Simulator class to add optional, application specific debug line drawing commands to the sensor output.
+        See Simulator.get_debug_line_render().
+
+        :param sensor_uuid: The uuid of the sensor being rendered to optionally limit debug drawing to specific visualizations (e.g. a third person eval camera)
+        """
 
     @property
     def _default_agent(self) -> Agent:
