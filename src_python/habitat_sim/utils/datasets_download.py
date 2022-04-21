ef7b9c3f2dea04e0c09fb388bad6ffea9366a39c
Alexander William Clegg <alexclegg@fb.com>
2022-04-21 09:10:26 -0700

    replicaCAD baked lighting v1.5 on S3

--- src_python/habitat_sim/utils/datasets_download.py
+++ src_python/habitat_sim/utils/datasets_download.py
@@ -115,10 +115,10 @@ def initialize_test_data_sources(data_path):
             "version": "1.4",
         },
         "replica_cad_baked_lighting": {
-            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_baked_lighting_v1.4.zip",
-            "package_name": "ReplicaCAD_baked_lighting_v1.4.zip",
+            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_baked_lighting_v1.5.zip",
+            "package_name": "ReplicaCAD_baked_lighting_v1.5.zip",
             "link": data_path + "replica_cad_baked_lighting",
-            "version": "1.4",
+            "version": "1.5",
         },
         "ycb": {
             "source": "https://dl.fbaipublicfiles.com/habitat/ycb/hab_ycb_v1.1.zip",
