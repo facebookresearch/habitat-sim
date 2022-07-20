91e30e81122c5127d13dacc7815fe505747eb78d
Vladimír Vondruš <mosra@centrum.cz>
2022-07-20 21:35:50 +0200

    clang-format!

--- src/esp/bindings/EnumOperators.h
+++ src/esp/bindings/EnumOperators.h
@@ -11,15 +11,29 @@ namespace esp {
 
 /* Pybind's py::arithmetic() doesn't work on strongly-typed enums with custom
    operators */
-template<class T> void pybindEnumOperators(pybind11::enum_<T>& e) {
-    e
-        .def("__or__", [](const T& a, const T& b) { return T(typename std::underlying_type<T>::type(a | b)); })
-        .def("__and__", [](const T& a, const T& b) { return T(typename std::underlying_type<T>::type(a & b)); })
-        .def("__xor__", [](const T& a, const T& b) { return T(typename std::underlying_type<T>::type(a ^ b)); })
-        .def("__invert__", [](const T& a) { return T(typename std::underlying_type<T>::type(~a)); })
-        .def("__bool__", [](const T& a) { return bool(typename std::underlying_type<T>::type(a)); });
+template <class T>
+void pybindEnumOperators(pybind11::enum_<T>& e) {
+  e.def("__or__",
+        [](const T& a, const T& b) {
+          return T(typename std::underlying_type<T>::type(a | b));
+        })
+      .def("__and__",
+           [](const T& a, const T& b) {
+             return T(typename std::underlying_type<T>::type(a & b));
+           })
+      .def("__xor__",
+           [](const T& a, const T& b) {
+             return T(typename std::underlying_type<T>::type(a ^ b));
+           })
+      .def("__invert__",
+           [](const T& a) {
+             return T(typename std::underlying_type<T>::type(~a));
+           })
+      .def("__bool__", [](const T& a) {
+        return bool(typename std::underlying_type<T>::type(a));
+      });
 }
 
-}
+}  // namespace esp
 
 #endif
