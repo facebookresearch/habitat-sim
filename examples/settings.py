644ac9b43b2b770d11ddf6c5e65566583115cfe0
Joao Pedro Araujo <12978228+jaraujo98@users.noreply.github.com>
2022-08-30 15:23:15 -0700

    Separate example.py specific settings

--- examples/settings.py
+++ examples/settings.py
@@ -2,4 +2,22 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from habitat_sim.utils.settings import *
+from habitat_sim.utils.settings import default_sim_settings, make_cfg  # noqa: F401
+
+example_settings = {
+    "save_png": False,  # save the pngs to disk (default: OFF)
+    "print_semantic_scene": False,
+    "print_semantic_mask_stats": False,
+    "compute_shortest_path": False,
+    "compute_action_shortest_path": False,
+    "scene": "data/scene_datasets/habitat-test-scenes/skokloster-castle.glb",
+    "test_scene_data_url": "http://dl.fbaipublicfiles.com/habitat/habitat-test-scenes.zip",
+    "goal_position": [5.047, 0.199, 11.145],
+    "enable_physics": False,
+    "enable_gfx_replay_save": False,
+    "physics_config_file": "./data/default.physics_config.json",
+    "num_objects": 10,
+    "test_object_index": 0,
+    "frustum_culling": True,
+}
+default_sim_settings.update(example_settings)
--- src_python/habitat_sim/utils/settings.py
+++ src_python/habitat_sim/utils/settings.py
@@ -6,7 +6,6 @@ import habitat_sim
 import habitat_sim.agent
 
 default_sim_settings = {
-    # settings shared by example.py and benchmark.py
     "max_frames": 1000,
     "width": 640,
     "height": 480,
@@ -27,21 +26,6 @@ default_sim_settings = {
     "equirect_semantic_sensor": False,
     "seed": 1,
     "silent": False,  # do not print log info (default: OFF)
-    # settings exclusive to example.py
-    "save_png": False,  # save the pngs to disk (default: OFF)
-    "print_semantic_scene": False,
-    "print_semantic_mask_stats": False,
-    "compute_shortest_path": False,
-    "compute_action_shortest_path": False,
-    "scene": "data/scene_datasets/habitat-test-scenes/skokloster-castle.glb",
-    "test_scene_data_url": "http://dl.fbaipublicfiles.com/habitat/habitat-test-scenes.zip",
-    "goal_position": [5.047, 0.199, 11.145],
-    "enable_physics": False,
-    "enable_gfx_replay_save": False,
-    "physics_config_file": "./data/default.physics_config.json",
-    "num_objects": 10,
-    "test_object_index": 0,
-    "frustum_culling": True,
 }
 
 # build SimulatorConfiguration
