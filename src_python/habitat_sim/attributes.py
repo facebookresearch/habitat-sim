8cbc1204866137a566cf656b25179cd517ecb901
Alexander Clegg <alexanderwclegg@gmail.com>
2022-09-13 15:19:47 -0700

    [docs] SceneDataset JSON (#1863)
    * added docstrings and new doc page entries for SceneDataset JSON and Attributes configuration.

--- src_python/habitat_sim/attributes.py
+++ src_python/habitat_sim/attributes.py
@@ -2,6 +2,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+"""
+Attributes objects store metadata relevant to a specific type of simulation objects for programmatic manipulation and instantiation (e.g. a blueprint).
+
+Note: SceneDatasetAttributes and SceneInstanceAttributes are not publicly exposed.
+"""
+
+
 from habitat_sim._ext.habitat_sim_bindings import (
     CapsulePrimitiveAttributes,
     ConePrimitiveAttributes,
