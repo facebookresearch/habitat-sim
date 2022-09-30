3a69c545a10455837fc48739b795b3df533269d7
jrreyna <johnreyna@fb.com>
2022-09-30 15:47:19 -0700

    added code to print out info from the object attributes manager and asset template manager (to see what kind of data is in there)

--- tools/dataset_object_processor.py
+++ tools/dataset_object_processor.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import git
 
@@ -32,15 +32,10 @@ class PrintColors:
     BLUE = "\033[94m"
     CYAN = "\033[96m"
     MAGENTA = "\u001b[35m"
-    YELLOW = "\u001b[33m"
     BROWN = "\033[0;33m"
     LIGHT_RED = "\033[1;31m"
-    LIGHT_GREEN = "\033[1;32m"
-    LIGHT_BLUE = "\033[1;34m"
     LIGHT_PURPLE = "\033[1;35m"
     LIGHT_CYAN = "\033[1;36m"
-    LIGHT_WHITE = "\033[1;37m"
-    LIGHT_GRAY = "\033[0;37m"
     TEST = "\u001a[35m"
     WARNING = "\033[93m"
     FAIL = "\033[91m"
@@ -56,42 +51,6 @@ def print_in_color(print_string="", color=PrintColors.WHITE) -> None:
     print(color + print_string + PrintColors.ENDC)
 
 
-def build_parser(
-    parser: Optional[argparse.ArgumentParser] = None,
-) -> argparse.ArgumentParser:
-    if parser is None:
-        parser = argparse.ArgumentParser(
-            description="Tool to evaluate all objects in a dataset. Assesses CPU, GPU, mesh size, "
-            " and other characteristics to determine if an object will be problematic when using it"
-            " in a simulation",
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-
-    parser = argparse.ArgumentParser()
-
-    # optional arguments
-    parser.add_argument(
-        "--scene",
-        default="./data/test_assets/scenes/simple_room.glb",
-        type=str,
-        help='scene/stage file to load (default: "./data/test_assets/scenes/simple_room.glb")',
-    )
-    parser.add_argument(
-        "--dataset",
-        default="./data/objects/ycb/ycb.scene_dataset_config.json",
-        type=str,
-        metavar="DATASET",
-        help='dataset configuration file to use (default: "./data/objects/ycb/ycb.scene_dataset_config.json")',
-    )
-    parser.add_argument(
-        "--disable_physics",
-        action="store_true",
-        help="disable physics simulation (default: False)",
-    )
-
-    return parser
-
-
 def parse_dataset(sim: habitat_sim.Simulator = None, dataset_path: str = None) -> None:
     if sim is None:
         return
@@ -102,12 +61,68 @@ def parse_dataset(sim: habitat_sim.Simulator = None, dataset_path: str = None) -
         or metadata_mediator.dataset_exists(dataset_path) is False
     ):
         return
+
+    # ----------------------------------------------------------------
     active_dataset: str = metadata_mediator.active_dataset
-    print_in_color("* " * 39, PrintColors.CYAN)
-    print_in_color(f"{active_dataset}\n", PrintColors.CYAN)
-    dataset_report: str = metadata_mediator.dataset_report(dataset_path)
     print_in_color("* " * 39, PrintColors.BLUE)
-    print_in_color(f"{dataset_report}\n", PrintColors.BLUE)
+    print_in_color(f"{active_dataset}\n", PrintColors.BLUE)
+
+    # ----------------------------------------------------------------
+    dataset_report: str = metadata_mediator.dataset_report(dataset_path)
+    print_in_color("* " * 39, PrintColors.CYAN)
+    print_in_color(f"{dataset_report}\n", PrintColors.CYAN)
+
+    # ----------------------------------------------------------------
+    scene_handles: List[str] = metadata_mediator.get_scene_handles()
+    print_in_color("* " * 39, PrintColors.LIGHT_PURPLE)
+    for handle in scene_handles:
+        print_in_color(f"{handle}\n", PrintColors.LIGHT_PURPLE)
+
+    # ----------------------------------------------------------------
+    urdf_paths = metadata_mediator.urdf_paths
+    urdf_paths_list = list(urdf_paths.keys())
+    print_in_color("-" * 39, PrintColors.MAGENTA)
+    print_in_color(f"num urdf paths: {len(urdf_paths_list)}\n", PrintColors.MAGENTA)
+    for name in urdf_paths_list:
+        print_in_color("-" * 39, PrintColors.MAGENTA)
+        print_in_color(f"{name}\n", PrintColors.MAGENTA)
+
+    # ----------------------------------------------------------------
+    # Get object attribute manager for test objects from dataset,
+    # load the dataset, store all the object template handles in a list,
+    object_attributes_manager = sim.get_object_template_manager()
+    object_attributes_manager.load_configs(dataset_path)
+    object_template_handles = object_attributes_manager.get_file_template_handles("")
+    print_in_color("-" * 39, PrintColors.RED)
+    print_in_color(
+        f"number of ojects in dataset: {len(object_template_handles)}",
+        PrintColors.RED,
+    )
+
+    # ----------------------------------------------------------------
+    asset_template_manager = metadata_mediator.asset_template_manager
+    print_in_color(
+        f"\nnumber of primitive asset templates: {asset_template_manager.get_num_templates()}",
+        PrintColors.BROWN,
+    )
+
+    # ----------------------------------------------------------------
+    print_in_color("-" * 39, PrintColors.BROWN)
+    template_handles = asset_template_manager.get_template_handles()
+    templates_info = asset_template_manager.get_templates_info()
+    for (handle, info) in zip(template_handles, templates_info):
+        print_in_color(
+            f"-{handle}",
+            PrintColors.GREEN,
+        )
+        print_in_color(
+            f"{info}\n",
+            PrintColors.BROWN + PrintColors.UNDERLINE,
+        )
+
+    # ----------------------------------------------------------------
+    rigid_object_manager = sim.get_rigid_object_manager()
+    print_in_color(rigid_object_manager.get_objects_CSV_info(), PrintColors.CYAN)
 
 
 def make_configuration(sim_settings):
@@ -133,6 +148,41 @@ def make_configuration(sim_settings):
     agent_cfg.sensor_specifications = [camera_sensor_spec]
 
     return habitat_sim.Configuration(sim_cfg, [agent_cfg])
+
+
+def build_parser(
+    parser: Optional[argparse.ArgumentParser] = None,
+) -> argparse.ArgumentParser:
+    if parser is None:
+        parser = argparse.ArgumentParser(
+            description="Tool to evaluate all objects in a dataset. Assesses CPU, GPU, mesh size, "
+            " and other characteristics to determine if an object will be problematic when using it"
+            " in a simulation",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        )
+    parser = argparse.ArgumentParser()
+
+    # optional arguments
+    parser.add_argument(
+        "--scene",
+        default="./data/test_assets/scenes/simple_room.glb",
+        type=str,
+        help='scene/stage file to load (default: "./data/test_assets/scenes/simple_room.glb")',
+    )
+    parser.add_argument(
+        "--dataset",
+        default="./data/objects/ycb/ycb.scene_dataset_config.json",
+        type=str,
+        metavar="DATASET",
+        help='dataset configuration file to use (default: "./data/objects/ycb/ycb.scene_dataset_config.json")',
+    )
+    parser.add_argument(
+        "--disable_physics",
+        action="store_true",
+        help="disable physics simulation (default: False)",
+    )
+
+    return parser
 
 
 def main() -> None:
