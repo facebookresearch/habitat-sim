c4ddbc19078f23373031c7c6da803e428f141885
NakuraMino <33564642+NakuraMino@users.noreply.github.com>
2022-10-25 16:39:16 -0700

    changed header from facebook to meta platforms (#1913)
    * changed header from facebook to meta platforms
    
    Co-authored-by: Mino Nakura <minonakura@fb.com>

--- src_python/habitat_sim/utils/compare_profiles.py
+++ src_python/habitat_sim/utils/compare_profiles.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 r"""Helper script to print summary timings for one or more profiles captured by
