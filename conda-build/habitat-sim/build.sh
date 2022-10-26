5fb04078b0b8432dc4a88ec186a2b7af74163be1
NakuraMino <33564642+NakuraMino@users.noreply.github.com>
2022-10-26 15:41:02 -0700

    Add comment blocks (#1917)
    * adding comment blocks and changing facebook to meta platforms
    
    * adding more copyright blocks
    
    * cmakeslist and css comments
    
    * found more files without copyright block using grep
    
    Co-authored-by: Mino <minonakura@fb.com>

--- conda-build/habitat-sim/build.sh
+++ conda-build/habitat-sim/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 build_args=(--skip-install-magnum)
 build_args+=("--vhacd")
