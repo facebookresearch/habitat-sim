4f873f759996d20af00a1d6d8b31b79dbf421cc2
Mino <minonakura@fb.com>
2022-10-26 10:58:40 -0700

    adding comment blocks and changing facebook to meta platforms

--- src_python/habitat_sim/sensors/sensor_suite.py
+++ src_python/habitat_sim/sensors/sensor_suite.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.from typing import Dict
+
 from typing import Dict
 
 from habitat_sim import bindings as hsim
