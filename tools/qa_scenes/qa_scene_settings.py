a2d8c44a5f6283978d29119f4dd533fe1c030b6a
aclegg3 <alexanderwclegg@gmail.com>
2023-02-13 14:15:41 -0800

    clean-up qa_scene_settings

--- tools/qa_scenes/qa_scene_settings.py
+++ tools/qa_scenes/qa_scene_settings.py
@@ -2,25 +2,26 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import habitat_sim
 from habitat_sim.utils.settings import default_sim_settings
-from habitat_sim.utils.settings import make_cfg as _make_cfg
 
 qa_scene_settings = {
     # These can be overridden in a qa_scene_config.json file in the
-    # "./tools/qa_scenes/configs" directory, otherwise it will use these
-    # as defaults
-    "scene_dataset_config_file": "scene_datasets/replica_cad/replicaCAD.scene_dataset_config.json",
+    # "./tools/qa_scenes/configs" directory
+    # standard SimulatorConfiguration settings
+    "scene_dataset_config_file": "replica_cad/replicaCAD.scene_dataset_config.json",
     "color_sensor": True,
     "semantic_sensor": True,
     "depth_sensor": True,
     "width": 256,
     "height": 256,
     "enable_physics": True,
+    # set the desired scenes
     "start_scene_index": 0,
+    # -1 indicates end of list
     "end_scene_index": -1,
     "output_file_prefix": "default",
     "generate_navmesh": True,
+    "navmesh_include_static": True,
     "render_sensor_obs": True,
     "make_video": True,
     "show_video": True,
@@ -31,7 +32,7 @@ qa_scene_settings = {
     # files, but rather, you can look at the scene and press "c" to run the collision
     # test and watch it
     "run_viewer": False,
-    "scene": "scene_datasets/replica_cad/configs/scenes/apt_1.scene_instance.json",
+    "scene": "apt_0",
     "agent_pos": [-1.6971087, 0.11937292, -1.0771935],
     "agent_rot": [0.0, 0.0, 1.0, 0.0],  # (angle, axis.x, axis.y, axis.z)
     # for output json storing detailed scene information. "render_test_max_time"
@@ -60,7 +61,7 @@ qa_scene_settings = {
         "test_time": 0.0,
         "exceeds_time_threshold": False,
     },
-    #
+    # settling test
     "sleep_test_steps_per_sec": 60,
     "asset_sleep_test_duration_seconds": 10.0,
     "asleep_test_json_entry": {
@@ -75,24 +76,3 @@ qa_scene_settings = {
     "fps": 60.0,
 }
 default_sim_settings.update(qa_scene_settings)
-
-
-def make_cfg(settings):
-    # Start with the built-in configuration, then modify as
-    # appropriate.
-    cfg = _make_cfg(settings)
-    sim_cfg = cfg.sim_cfg
-    agent_cfg = cfg.agents[0]
-
-    if not settings["silent"]:
-        print("sim_cfg.physics_config_file = " + sim_cfg.physics_config_file)
-
-    # override action space to no-op to test physics
-    if sim_cfg.enable_physics:
-        agent_cfg.action_space = {
-            "move_forward": habitat_sim.agent.ActionSpec(
-                "move_forward", habitat_sim.agent.ActuationSpec(amount=0.0)
-            )
-        }
-
-    return cfg
