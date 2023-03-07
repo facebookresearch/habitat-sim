105a455a490e3790857519f8f4915287d05f56c6
aclegg3 <alexanderwclegg@gmail.com>
2023-03-07 11:07:01 -0800

    Implement stateful API class for easier iteration and export of batch results

--- tools/collision_shape_automation.py
+++ tools/collision_shape_automation.py
@@ -517,20 +517,346 @@ def evaluate_collision_shape(
         )
 
 
+# ===================================================================
+# CollisionProxyOptimizer class provides a stateful API for
+# configurable evaluation and optimization of collision proxy shapes.
+# ===================================================================
+
+
+class CollisionProxyOptimizer:
+    """
+    Stateful control flow for using Habitat-sim to evaluate and optimize collision proxy shapes.
+    """
+
+    def __init__(self, sim_settings: Dict[str, Any]) -> None:
+        # load the dataset into a persistent, shared MetadataMediator instance.
+        self.mm = habitat_sim.metadata.MetadataMediator()
+        self.mm.active_dataset = sim_settings["scene_dataset_config_file"]
+
+        # cache of test points, rays, distances, etc... for use by active processes
+        # NOTE: entries created by `setup_obj_gt` and cleaned by `clean_obj_gt` for memory efficiency.
+        self.gt_data: Dict[str, Dict[str, Any]] = {}
+
+        # cache global results to be written to csv.
+        self.results: Dict[str, Dict[str, Any]] = {}
+
+    def get_cfg_with_mm(
+        self, scene: str = "NONE"
+    ) -> habitat_sim.simulator.Configuration:
+        """
+        Get a Configuration object for initializing habitat_sim Simulator object with the correct dataset and MetadataMediator passed along.
+
+        :param scene: The desired scene entry, defaulting to the empty NONE scene.
+        """
+        sim_settings = default_sim_settings.copy()
+        sim_settings["scene_dataset_config_file"] = self.mm.active_dataset
+        sim_settings["scene"] = scene
+        cfg = make_cfg(sim_settings)
+        cfg.metadata_mediator = self.mm
+        return cfg
+
+    def setup_obj_gt(
+        self,
+        obj_handle: str,
+        sample_shape: str = "jittered_aabb",
+        num_point_samples=100,
+    ) -> None:
+        """
+        Prepare the ground truth and sample point sets for an object.
+        """
+        assert (
+            obj_handle not in self.gt_data
+        ), f"`{obj_handle}` already setup in gt_data: {self.gt_data.keys()}"
+
+        # find object
+        otm = self.mm.object_template_manager
+        obj_template = otm.get_template_by_handle(obj_handle)
+        assert obj_template is not None, f"Could not find object handle `{obj_handle}`"
+        # correct now for any COM automation
+        obj_template.compute_COM_from_shape = False
+        obj_template.com = mn.Vector3(0)
+        otm.register_template(obj_template)
+
+        self.gt_data[obj_handle] = {}
+
+        # load a simulator instance with this object as the stage
+        stm = self.mm.stage_template_manager
+        stage_template_name = obj_handle + "_as_stage"
+        self.gt_data[obj_handle]["stage_template_name"] = stage_template_name
+        new_stage_template = stm.create_new_template(handle=stage_template_name)
+        new_stage_template.render_asset_handle = obj_template.render_asset_handle
+        stm.register_template(
+            template=new_stage_template, specified_handle=stage_template_name
+        )
+        cfg = self.get_cfg_with_mm(scene=stage_template_name)
+        with habitat_sim.Simulator(cfg) as sim:
+            # get test points from bounding box info:
+            scene_bb = sim.get_active_scene_graph().get_root_node().cumulative_bb
+            inflated_scene_bb = scene_bb.scaled(mn.Vector3(1.25))
+            inflated_scene_bb = mn.Range3D.from_center(
+                scene_bb.center(), inflated_scene_bb.size() / 2.0
+            )
+            test_points = None
+            if sample_shape == "aabb":
+                # bounding box sample
+                test_points = sample_points_from_range3d(
+                    range3d=inflated_scene_bb, num_points=num_point_samples
+                )
+            elif sample_shape == "jittered_aabb":
+                # bounding box sample
+                test_points = sample_jittered_points_from_range3d(
+                    range3d=inflated_scene_bb, num_points=num_point_samples
+                )
+            elif sample_shape == "sphere":
+                # bounding sphere sample
+                half_diagonal = (scene_bb.max - scene_bb.min).length() / 2.0
+                test_points = sample_points_from_sphere(
+                    center=inflated_scene_bb.center(),
+                    radius=half_diagonal,
+                    num_points=num_point_samples,
+                )
+            else:
+                raise NotImplementedError(
+                    f"sample_shape == `{sample_shape}` is not implemented. Use `sphere` or `aabb`."
+                )
+            self.gt_data[obj_handle]["test_points"] = test_points
+
+            # compute and cache "ground truth" raycast on object as stage
+            gt_raycast_results = run_pairwise_raycasts(test_points, sim)
+            self.gt_data[obj_handle]["raycasts"] = {
+                "gt": {"results": gt_raycast_results}
+            }
+
+    def clean_obj_gt(self, obj_handle: str) -> None:
+        """
+        Cleans the global object cache to better manage process memory.
+        Call this to clean-up after global data are written and detailed sample data are no longer necessary.
+        """
+        assert (
+            obj_handle in self.gt_data
+        ), f"`{obj_handle}` does not have any entry in gt_data: {self.gt_data.keys()}. Call to `setup_obj_gt(obj_handle)` required."
+        self.gt_data.pop(obj_handle)
+
+    def compute_baseline_metrics(self, obj_handle: str) -> None:
+        """
+        Computes 2 baselines for the evaluation metric and caches the results:
+        1. No collision object
+        2. AABB collision object
+        """
+        assert (
+            obj_handle in self.gt_data
+        ), f"`{obj_handle}` does not have any entry in gt_data: {self.gt_data.keys()}. Call to `setup_obj_gt(obj_handle)` required."
+
+        print(self.gt_data[obj_handle]["raycasts"].keys())
+
+        # start with empty scene
+        cfg = self.get_cfg_with_mm()
+        with habitat_sim.Simulator(cfg) as sim:
+            empty_raycast_results = run_pairwise_raycasts(
+                self.gt_data[obj_handle]["test_points"], sim
+            )
+            self.gt_data[obj_handle]["raycasts"]["empty"] = {
+                "results": empty_raycast_results
+            }
+
+        cfg = self.get_cfg_with_mm()
+        with habitat_sim.Simulator(cfg) as sim:
+            # modify the template
+            obj_template = sim.get_object_template_manager().get_template_by_handle(
+                obj_handle
+            )
+            assert (
+                obj_template is not None
+            ), f"Could not find object handle `{obj_handle}`"
+            # bounding box as collision object
+            obj_template.bounding_box_collisions = True
+            sim.get_object_template_manager().register_template(obj_template)
+
+            # load the object
+            sim.get_rigid_object_manager().add_object_by_template_handle(obj_handle)
+
+            # run evaluation
+            bb_raycast_results = run_pairwise_raycasts(
+                self.gt_data[obj_handle]["test_points"], sim
+            )
+            self.gt_data[obj_handle]["raycasts"]["bb"] = {"results": bb_raycast_results}
+
+            # un-modify the template
+            obj_template.bounding_box_collisions = False
+            sim.get_object_template_manager().register_template(obj_template)
+
+    def compute_proxy_metrics(self, obj_handle: str) -> None:
+        """
+        Computes the evaluation metric on the currently configred proxy shape and caches the results.
+        """
+        assert (
+            obj_handle in self.gt_data
+        ), f"`{obj_handle}` does not have any entry in gt_data: {self.gt_data.keys()}. Call to `setup_obj_gt(obj_handle)` required."
+
+        # start with empty scene
+        cfg = self.get_cfg_with_mm()
+        with habitat_sim.Simulator(cfg) as sim:
+            # load the object
+            sim.get_rigid_object_manager().add_object_by_template_handle(obj_handle)
+
+            # run evaluation
+            pr_raycast_results = run_pairwise_raycasts(
+                self.gt_data[obj_handle]["test_points"], sim
+            )
+            self.gt_data[obj_handle]["raycasts"]["pr"] = {"results": pr_raycast_results}
+
+    def compute_gt_errors(self, obj_handle: str) -> None:
+        """
+        Compute and cache all ground truth error metrics.
+        Assumes `self.gt_data[obj_handle]["raycasts"]` keys are different raycast results to be compared.
+          'gt' must exist.
+        """
+
+        assert (
+            obj_handle in self.gt_data
+        ), f"`{obj_handle}` does not have any entry in gt_data: {self.gt_data.keys()}. Call to `setup_obj_gt(obj_handle)` required."
+        assert (
+            len(self.gt_data[obj_handle]["raycasts"]) > 1
+        ), "Only gt results acquired, no error to compute. Try `compute_proxy_metrics` or `compute_baseline_metrics`."
+        assert (
+            "gt" in self.gt_data[obj_handle]["raycasts"]
+        ), "Must have a ground truth to compare against. Should be generated in `setup_obj_gt(obj_handle)`."
+
+        for key in self.gt_data[obj_handle]["raycasts"].keys():
+            if (
+                key != "gt"
+                and "normalized_errors" not in self.gt_data[obj_handle]["raycasts"][key]
+            ):
+                normalized_error = get_raycast_results_cumulative_error_metric(
+                    self.gt_data[obj_handle]["raycasts"]["gt"]["results"],
+                    self.gt_data[obj_handle]["raycasts"][key]["results"],
+                )
+                self.gt_data[obj_handle]["raycasts"][key][
+                    "normalized_errors"
+                ] = normalized_error
+
+    def cache_global_results(self) -> None:
+        """
+        Cache the current global cumulative results.
+        Do this after an object's computation is done before cleaning the gt data.
+        """
+
+        for obj_handle in self.gt_data.keys():
+            if obj_handle not in self.results:
+                self.results[obj_handle] = {}
+            for key in self.gt_data[obj_handle]["raycasts"].keys():
+                if (
+                    key != "gt"
+                    and "normalized_errors" in self.gt_data[obj_handle]["raycasts"][key]
+                ):
+                    if "normalized_errors" not in self.results[obj_handle]:
+                        self.results[obj_handle]["normalized_errors"] = {}
+                    self.results[obj_handle]["normalized_errors"][key] = self.gt_data[
+                        obj_handle
+                    ]["raycasts"][key]["normalized_errors"]
+
+    def save_results_to_csv(self, filename: str) -> None:
+        """
+        Save current global results to a csv file.
+        """
+
+        assert len(self.results) > 0, "There musst be results to save."
+
+        import csv
+
+        # save normalized error csv
+        with open(filename, "w") as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            # first collect all column names:
+            existing_cols = []
+            for obj_handle in self.results.keys():
+                if "normalized_errors" in self.results[obj_handle]:
+                    for key in self.results[obj_handle]["normalized_errors"]:
+                        if key not in existing_cols:
+                            existing_cols.append(key)
+            print(f"existing_cols = {existing_cols}")
+            # put the baselines first
+            ordered_cols = ["object_handle"]
+            if "empty" in existing_cols:
+                ordered_cols.append("empty")
+            if "bb" in existing_cols:
+                ordered_cols.append("bb")
+            for key in existing_cols:
+                if key not in ordered_cols:
+                    ordered_cols.append(key)
+            print(f"ordered_cols = {ordered_cols}")
+            # write column names row
+            writer.writerow(ordered_cols)
+
+            # write results rows
+            for obj_handle in self.results.keys():
+                row_data = [obj_handle]
+                if "normalized_errors" in self.results[obj_handle]:
+                    for key in ordered_cols:
+                        if key != "object_handle":
+                            if key in self.results[obj_handle]["normalized_errors"]:
+                                row_data.append(
+                                    self.results[obj_handle]["normalized_errors"][key]
+                                )
+                            else:
+                                row_data.append("")
+                print(f"row_data = {row_data}")
+                writer.writerow(row_data)
+
+    def compute_and_save_results_for_objects(
+        self, obj_handle_substrings: List[str], output_filename: str = "cpo_out.csv"
+    ) -> None:
+        # first find all full object handles
+        otm = self.mm.object_template_manager
+        obj_handles = []
+        for obj_h in obj_handle_substrings:
+            # find the full handle
+            matching_obj_handles = otm.get_file_template_handles(obj_h)
+            assert (
+                len(matching_obj_handles) == 1
+            ), f"None or many matching handles to substring `{obj_h}`: {matching_obj_handles}"
+            obj_handles.append(matching_obj_handles[0])
+
+        print(f"Found handles: {obj_handles}.")
+        print("Computing metrics:")
+        # then compute metrics for all objects and cache
+        for obix, obj_h in enumerate(obj_handles):
+            print("-------------------------------")
+            print(f"Computing metric for `{obj_h}`, {obix}|{len(obj_handles)}")
+            print("-------------------------------")
+            self.setup_obj_gt(obj_h)
+            self.compute_baseline_metrics(obj_h)
+            self.compute_proxy_metrics(obj_h)
+            self.compute_gt_errors(obj_h)
+            self.cache_global_results()
+            self.clean_obj_gt(obj_h)
+
+        # then save results to file
+        self.save_results_to_csv(output_filename)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Automate collision shape creation and validation."
     )
     parser.add_argument("--dataset", type=str, help="path to SceneDataset.")
-    parser.add_argument(
-        "--object-handle", type=str, help="handle identifying the object to evaluate."
-    )
+    # parser.add_argument(
+    #    "--object-handle", type=str, help="handle identifying the object to evaluate."
+    # )
     args = parser.parse_args()
 
     sim_settings = default_sim_settings.copy()
     sim_settings["scene_dataset_config_file"] = args.dataset
 
-    evaluate_collision_shape(args.object_handle, sim_settings)
+    # use the CollisionProxyOptimizer to compute metrics for multiple objects
+    cpo = CollisionProxyOptimizer(sim_settings)
+
+    obj_handle1 = "0a5e809804911e71de6a4ef89f2c8fef5b9291b3"
+    obj_handle2 = "d1d1e0cdaba797ee70882e63f66055675c3f1e7f"
+
+    cpo.compute_and_save_results_for_objects([obj_handle1, obj_handle2])
+
+    # evaluate_collision_shape(args.object_handle, sim_settings)
 
 
 if __name__ == "__main__":
