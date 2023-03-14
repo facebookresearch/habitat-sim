67b8ffddcf884456948ac1ef1d8c0f038f5ab9f3
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2023-03-14 14:53:58 -0400

    Upgrade python to 3.9.

--- conda-build/matrix_builder.py
+++ conda-build/matrix_builder.py
@@ -37,7 +37,7 @@ def call(cmd, env=None):
 
 
 def get_default_modes_and_vers():
-    py_vers = ["3.7", "3.8"]
+    py_vers = ["3.9"]
     bullet_modes = [False, True]
     if platform.system() == "Darwin":
         return py_vers, bullet_modes, [False], [None]
@@ -92,7 +92,7 @@ def main():
     if args.ci_test:
         bullet_modes = [True]
         headless_modes = [get_headless_mode_for_test()]
-        py_vers = ["3.7"]
+        py_vers = ["3.9"]
 
     for py_ver, use_bullet, headless, cuda_ver in itertools.product(
         py_vers, bullet_modes, headless_modes, cuda_vers
