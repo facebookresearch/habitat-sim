aa687817e23fca49ced379b8003f73aa4f72b6dc
Vladimír Vondruš <mosra@centrum.cz>
2023-03-22 19:03:28 +0100

    [wip] this slowness needs fixing in the renderer

--- src/esp/gfx_batch/Renderer.cpp
+++ src/esp/gfx_batch/Renderer.cpp
@@ -762,7 +762,7 @@ bool Renderer::addFile(const Cr::Containers::StringView filename,
       Mn::UnsignedInt offset = meshViewOffset;
       for (Mn::UnsignedLong root : scene->childrenFor(-1)) {
         Cr::Containers::Array<Mn::UnsignedLong> children =
-            scene->childrenFor(root);
+            scene->childrenFor(root); // TODO ugh slow AF
 
         Cr::Containers::String name = importer->objectName(root);
         if (!name) {
