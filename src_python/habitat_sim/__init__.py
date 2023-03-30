f179b584bcd713c5a2a998132211e2cae881d6d1
Alexander Clegg <alexanderwclegg@gmail.com>
2023-03-30 12:00:03 -0700

    update to v0.2.4 (#2047)
    * update to v0.2.4

--- src_python/habitat_sim/__init__.py
+++ src_python/habitat_sim/__init__.py
@@ -6,7 +6,7 @@
 
 import builtins
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 if not getattr(builtins, "__HSIM_SETUP__", False):
     # TODO: kept only for compatibility with existing code. Please gradually remove
