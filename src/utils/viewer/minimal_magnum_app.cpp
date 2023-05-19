feb037fc0e20765a9779606c35568f8c01a2a845
Eric Undersander <eundersander@fb.com>
2023-05-19 18:06:40 -0400

    add minimal_magnum_app.cpp (builds to viewer binary)

---
+++ src/utils/viewer/minimal_magnum_app.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) Meta Platforms, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <Magnum/Platform/GlfwApplication.h>
+
+class MyApplication : public Magnum::Platform::Application {
+ public:
+  explicit MyApplication(const Arguments& arguments)
+      : Magnum::Platform::Application{
+            arguments,
+            Configuration{}.setTitle("MyApplication").setSize({400, 300})} {}
+
+  void drawEvent() override {}
+};
+
+MAGNUM_APPLICATION_MAIN(MyApplication)
