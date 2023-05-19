1f9351b8a29068b805560989203c62b2b47b529b
Eric Undersander <eundersander@fb.com>
2023-05-19 19:48:01 -0400

    make minimal magnum app match minimal_glfw.py (no triangle; flashing red background)

--- src/utils/viewer/minimal_magnum_app.cpp
+++ src/utils/viewer/minimal_magnum_app.cpp
@@ -26,29 +26,31 @@ class MyApplication : public Magnum::Platform::Application {
             Configuration{}.setTitle("MyApplication").setSize({400, 300})} {
     // _ctx = std::make_unique<Magnum::Platform::GLContext>();
 
-    struct TriangleVertex {
-      Magnum::Vector2 position;
-      Magnum::Color3 color;
-    };
-    const TriangleVertex vertices[]{
-        {{-0.5f, -0.5f}, 0xff0000_rgbf}, /* Left vertex, red color */
-        {{0.5f, -0.5f}, 0x00ff00_rgbf},  /* Right vertex, green color */
-        {{0.0f, 0.5f}, 0x0000ff_rgbf}    /* Top vertex, blue color */
-    };
+    // struct TriangleVertex {
+    //   Magnum::Vector2 position;
+    //   Magnum::Color3 color;
+    // };
+    // const TriangleVertex vertices[]{
+    //     {{-0.5f, -0.5f}, 0xff0000_rgbf}, /* Left vertex, red color */
+    //     {{0.5f, -0.5f}, 0x00ff00_rgbf},  /* Right vertex, green color */
+    //     {{0.0f, 0.5f}, 0x0000ff_rgbf}    /* Top vertex, blue color */
+    // };
 
-    _mesh.setCount(Corrade::Containers::arraySize(vertices))
-        .addVertexBuffer(Magnum::GL::Buffer{vertices}, 0,
-                         Magnum::Shaders::VertexColorGL2D::Position{},
-                         Magnum::Shaders::VertexColorGL2D::Color3{});
+    // _mesh.setCount(Corrade::Containers::arraySize(vertices))
+    //     .addVertexBuffer(Magnum::GL::Buffer{vertices}, 0,
+    //                      Magnum::Shaders::VertexColorGL2D::Position{},
+    //                      Magnum::Shaders::VertexColorGL2D::Color3{});
   }
 
   void drawEvent() override {
     /* Render here */
-    Magnum::GL::Renderer::setClearColor(Magnum::Color4(1, 0, 0, 1));
-    Magnum::GL::defaultFramebuffer.clear(Magnum::GL::FramebufferClear::Color);
-    _shader.draw(_mesh);
+    static float animFraction = 0.f;
+    animFraction = fmodf(animFraction + 0.05, 1.0);
 
-    printf("drawEvent\n");
+    Magnum::GL::Renderer::setClearColor(Magnum::Color4(animFraction, 0, 0, 1));
+    Magnum::GL::defaultFramebuffer.clear(Magnum::GL::FramebufferClear::Color);
+    // _shader.draw(_mesh);
+
     swapBuffers();
     redraw();
   }
