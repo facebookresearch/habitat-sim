517731689817c9b7e952b37a83e09383812dc310
Aaron Gokaslan <aaronGokaslan@gmail.com>
2023-05-24 09:55:16 -0400

    Decrease minimum required GL version for most shaders

--- src/esp/gfx/PbrShader.cpp
+++ src/esp/gfx/PbrShader.cpp
@@ -54,7 +54,7 @@ PbrShader::PbrShader(Flags originalFlags, unsigned int lightCount)
 #ifdef MAGNUM_TARGET_WEBGL
   Mn::GL::Version glVersion = Mn::GL::Version::GLES300;
 #else
-  Mn::GL::Version glVersion = Mn::GL::Version::GL410;
+  Mn::GL::Version glVersion = Mn::GL::Version::GL330;
 #endif
 
   // this is not the file name, but the group name in the config file
