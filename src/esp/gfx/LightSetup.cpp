b67958d883c782d85d62ae99e54804e2949be5f4
John Turner <7strbass@gmail.com>
2023-05-24 13:46:35 -0400

    --better default lights
    By having the main lights global, constructive interference (i.e. the lights getting brighter because of overlap) is avoided when turning the camera.  The camera-relative backwards light makes sure there's always a glancing specular when looking at horizontal clearcoat/specular/metallic object surfaces.  Also, this has a better mix of direct and indirect lighting when IBL is active

--- src/esp/gfx/LightSetup.cpp
+++ src/esp/gfx/LightSetup.cpp
@@ -79,18 +79,30 @@ LightSetup getLightsAtBoxCorners(const Magnum::Range3D& box,
 
 LightSetup getDefaultLights() {
   return LightSetup{
-      {{0.0, -0.5, -0.5, 0.0},
+      {{0.0, -0.1, -1.0, 0.0},
        {0.5, 0.5, 0.5},
        LightPositionModel::Global},  // -z
-      {{0.0, -0.5, 0.5, 0.0},
+      {{0.0, -0.1, 1.0, 0.0},
        {0.5, 0.5, 0.5},
        LightPositionModel::Global},  // +z
-      {{-0.5, -0.5, 0.0, 0.0},
+      {{-1.0, -0.1, 0.0, 0.0},
        {0.5, 0.5, 0.5},
        LightPositionModel::Global},  // -x
-      {{0.5, -0.5, 0.0, 0.0},
+      {{+1.0, -0.1, -1.0, 0.0},
        {0.5, 0.5, 0.5},
        LightPositionModel::Global},  // +x
+      {{0.0, -1.0, 0.0, 0.0},
+       {0.6, 0.6, 0.6},
+       LightPositionModel::Global},  // downward
+
+      // camera-relative
+      {{0.0, -0.1, -1.0, 0.0},
+       {0.5, 0.5, 0.5},
+       LightPositionModel::Camera},  // forward
+      {{0.0, -0.3, 1.0, 0.0},
+       {0.5, 0.5, 0.5},
+       LightPositionModel::Camera},  // backward (for glancing speculars)
+
   };
 }
 
