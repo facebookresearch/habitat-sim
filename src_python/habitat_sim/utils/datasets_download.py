d0797d3a1cfe07751b2e2b553e33c271ef457252
Alexander Clegg <alexanderwclegg@gmail.com>
2023-06-01 14:28:59 -0700

    Replica_cad_v1.6 (#1783)

--- src_python/habitat_sim/utils/datasets_download.py
+++ src_python/habitat_sim/utils/datasets_download.py
@@ -109,16 +109,16 @@ def initialize_test_data_sources(data_path):
             "version": "1.0",
         },
         "replica_cad_dataset": {
-            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_dataset_v1.5.zip",
-            "package_name": "ReplicaCAD_dataset_v1.5.zip",
+            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_dataset_v1.6.zip",
+            "package_name": "ReplicaCAD_dataset_v1.6.zip",
             "link": data_path + "replica_cad",
-            "version": "1.5",
+            "version": "1.6",
         },
         "replica_cad_baked_lighting": {
-            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_baked_lighting_v1.5.zip",
-            "package_name": "ReplicaCAD_baked_lighting_v1.5.zip",
+            "source": "https://dl.fbaipublicfiles.com/habitat/ReplicaCAD/ReplicaCAD_baked_lighting_v1.6.zip",
+            "package_name": "ReplicaCAD_baked_lighting_v1.6.zip",
             "link": data_path + "replica_cad_baked_lighting",
-            "version": "1.5",
+            "version": "1.6",
         },
         "ycb": {
             "source": "https://dl.fbaipublicfiles.com/habitat/ycb/hab_ycb_v1.2.zip",
