2ed307ab381996d3c3cd74360ef48491c1be9886
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2023-07-07 17:26:02 -0400

    Pin CI dependencies.

--- conda-build/common/install_conda.sh
+++ conda-build/common/install_conda.sh
@@ -12,5 +12,5 @@ chmod +x  Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda
 rm Miniconda3-latest-Linux-x86_64.sh
 export PATH=/opt/conda/bin:$PATH
-conda install -y anaconda-client git gitpython ninja conda-build # conda-build=3.18.9 # last version that works with our setup
+conda install -y --file .circleci/requirements-conda-build.txt
 conda remove -y --force patchelf
