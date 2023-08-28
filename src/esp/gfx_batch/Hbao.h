a5c1dc7bdc0876095db78698c3f313ca707e7d82
John Turner <7strbass@gmail.com>
2023-08-28 12:03:02 -0400

    --more clang-tidy

--- src/esp/gfx_batch/Hbao.h
+++ src/esp/gfx_batch/Hbao.h
@@ -2,11 +2,12 @@
 #define ESP_GFX_BATCH_HBAO_H_
 
 #include <Corrade/Containers/Pointer.h>
-#include <Magnum/Magnum.h>
 #include <Magnum/GL/GL.h>
+#include <Magnum/Magnum.h>
 #include <Magnum/Math/Vector2.h>
 
-namespace esp { namespace gfx_batch {
+namespace esp {
+namespace gfx_batch {
 
 enum class HbaoFlag {
   Blur = 1 << 0,
@@ -22,96 +23,105 @@ typedef Corrade::Containers::EnumSet<HbaoFlag> HbaoFlags;
 CORRADE_ENUMSET_OPERATORS(HbaoFlags)
 
 class HbaoConfiguration {
-  public:
-    Magnum::Vector2i size() const { return size_; }
+ public:
+  Magnum::Vector2i size() const { return size_; }
 
-    HbaoConfiguration& setSize(const Magnum::Vector2i& size) {
-      size_ = size;
-      return *this;
-    }
+  HbaoConfiguration& setSize(const Magnum::Vector2i& size) {
+    size_ = size;
+    return *this;
+  }
 
-    HbaoFlags flags() const { return flags_; }
+  HbaoFlags flags() const { return flags_; }
 
-    HbaoConfiguration& setFlags(HbaoFlags flags) {
-      flags_ = flags;
-      return *this;
-    }
+  HbaoConfiguration& setFlags(HbaoFlags flags) {
+    flags_ = flags;
+    return *this;
+  }
 
-    Magnum::Int samples() const { return samples_; }
+  Magnum::Int samples() const { return samples_; }
 
-    HbaoConfiguration& setSamples(Magnum::Int samples) {
-      samples_ = samples;
-      return *this;
-    }
+  HbaoConfiguration& setSamples(Magnum::Int samples) {
+    samples_ = samples;
+    return *this;
+  }
 
-    Magnum::Float intensity() const { return intensity_; }
+  Magnum::Float intensity() const { return intensity_; }
 
-    HbaoConfiguration& setIntensity(Magnum::Float intensity) {
-      intensity_ = intensity;
-      return *this;
-    }
+  HbaoConfiguration& setIntensity(Magnum::Float intensity) {
+    intensity_ = intensity;
+    return *this;
+  }
 
-    Magnum::Float bias() const { return bias_; }
+  Magnum::Float bias() const { return bias_; }
 
-    HbaoConfiguration& setBias(Magnum::Float bias) {
-      bias_ = bias;
-      return *this;
-    }
+  HbaoConfiguration& setBias(Magnum::Float bias) {
+    bias_ = bias;
+    return *this;
+  }
 
-    Magnum::Float radius() const { return radius_; }
+  Magnum::Float radius() const { return radius_; }
 
-    HbaoConfiguration& setRadius(Magnum::Float radius) {
-      radius_ = radius;
-      return *this;
-    }
+  HbaoConfiguration& setRadius(Magnum::Float radius) {
+    radius_ = radius;
+    return *this;
+  }
 
-    Magnum::Float blurSharpness() const { return blurSharpness_; }
+  Magnum::Float blurSharpness() const { return blurSharpness_; }
 
-    HbaoConfiguration& setBlurSharpness(Magnum::Float blurSharpness) {
-      blurSharpness_ = blurSharpness;
-      return *this;
-    }
+  HbaoConfiguration& setBlurSharpness(Magnum::Float blurSharpness) {
+    blurSharpness_ = blurSharpness;
+    return *this;
+  }
 
-  private:
-    Magnum::Vector2i size_;
-    HbaoFlags flags_ = HbaoFlag::Blur;
-    Magnum::Int samples_ = 1;
-    Magnum::Float intensity_ = 0.732f,
-      bias_ = 0.05f,
-      radius_ = 1.24f,
-      blurSharpness_ = 10.0f;
+ private:
+  Magnum::Vector2i size_;
+  HbaoFlags flags_ = HbaoFlag::Blur;
+  Magnum::Int samples_ = 1;
+  Magnum::Float intensity_ = 0.732f, bias_ = 0.05f, radius_ = 1.24f,
+                blurSharpness_ = 10.0f;
 };
 
 class Hbao {
-  public:
-    // Use this to construct before a GL context is ready
-    explicit Hbao(Magnum::NoCreateT) noexcept;
+ public:
+  // Use this to construct before a GL context is ready
+  explicit Hbao(Magnum::NoCreateT) noexcept;
 
-    explicit Hbao(const HbaoConfiguration& configuration);
+  explicit Hbao(const HbaoConfiguration& configuration);
 
-    Hbao(const Hbao&) = delete;
-    Hbao(Hbao&&) noexcept;
+  Hbao(const Hbao&) = delete;
+  Hbao(Hbao&&) noexcept;
 
-    ~Hbao();
+  ~Hbao();
 
-    Hbao& operator=(const Hbao&) = delete;
-    Hbao& operator=(Hbao&&) noexcept;
+  Hbao& operator=(const Hbao&) = delete;
+  Hbao& operator=(Hbao&&) noexcept;
 
-    void drawClassicOrthographic(const Magnum::Matrix4& projection, Magnum::GL::Texture2D& inputDepthStencil, Magnum::GL::AbstractFramebuffer& output);
-    void drawClassicPerspective(const Magnum::Matrix4& projection, Magnum::GL::Texture2D& inputDepthStencil, Magnum::GL::AbstractFramebuffer& output);
-    void drawCacheAwareOrthographic(const Magnum::Matrix4& projection, Magnum::GL::Texture2D& inputDepthStencil, Magnum::GL::AbstractFramebuffer& output);
-    void drawCacheAwarePerspective(const Magnum::Matrix4& projection, Magnum::GL::Texture2D& inputDepthStencil, Magnum::GL::AbstractFramebuffer& output);
+  void drawClassicOrthographic(const Magnum::Matrix4& projection,
+                               Magnum::GL::Texture2D& inputDepthStencil,
+                               Magnum::GL::AbstractFramebuffer& output);
+  void drawClassicPerspective(const Magnum::Matrix4& projection,
+                              Magnum::GL::Texture2D& inputDepthStencil,
+                              Magnum::GL::AbstractFramebuffer& output);
+  void drawCacheAwareOrthographic(const Magnum::Matrix4& projection,
+                                  Magnum::GL::Texture2D& inputDepthStencil,
+                                  Magnum::GL::AbstractFramebuffer& output);
+  void drawCacheAwarePerspective(const Magnum::Matrix4& projection,
+                                 Magnum::GL::Texture2D& inputDepthStencil,
+                                 Magnum::GL::AbstractFramebuffer& output);
 
-  private:
-    void drawLinearDepth(const Magnum::Matrix4& projection, bool orthographic, Magnum::GL::Texture2D& inputDepthStencil);
-    void drawHbaoBlur(Magnum::GL::AbstractFramebuffer& output);
-    void drawClassicInternal(Magnum::GL::AbstractFramebuffer& output);
-    void drawCacheAwareInternal(Magnum::GL::AbstractFramebuffer& output);
+ private:
+  void drawLinearDepth(const Magnum::Matrix4& projection,
+                       bool orthographic,
+                       Magnum::GL::Texture2D& inputDepthStencil);
+  void drawHbaoBlur(Magnum::GL::AbstractFramebuffer& output);
+  void drawClassicInternal(Magnum::GL::AbstractFramebuffer& output);
+  void drawCacheAwareInternal(Magnum::GL::AbstractFramebuffer& output);
 
-    struct State;
-    Corrade::Containers::Pointer<State> state_;
+  struct State;
+  Corrade::Containers::Pointer<State> state_;
 };
 
-}}
+}  // namespace gfx_batch
+}  // namespace esp
 
 #endif
