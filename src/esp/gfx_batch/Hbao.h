a5108b7b37e741b0a40ec0db3e4bc0368cb8213a
John Turner <7strbass@gmail.com>
2023-09-19 11:55:26 -0400

    --update copyrights

--- src/esp/gfx_batch/Hbao.h
+++ src/esp/gfx_batch/Hbao.h
@@ -1,3 +1,7 @@
+// Copyright (c) Meta Platforms, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #ifndef ESP_GFX_BATCH_HBAO_H_
 #define ESP_GFX_BATCH_HBAO_H_
 
