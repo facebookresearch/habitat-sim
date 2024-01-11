b0bd929cc07dbbaa4b8c88ed545f6595a7cbc475
John Turner <7strbass@gmail.com>
2024-01-11 09:38:00 -0500

    --add SemanticAttributesManager

---
+++ src/esp/metadata/managers/SemanticAttributesManager.cpp
@@ -0,0 +1,171 @@
+// Copyright (c) Meta Platforms, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "SemanticAttributesManager.h"
+#include "AttributesManagerBase.h"
+
+#include "esp/io/Json.h"
+
+namespace Cr = Corrade;
+namespace esp {
+
+namespace metadata {
+
+using attributes::SemanticAttributes;
+using attributes::SemanticRegionAttributes;
+namespace managers {
+
+SemanticAttributes::ptr SemanticAttributesManager::createObject(
+    const std::string& semanticConfigFilename,
+    bool registerTemplate) {
+  std::string msg;
+  SemanticAttributes::ptr attrs = this->createFromJsonOrDefaultInternal(
+      semanticConfigFilename, msg, registerTemplate);
+
+  if (nullptr != attrs) {
+    ESP_DEBUG() << msg << "Semantic Attributes created"
+                << (registerTemplate ? "and registered." : ".");
+  }
+  return attrs;
+}  // SemanticAttributesManager::createObject
+
+SemanticAttributes::ptr SemanticAttributesManager::initNewObjectInternal(
+    const std::string& handleName,
+    bool) {
+  SemanticAttributes::ptr newAttributes =
+      this->constructFromDefault(handleName);
+  if (nullptr == newAttributes) {
+    newAttributes = SemanticAttributes::create(handleName);
+  }
+  // set the attributes source filedirectory, from the attributes name
+  this->setFileDirectoryFromHandle(newAttributes);
+
+  return newAttributes;
+}  // SemanticAttributesManager::initNewObjectInternal
+
+void SemanticAttributesManager::setSemanticRegionAttributesFromJson(
+    const SemanticRegionAttributes::ptr& instanceAttrs,
+    const io::JsonGenericValue& jCell) {
+  // unique name for region
+  io::jsonIntoConstSetter<std::string>(
+      jCell, "name", [instanceAttrs](const std::string& region_name) {
+        instanceAttrs->setHandle(region_name);
+      });
+
+  // semantic label for region
+  io::jsonIntoConstSetter<std::string>(
+      jCell, "label", [instanceAttrs](const std::string& label) {
+        instanceAttrs->setLabel(label);
+      });
+
+  // floor height
+  io::jsonIntoSetter<double>(jCell, "floor_height",
+                             [instanceAttrs](double floor_height) {
+                               instanceAttrs->setFloorHeight(floor_height);
+                             });
+
+  // extrusion height above floor
+  io::jsonIntoSetter<double>(
+      jCell, "extrusion_height", [instanceAttrs](double extrusion_height) {
+        instanceAttrs->setExtrusionHeight(extrusion_height);
+      });
+
+  // min bounds of region
+  io::jsonIntoConstSetter<Mn::Vector3>(
+      jCell, "min_bounds", [instanceAttrs](const Mn::Vector3& minBounds) {
+        instanceAttrs->setMinBounds(minBounds);
+      });
+
+  // max bounds of region
+  io::jsonIntoConstSetter<Mn::Vector3>(
+      jCell, "max_bounds", [instanceAttrs](const Mn::Vector3& maxBounds) {
+        instanceAttrs->setMaxBounds(maxBounds);
+      });
+
+  //////////////////////
+  // Polyloop points
+
+  io::JsonGenericValue::ConstMemberIterator polyLoopJSONIter =
+      jCell.FindMember("poly_loop");
+  if (polyLoopJSONIter != jCell.MemberEnd()) {
+    if (polyLoopJSONIter->value.IsArray()) {
+      std::vector<Mn::Vector3> polyLoop;
+      // read values into vector
+      io::readMember<Mn::Vector3>(jCell, "poly_loop", polyLoop);
+      instanceAttrs->setPolyLoop(polyLoop);
+
+    } else {
+      ESP_WARNING() << ": Unknown format for "
+                       "poly_loop specified for region instance"
+                    << instanceAttrs->getHandle()
+                    << "in Semantic Config File, so no values are set.";
+    }
+  }
+
+  // check for user defined attributes
+  this->parseUserDefinedJsonVals(instanceAttrs, jCell);
+
+}  // SemanticAttributesManager::setSemanticRegionAttributesFromJson
+
+SemanticRegionAttributes::ptr
+SemanticAttributesManager::createRegionAttributesFromJSON(
+    const io::JsonGenericValue& jCell) {
+  SemanticRegionAttributes::ptr regionAttrs = createEmptyRegionAttributes("");
+  // populate attributes
+  this->setSemanticRegionAttributesFromJson(regionAttrs, jCell);
+  return regionAttrs;
+}  // SemanticAttributesManager::createRegionAttributesFromJSON
+
+void SemanticAttributesManager::setValsFromJSONDoc(
+    SemanticAttributes::ptr semanticAttribs,
+    const io::JsonGenericValue& jsonConfig) {
+  const std::string attribsDispName = semanticAttribs->getSimplifiedHandle();
+
+  // Check for region instances existence
+  io::JsonGenericValue::ConstMemberIterator regionJSONIter =
+      jsonConfig.FindMember("region_annotations");
+  if (regionJSONIter != jsonConfig.MemberEnd()) {
+    // region_annotations tag exists
+    if (regionJSONIter->value.IsArray()) {
+      const auto& regionArray = regionJSONIter->value;
+      for (rapidjson::SizeType i = 0; i < regionArray.Size(); ++i) {
+        const auto& regionCell = regionArray[i];
+        if (regionCell.IsObject()) {
+          semanticAttribs->addRegionInstanceAttrs(
+              createRegionAttributesFromJSON(regionCell));
+        } else {
+          ESP_WARNING(Mn::Debug::Flag::NoSpace)
+              << "Region instance issue in Semantic Configuration `"
+              << attribsDispName << "` at idx : " << i
+              << " : JSON cell within `region_annotations` array is not a "
+                 "valid JSON object, so skipping entry.";
+        }
+      }
+    } else {
+      // region_annotations tag exists but is not an array. should warn (perhaps
+      // error?)
+      ESP_WARNING(Mn::Debug::Flag::NoSpace)
+          << "Region instance issue in Semantic Configuration `"
+          << attribsDispName
+          << "`: JSON cell `region_annotations` is not a valid JSON "
+             "array, so no object instances loaded.";
+    }
+  } else {
+    // No region_annotations tag exists in scene instance. Not necessarily a bad
+    // thing - not all semantic descriptions must include region descriptions.
+    ESP_VERY_VERBOSE(Mn::Debug::Flag::NoSpace)
+        << "No Regions were specified in Semantic Configuration `"
+        << attribsDispName
+        << "`: JSON cell with tag `region_annotations` does not exist in "
+           "Semantic Configuration file.";
+  }
+
+  // check for user defined attributes
+  this->parseUserDefinedJsonVals(semanticAttribs, jsonConfig);
+
+}  // SemanticAttributesManager::setValsFromJSONDoc
+
+}  // namespace managers
+}  // namespace metadata
+}  // namespace esp
---
+++ src/esp/metadata/managers/SemanticAttributesManager.h
@@ -0,0 +1,170 @@
+// Copyright (c) Meta Platforms, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef ESP_METADATA_MANAGERS_SEMANTICATTRIBUTESMANAGER_H_
+#define ESP_METADATA_MANAGERS_SEMANTICATTRIBUTESMANAGER_H_
+
+#include <utility>
+
+#include "AttributesManagerBase.h"
+
+#include "esp/metadata/attributes/SemanticAttributes.h"
+
+namespace Cr = Corrade;
+
+namespace esp {
+namespace metadata {
+namespace managers {
+
+class SemanticAttributesManager
+    : public AttributesManager<attributes::SemanticAttributes,
+                               ManagedObjectAccess::Copy> {
+ public:
+  SemanticAttributesManager()
+      : AttributesManager<attributes::SemanticAttributes,
+                          ManagedObjectAccess::Copy>::
+            AttributesManager("Semantic Attributes", "semantic_config.json") {
+    this->copyConstructorMap_["SemanticAttributes"] =
+        &SemanticAttributesManager::createObjectCopy<
+            attributes::SemanticAttributes>;
+  }  // ctor
+
+  /**
+   * @brief Creates an instance of a semantic attributes used to
+   * govern which calculations are performed by the PBR shader (such as
+   * Image-based Lighting and/or specific PBR layer calculations), as well as to
+   * provide some user-configurable shader-wide control values.
+   *
+   * If a template exists with this handle, this existing template will be
+   * overwritten with the newly created one if registerTemplate is true.
+   *
+   * @param semanticConfigFilename The location of the Semantic Configuratioo.
+   * @param registerTemplate whether to add this template to the library.
+   * If the user is going to edit this template, this should be false - any
+   * subsequent editing will require re-registration. Defaults to true. If
+   * specified as true, then this function returns a copy of the registered
+   * template.
+   * @return a reference to the physics simulation meta data object parsed from
+   * the specified configuration file.
+   */
+  attributes::SemanticAttributes::ptr createObject(
+      const std::string& semanticConfigFilename,
+      bool registerTemplate = true) override;
+
+  /**
+   * @brief Method to take an existing attributes and set its values from passed
+   * json config file.
+   * @param attribs (out) an existing attributes to be modified.
+   * @param jsonConfig json document to parse
+   */
+  void setValsFromJSONDoc(attributes::SemanticAttributes::ptr attribs,
+                          const io::JsonGenericValue& jsonConfig) override;
+
+  /**
+   * @brief This will return a @ref
+   * attributes::SemanticRegionAttributes object with passed handle.
+   */
+  attributes::SemanticRegionAttributes::ptr createEmptyRegionAttributes(
+      const std::string& handle) {
+    return attributes::SemanticRegionAttributes::create(handle);
+  }
+
+ protected:
+  /**
+   * @brief Used Internally.  Create a @ref
+   * esp::metadata::attributes::SemanticRegionAttributes object from the
+   * passed JSON doc.
+   * @param jCell JSON object containing the description of the semantic retion
+   * @return the constructed @ref
+   * esp::metadata::attributes::SemanticRegionAttributes object
+   */
+  attributes::SemanticRegionAttributes::ptr createRegionAttributesFromJSON(
+      const io::JsonGenericValue& jCell);
+
+  /**
+   * @brief Populate an existing @ref
+   * metadata::attributes::SemanticRegionAttributes from a JSON config.
+   *
+   * @param attributes (out) the attributes to populate with JSON values
+   * @param jCell JSON document to parse to populate the
+   * @ref metadata::attributes::SemanticRegionAttributes
+   */
+  void setSemanticRegionAttributesFromJson(
+      const attributes::SemanticRegionAttributes::ptr& attributes,
+      const io::JsonGenericValue& jCell);
+
+  /**
+   * @brief Used Internally.  Create and configure newly-created attributes with
+   * any default values, before any specific values are set.
+   *
+   * @param handleName handle name to be assigned to attributes
+   * @param builtFromConfig Whether this SemanticAttributes is being built
+   * from a config file (i.e. handleName is the name of a configuration file) or
+   * from some other source.
+   * @return Newly created but unregistered SemanticAttributes pointer,
+   * with only default values set.
+   */
+  attributes::SemanticAttributes::ptr initNewObjectInternal(
+      const std::string& handleName,
+      CORRADE_UNUSED bool builtFromConfig) override;
+
+  /**
+   * @brief This method will perform any necessary updating that is
+   * attributesManager-specific upon template removal, such as removing a
+   * specific template handle from the list of file-based template handles in
+   * ObjectAttributesManager.  This should only be called @ref
+   * esp::core::ManagedContainerBase.
+   *
+   * @param templateID the ID of the template to remove
+   * @param templateHandle the string key of the attributes desired.
+   */
+  void deleteObjectInternalFinalize(
+      CORRADE_UNUSED int templateID,
+      CORRADE_UNUSED const std::string& templateHandle) override {}
+
+  /**
+   * @brief Add a copy of the @ref
+   * esp::metadata::attributes::SemanticAttributes shared_ptr object to
+   * the @ref objectLibrary_.
+   *
+   * @param pbrShaderConfigurationTemplate The attributes template.
+   * @param pbrShaderConfigurationHandle The key for referencing the template in
+   * the
+   * @ref objectLibrary_.
+   * @param forceRegistration Will register object even if conditionalE
+   * registration checks fail.
+   * @return The index in the @ref objectLibrary_ of object
+   * template.
+   */
+  int registerObjectFinalize(
+      attributes::SemanticAttributes::ptr pbrShaderConfigurationTemplate,
+      const std::string& pbrShaderConfigurationHandle,
+      CORRADE_UNUSED bool forceRegistration) override {
+    // adds template to library, and returns either the ID of the existing
+    // template referenced by pbrShaderConfigurationHandle, or the next
+    // available ID if not found.
+    int pbrConfigId =
+        this->addObjectToLibrary(std::move(pbrShaderConfigurationTemplate),
+                                 pbrShaderConfigurationHandle);
+    return pbrConfigId;
+  }  // SemanticAttributesManager::registerObjectFinalize
+
+  /**
+   * @brief Any physics-attributes-specific resetting that needs to happen on
+   * reset.
+   */
+  void resetFinalize() override {}
+
+  // instance vars
+
+ public:
+  ESP_SMART_POINTERS(SemanticAttributesManager)
+
+};  // SemanticAttributesManager
+
+}  // namespace managers
+}  // namespace metadata
+}  // namespace esp
+
+#endif  // ESP_METADATA_MANAGERS_SEMANTICATTRIBUTESMANAGER_H_
