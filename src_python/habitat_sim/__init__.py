268f942d7eb60e1f701a5d009d7cb02a25c5e2a6
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-01-31 16:24:16 -0500

    Set RTLD_GLOBAL in habitat-sim __init__.py.

--- src_python/habitat_sim/__init__.py
+++ src_python/habitat_sim/__init__.py
@@ -4,7 +4,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+
 import builtins
+import ctypes
+import sys
+
+sys.setdlopenflags(sys.getdlopenflags() | ctypes.RTLD_GLOBAL)
 
 __version__ = "0.3.1"
 
