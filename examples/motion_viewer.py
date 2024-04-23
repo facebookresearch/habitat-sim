d501fe9ab52814a692b6550fa2bc0c2802688745
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-04-22 23:20:44 -0400

    Reapply "Remove RTLD_GLOBAL flag from apps. (#2312)" (#2342)
    This reverts commit 5f2bc6a0bde1da77b334022ca28e8903952f4a24.

--- examples/motion_viewer.py
+++ examples/motion_viewer.py
@@ -2,14 +2,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import ctypes
 import random
-import sys
 import time
 from typing import Any, Callable, Dict, Optional, Tuple
-
-flags = sys.getdlopenflags()
-sys.setdlopenflags(flags | ctypes.RTLD_GLOBAL)
 
 import magnum as mn
 from magnum.platform.glfw import Application
