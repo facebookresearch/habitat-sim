9c7c25f5e78a84514b58147307b815d603076c12
John Turner <7strbass@gmail.com>
2024-12-03 14:32:29 -0500

    --add UfbxImporter for Obj and Fbx assets.
    ..instead of assimp!

--- src/esp/assets/ResourceManager.cpp
+++ src/esp/assets/ResourceManager.cpp
@@ -133,12 +133,14 @@ ResourceManager::~ResourceManager() = default;
 
 void ResourceManager::buildImporters() {
   // Preferred plugins, Basis target GPU format
+  importerManager_.setPreferredPlugins("ObjImporter", {"UfbxImporter"});
+  importerManager_.setPreferredPlugins("FbxImporter", {"UfbxImporter"});
 #ifdef ESP_BUILD_ASSIMP_SUPPORT
-  importerManager_.setPreferredPlugins("ObjImporter", {"AssimpImporter"});
-  Cr::PluginManager::PluginMetadata* const assimpmetadata =
-      importerManager_.metadata("AssimpImporter");
-  assimpmetadata->configuration().setValue("ImportColladaIgnoreUpDirection",
-                                           "true");
+  if (Cr::PluginManager::PluginMetadata* const assimpmetadata =
+          importerManager_.metadata("AssimpImporter")) {
+    assimpmetadata->configuration().setValue("ImportColladaIgnoreUpDirection",
+                                             "true");
+  }
 #endif
 
   // instantiate a primitive importer
