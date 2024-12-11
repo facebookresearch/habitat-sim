f218d98e3227fb65a180a990438149a93ffb1ccb
John Turner <7strbass@gmail.com>
2024-12-11 09:32:09 -0500

    --update method name for SensorAttributesManager

--- src/esp/metadata/managers/SensorAttributesManager.cpp
+++ src/esp/metadata/managers/SensorAttributesManager.cpp
@@ -158,7 +158,7 @@ AbstractSensorAttributes::ptr SensorAttributesManager::buildObjectFromJSONDoc(
   return sensorAttributes;
 }  // SensorAttributesManager::buildObjectFromJSONDoc
 
-void SensorAttributesManager::setValsFromJSONDoc(
+void SensorAttributesManager::setValsFromJSONDocInternal(
     AbstractSensorAttributes::ptr attribs,
     const io::JsonGenericValue& jsonConfig) {
   // TODO support loading values from JSON docs for each type of
